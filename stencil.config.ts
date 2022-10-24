6b9c8d1ab16a704dbf6964978e5a9b32ecfe68e5
Jake Merringer <jake.merringer@gmail.com>
2022-10-24 14:43:05 -0500

    update component namespace

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'stencil-webcomponents',
+  namespace: 'justifi-webcomponents',
   outputTargets: [
     {
       type: 'dist',
