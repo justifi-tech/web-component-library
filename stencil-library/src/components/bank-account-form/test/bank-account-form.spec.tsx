62473954f807c38ef563b9ab41a86c0659e0f031
Jake Merringer <jake.merringer@gmail.com>
2022-12-18 17:19:45 -0600

    add bank-account-form tests

--- stencil-library/src/components/bank-account-form/test/bank-account-form.spec.tsx
+++ stencil-library/src/components/bank-account-form/test/bank-account-form.spec.tsx
@@ -1,18 +1,42 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { BankAccountForm } from '../bank-account-form';
 
-describe('bank-account-form', () => {
-  it('renders', async () => {
+describe('justifi-bank-account-form', () => {
+  it('renders with justifi-payment-method-form as a child element', async () => {
     const page = await newSpecPage({
       components: [BankAccountForm],
-      html: `<bank-account-form></bank-account-form>`,
+      html: `<justifi-bank-account-form></justifi-bank-account-form>`,
     });
     expect(page.root).toEqualHtml(`
-      <bank-account-form>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </bank-account-form>
+      <justifi-bank-account-form>
+        <justifi-payment-method-form iframe-origin="https://js.justifi.ai/bank-account"></justifi-payment-method-form>
+      </justifi-bank-account-form>
     `);
+  });
+
+  it('passes iframe-origin down to justifi-payment-method-form', async () => {
+    const page = await newSpecPage({
+      components: [BankAccountForm],
+      html: `
+      <justifi-bank-account-form iframe-origin="https://www.test.com"></justifi-bank-account-form>
+      `,
+    });
+    expect(page.root).toEqualHtml(`
+      <justifi-bank-account-form iframe-origin="https://www.test.com">
+        <justifi-payment-method-form iframe-origin="https://www.test.com"></justifi-payment-method-form>
+      </justifi-bank-account-form>
+    `);
+  });
+
+  it('has a tokenize method witch calls tokenize on justifi-payment-method-form', async () => {
+    const bankAccountForm = new BankAccountForm();
+    expect(bankAccountForm.tokenize).toBeDefined();
+
+    // mock childRef
+    (bankAccountForm as any).childRef = { tokenize: () => { } };
+    const childRefTokenizeSpy = jest.spyOn((bankAccountForm as any).childRef, 'tokenize');
+
+    bankAccountForm.tokenize();
+    expect(childRefTokenizeSpy).toHaveBeenCalled();
   });
 });
