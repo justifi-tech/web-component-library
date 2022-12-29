87c3efa60fa59f1bccde5b19cea64289a89388b7
Tomás Chernov <Chernatom@gmail.com>
2022-12-29 14:49:08 -0300

    fix JSX error

--- react-library/src/components/stencil-generated/index.ts
+++ react-library/src/components/stencil-generated/index.ts
@@ -3,7 +3,7 @@
 /* auto-generated react proxies */
 import { createReactComponent } from './react-component-lib';
 
-import type { JSX } from '@justifi/webcomponents/dist/types/components';
+import type { JSX } from '@justifi/webcomponents';
 
 import { defineCustomElements } from '@justifi/webcomponents/loader';
 
--- stencil-library/src/index.ts
+++ stencil-library/src/index.ts
@@ -1,3 +1,8 @@
 export { BankAccountForm } from './components/bank-account-form/bank-account-form';
 export { CardForm } from './components/card-form/card-form';
 export { PaymentsList } from './components/payments-list/payments-list';
+
+
+// I have many doubts about this, but it seems to build properly,
+// and without it, the React Library throws an error
+export { JSX } from './components';
