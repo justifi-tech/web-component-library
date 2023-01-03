005ee0fb01c209fe96e3998d42e9feeb9e0b2dc0
Jake Merringer <jakemerringer@Jakes-MacBook-Pro.local>
2023-01-03 13:41:28 -0600

    revert react-library changes

--- react-library/src/components/stencil-generated/index.ts
+++ react-library/src/components/stencil-generated/index.ts
@@ -8,7 +8,5 @@ import type { JSX } from '@justifi/webcomponents';
 import { defineCustomElements } from '@justifi/webcomponents/loader';
 
 defineCustomElements();
-export const JustifiBankAccountForm = /*@__PURE__*/createReactComponent<JSX.JustifiBankAccountForm, HTMLJustifiBankAccountFormElement>('justifi-bank-account-form');
 export const JustifiCardForm = /*@__PURE__*/createReactComponent<JSX.JustifiCardForm, HTMLJustifiCardFormElement>('justifi-card-form');
-export const JustifiPaymentMethodForm = /*@__PURE__*/createReactComponent<JSX.JustifiPaymentMethodForm, HTMLJustifiPaymentMethodFormElement>('justifi-payment-method-form');
 export const JustifiPaymentsList = /*@__PURE__*/createReactComponent<JSX.JustifiPaymentsList, HTMLJustifiPaymentsListElement>('justifi-payments-list');
