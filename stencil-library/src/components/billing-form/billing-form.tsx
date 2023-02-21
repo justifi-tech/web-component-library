204eedcf6f38378b43fe1f3276f01eaeabec604d
Jake Merringer <jakemerringer@Jakes-MacBook-Pro.local>
2023-02-21 17:02:22 -0600

    basic billing-form markup

--- stencil-library/src/components/billing-form/billing-form.tsx
+++ stencil-library/src/components/billing-form/billing-form.tsx
@@ -10,7 +10,22 @@ export class BillingForm {
   render() {
     return (
       <Host>
-        <slot></slot>
+        <fieldset>
+          <label>Street Address</label>
+          <input name="address_line1" type="text" />
+          <br />
+          <label>Apartment, Suite, etc. (optional)</label>
+          <input name="address_line2" type="text" />
+          <br />
+          <label>City</label>
+          <input name="address_city" type="text" />
+          <br />
+          <label>State</label>
+          <input name="address_state" type="text" />
+          <br />
+          <label>ZIP</label>
+          <input name="address_postal_code" type="text" />
+        </fieldset>
       </Host>
     );
   }
