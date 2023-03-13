873891690bb0d7d5a07c77b235191ec620165b71
Jake Merringer <jakemerringer@Jakes-MacBook-Pro.local>
2023-03-13 16:05:12 -0500

    check that ZIP is 5 digits

--- stencil-library/src/components/billing-form/billing-form-schema.ts
+++ stencil-library/src/components/billing-form/billing-form-schema.ts
@@ -1,5 +1,7 @@
 import { object, string } from 'yup';
 
+export const RegExZip = /^\d{5}/;
+
 const BillingFormSchema = object({
   // name: string().required('Enter name'),
   address_line1: string().required('Enter street address'),
@@ -8,6 +10,7 @@ const BillingFormSchema = object({
   address_state: string().required('Choose state'),
   address_postal_code: string()
     .required('Enter ZIP')
+    .matches(RegExZip, 'Enter a valid ZIP')
     .min(5, 'Enter a valid ZIP')
 });
 
