ab05f1682ab29ba3df4a9fbb6a001d61a3a78fae
Jake Merringer <jakemerringer@gmail.com>
2023-04-07 11:37:18 -0500

    payment-form story cleanup

--- stencil-library/src/components/payment-form/payment-form.stories.ts
+++ stencil-library/src/components/payment-form/payment-form.stories.ts
@@ -23,37 +23,18 @@ const paymentMethodData = {
 // IMPORTANT:
 // Only use named functions when binding to event handlers. Otherwise, they will be bound multiple times
 // Going forward, we could create a decorator that binds events
-const handleTokenizeClick = async () => {
+const handleSubmitClick = async () => {
   const paymentForm = document.querySelector('justifi-payment-form') as HTMLJustifiPaymentFormElement;
   const tokenizeResponse = await paymentForm.submit({ clientId: '123', paymentMethodData: paymentMethodData });
   console.log(tokenizeResponse);
 }
 const handleReadyClick = () => {
   const submitButton = document.querySelector('#submit-button');
-  submitButton?.addEventListener('click', handleTokenizeClick);
+  submitButton?.addEventListener('click', handleSubmitClick);
 }
 const addEvents = () => {
   addEventListener('cardFormReady', handleReadyClick);
 }
-
-
-// This should be abstracted away, included as a decorator and styled properly
-const buttons = `
-  <style>
-    .button-bar {
-      display: flex;
-      aligin-items: center;
-      padding: 10px;
-    }
-    .button-bar button {
-      margin-right: 10px;
-    }
-  </style>
-  <div class="button-bar">
-    <button id="validate-btn">Validate</button>
-    <button id="tokenize-btn">Tokenize</button>
-  </div>
-`;
 
 const Template = () => {
   // The <div> here should be replaced by a `display` property in the cardForm potentially
@@ -61,7 +42,9 @@ const Template = () => {
     <div>
       <justifi-payment-form />
     </div>
-    ${buttons}
+    <div>
+      <button id="submit-button">Submit</button>
+    </div>
   `);
 };
 
