ccbcd19f08296168ecc09e8f1e0b9363494e302c
Jake Merringer <jakemerringer@gmail.com>
2023-04-10 17:09:41 -0500

    fill out argTypes

--- stencil-library/src/components/payment-form/payment-form.stories.ts
+++ stencil-library/src/components/payment-form/payment-form.stories.ts
@@ -1,20 +1,3 @@
-export default {
-  title: 'Components/PaymentForm',
-  component: 'justifi-payment-form',
-  parameters: {
-    actions: {
-      handles: [
-      ],
-    },
-  },
-  decorators: [
-    (story, { args }) => `
-      ${story()}
-      <script>${addEvents()}</script>
-    `,
-  ],
-};
-
 const paymentMethodData = {
   name: 'John Doe',
   metadata: { something: "somevalue" } // optional
@@ -24,6 +7,7 @@ const paymentMethodData = {
 // Only use named functions when binding to event handlers. Otherwise, they will be bound multiple times
 // Going forward, we could create a decorator that binds events
 const handleSubmitClick = async () => {
+  console.log('handleSubmitClick');
   const paymentForm = document.querySelector('justifi-payment-form') as HTMLJustifiPaymentFormElement;
   const tokenizeResponse = await paymentForm.submit({ clientId: '123', paymentMethodData: paymentMethodData });
   console.log(tokenizeResponse);
@@ -31,9 +15,6 @@ const handleSubmitClick = async () => {
 const handleReadyClick = () => {
   const submitButton = document.querySelector('#submit-button');
   submitButton?.addEventListener('click', handleSubmitClick);
-}
-const addEvents = () => {
-  addEventListener('cardFormReady', handleReadyClick);
 }
 
 const Template = (args: { 'bank-account': boolean, card: boolean }) => {
@@ -46,6 +27,55 @@ const Template = (args: { 'bank-account': boolean, card: boolean }) => {
       <button id="submit-button">Submit</button>
     </div>
   `);
+};
+
+export default {
+  title: 'Components/PaymentForm',
+  component: 'justifi-payment-form',
+  argTypes: {
+    'bank-account': {
+      control: 'boolean',
+      table: {
+        category: 'Props'
+      }
+    },
+    card: {
+      control: 'boolean',
+      table: {
+        category: 'Props'
+      }
+    },
+    accountId: {
+      control: 'text',
+      table: {
+        category: 'Tokenize Args'
+      }
+    },
+    clientId: {
+      control: 'text',
+      table: {
+        category: 'Tokenize Args'
+      }
+    },
+    paymentMethodData: {
+      control: 'object',
+      table: {
+        category: 'Tokenize Args'
+      }
+    },
+  },
+  parameters: {
+    actions: {
+      handles: [],
+    },
+  },
+  decorators: [
+    (story, { args }) => `
+      ${story()}
+      ${JSON.stringify(args)}
+      <script>${handleReadyClick()}</script>
+    `,
+  ],
 };
 
 export const Basic = Template.bind({});
