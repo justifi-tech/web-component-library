e6c8969eb5c9ea7e4ba2e4dc48896a1b357a6483
Tomás Chernov <Chernatom@gmail.com>
2023-04-14 15:16:51 -0300

    address feedback

--- stencil-library/src/components/card-form/card-form.stories.ts
+++ stencil-library/src/components/card-form/card-form.stories.ts
@@ -1,7 +1,5 @@
-import { userEvent, waitFor, within } from '@storybook/testing-library';
+import { userEvent, within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
-
-import { FormButtons, handleReady } from "../../../storybook-utils";
 
 export default {
   title: 'Components/CardForm',
@@ -23,9 +21,43 @@ export default {
   ],
 };
 
+const handleValidateClick = async (cardForm: HTMLJustifiCardFormElement) => {
+  const valid = await cardForm.validate();
+  console.log(valid);
+}
+const handleTokenizeClick = async (cardForm: HTMLJustifiCardFormElement, paymentMethodData) => {
+  const tokenizeResponse = await cardForm.tokenize('CLIENT_ID', paymentMethodData);
+  console.log(tokenizeResponse);
+}
+const handleReady = () => {
+  console.log('card form is ready');
+  const cardForm = document.querySelector('justifi-card-form') as HTMLJustifiCardFormElement;
+  const validateBtn = document.querySelector('#validate-btn');
+  const tokenizeBtn = document.querySelector('#tokenize-btn');
+  validateBtn?.addEventListener('click', () => { handleValidateClick(cardForm) });
+  tokenizeBtn?.addEventListener('click', () => { handleTokenizeClick(cardForm, {}) });
+}
+
 const addEvents = () => {
   addEventListener('cardFormReady', handleReady);
 }
+
+const FormButtons = `
+  <style>
+    .button-bar {
+      display: flex;
+      aligin-items: center;
+      padding: 10px;
+    }
+    .button-bar button {
+      margin-right: 10px;
+    }
+  </style>
+  <div class="button-bar">
+    <button id="validate-btn">Validate</button>
+    <button id="tokenize-btn">Tokenize</button>
+  </div>
+`
 
 const storyStyleOverrides = {
   "layout": {
