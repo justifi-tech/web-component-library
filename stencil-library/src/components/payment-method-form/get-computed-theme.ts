8a706680ddd713184e6e69f1c59429c5075af34a
Jake Merringer <jakemerringer@gmail.com>
2023-04-18 15:08:43 -0500

    attempt at computing styles

---
+++ stencil-library/src/components/payment-method-form/get-computed-theme.ts
@@ -0,0 +1,35 @@
+import { Theme } from "./theme";
+
+const getComputedTheme = (labelElement, inputElement) => {
+  if (!inputElement || !labelElement) return;
+
+  console.log(document.styleSheets[0].rules[0])
+
+  // default state styles
+  const labelStyles = getComputedStyle(labelElement);
+  const inputStyles = getComputedStyle(inputElement);
+
+  // focus state styles
+  inputElement.focus();
+  const inputFocusStyles = getComputedStyle(inputElement);
+  inputElement.blur();
+
+  // hover state styles
+  const inputHoverStyles = getComputedStyle(inputElement);
+
+  const computedTheme: Theme = {
+    formControl: {
+      backgroundColor: inputStyles.getPropertyValue('background-color'),
+      backgroundColorHover: inputHoverStyles.getPropertyValue('background-color'),
+      borderColor: inputStyles.getPropertyValue('border-color'),
+      borderColorFocus: inputFocusStyles.getPropertyValue('border-color'),
+      color: inputStyles.getPropertyValue('color'),
+    },
+    formLabel: {
+      ...labelStyles
+    }
+  };
+  return computedTheme;
+}
+
+export default getComputedTheme;
