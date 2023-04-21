4be0ebc8b6b52179ad56af6a0542e90a2a52f4fa
Tomás Chernov <Chernatom@gmail.com>
2023-04-21 14:54:26 -0300

    move to docs approach

--- stencil-library/storybook-static/sb-addons/storybook-9/manager-bundle.mjs
+++
@@ -1,4 +0,0 @@
-try{
-var c=__STORYBOOKAPI__,{ActiveTabs:l,Consumer:m,ManagerContext:b,Provider:h,addons:r,combineParameters:p,controlOrMetaKey:y,controlOrMetaSymbol:d,eventMatchesShortcut:A,eventToShortcut:O,isMacLike:T,isShortcutTaken:_,keyToSymbol:P,merge:g,mockChannel:k,optionOrAltSymbol:M,shortcutMatchesShortcut:v,shortcutToHumanString:C,types:f,useAddonState:x,useArgTypes:K,useArgs:B,useChannel:G,useGlobalTypes:I,useGlobals:R,useParameter:Y,useSharedState:H,useStoryPrepared:L,useStorybookApi:j,useStorybookState:q}=__STORYBOOKAPI__;r.setConfig({panelPosition:"bottom",enableShortcuts:!1,initialActive:"bottom"});
-}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
-//# sourceMappingURL=manager-bundle.mjs.map
