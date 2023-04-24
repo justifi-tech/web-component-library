1e1d66745b124b5a6677c2000c6ef21991b27ae0
Tomás Chernov <Chernatom@gmail.com>
2023-04-24 17:17:04 -0300

    remove styled story

--- stencil-library/src/components/bank-account-form/bank-account-form.stories.ts
+++ stencil-library/src/components/bank-account-form/bank-account-form.stories.ts
@@ -58,64 +58,13 @@ const FormButtons = `
   </div>
 `
 
-const storyStyleOverrides = {
-  "layout": {
-    "padding": "100px",
-    "formControlSpacingHorizontal": ".5rem",
-    "formControlSpacingVertical": "1rem"
-  },
-  "formLabel": {
-    "fontWeight": "700",
-    "fontFamily": "sans-serif",
-    "margin": "0 0 .5rem 0"
-  },
-  "formControl": {
-    "backgroundColor": "#00F4F6",
-    "backgroundColorHover": "#EEEEF5",
-    "borderColor": "rgba(0, 0, 0, 0.42)",
-    "borderColorHover": "rgba(0, 0, 0, 0.62)",
-    "borderColorFocus": "#fccc32",
-    "borderColorError": "#C12727",
-    "borderWidth": "0px",
-    "borderBottomWidth": "1px",
-    "borderRadius": "40px 4px 0 0",
-    "borderStyle": "solid",
-    "boxShadowErrorFocus": "none",
-    "boxShadowFocus": "none",
-    "color": "#212529",
-    "fontSize": "1rem",
-    "fontWeight": "400",
-    "lineHeight": "2",
-    "margin": "0",
-    "padding": ".5rem .875rem"
-  },
-  "errorMessage": {
-    "color": "#C12727",
-    "margin": ".25rem 0 0 0",
-    "fontSize": ".875rem"
-  }
-};
-
-const Template = ({
-  includeButtons = true,
-  styleOverrides
-}: {
-  includeButtons: boolean,
-  styleOverrides?: object
-}) => {
-  const parsedStyleOverrides = styleOverrides ? JSON.stringify(styleOverrides) : null;
-
+const Template = ({ includeButtons = true }: { includeButtons: boolean }) => {
   return (`
     <div>
-      <justifi-bank-account-form data-testid="bank-account-form-iframe" style-overrides='${parsedStyleOverrides || ''}' />
+      <justifi-bank-account-form data-testid="bank-account-form-iframe" />
     </div>
     ${includeButtons ? FormButtons : ''}
   `);
 };
 
 export const Basic = Template.bind({});
-
-export const Styled = Template.bind({});
-Styled.args = {
-  styleOverrides: storyStyleOverrides
-}
