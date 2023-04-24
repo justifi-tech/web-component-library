c9935ec4bd7d1fe728a1af374eb39ec6a9f1446e
Jake Merringer <jakemerringer@gmail.com>
2023-04-24 15:36:45 -0500

    update styling example for CardForm

--- stencil-library/src/components/card-form/card-form.stories.ts
+++ stencil-library/src/components/card-form/card-form.stories.ts
@@ -59,56 +59,19 @@ const FormButtons = `
   </div>
 `
 
-const storyStyleOverrides = {
-  "layout": {
-    "padding": "100px",
-    "formControlSpacingHorizontal": ".5rem",
-    "formControlSpacingVertical": "1rem"
-  },
-  "formLabel": {
-    "fontWeight": "700",
-    "fontFamily": "sans-serif",
-    "margin": "0 0 .5rem 0"
-  },
-  "formControl": {
-    "backgroundColor": "#00F4F6",
-    "backgroundColorHover": "#EEEEF5",
-    "borderColor": "rgba(0, 0, 0, 0.42)",
-    "borderColorHover": "rgba(0, 0, 0, 0.62)",
-    "borderColorFocus": "#fccc32",
-    "borderColorError": "#C12727",
-    "borderWidth": "0px",
-    "borderBottomWidth": "1px",
-    "borderRadius": "40px 4px 0 0",
-    "borderStyle": "solid",
-    "boxShadowErrorFocus": "none",
-    "boxShadowFocus": "none",
-    "color": "#212529",
-    "fontSize": "1rem",
-    "fontWeight": "400",
-    "lineHeight": "2",
-    "margin": "0",
-    "padding": ".5rem .875rem"
-  },
-  "errorMessage": {
-    "color": "#C12727",
-    "margin": ".25rem 0 0 0",
-    "fontSize": ".875rem"
-  }
-};
-
 const Template = ({
   includeButtons = true,
-  styleOverrides
+  cssVariables
 }: {
   includeButtons: boolean,
-  styleOverrides?: object
+  cssVariables?: string
 }) => {
-  const parsedStyleOverrides = styleOverrides ? JSON.stringify(styleOverrides) : null;
-
   return (`
     <div>
-      <justifi-card-form data-testid="card-form-iframe" style-overrides='${parsedStyleOverrides || ''}' />
+      <style>
+        :root { ${cssVariables} }
+      </style>
+      <justifi-card-form data-testid="card-form-iframe" />
     </div>
     ${includeButtons ? FormButtons : ''}
   `);
@@ -132,7 +95,38 @@ Embedded.decorators = [
 
 export const Styled = Template.bind({});
 Styled.args = {
-  styleOverrides: storyStyleOverrides
+  cssVariables: (`
+  --jfi-layout-padding: 0;
+  --jfi-layout-form-control-spacing-x: .5rem;
+  --jfi-layout-form-control-spacing-y: 1rem;
+  --jfi-form-label-font-weight: 700;
+  --jfi-form-label-font-family: sans-serif;
+  --jfi-form-label-margin: 0 0 .5rem 0;
+  --jfi-form-control-background-color: #F4F4F6;
+  --jfi-form-control-background-color-hover: #EEEEF5;
+  --jfi-form-control-border-color: rgba(0, 0, 0, 0.42);
+  --jfi-form-control-border-color-hover: rgba(0, 0, 0, 0.62);
+  --jfi-form-control-border-color-focus: #fccc32;
+  --jfi-form-control-border-color-error: #C12727;
+  --jfi-form-control-border-top-width: 0;
+  --jfi-form-control-border-left-width: 0;
+  --jfi-form-control-border-bottom-width: 1px;
+  --jfi-form-control-border-right-width: 0;
+  --jfi-form-control-border-radius: 4px 4px 0 0;
+  --jfi-form-control-border-style: solid;
+  --jfi-form-control-box-shadow-focus: none;
+  --jfi-form-control-box-shadow-error-focus: none;
+  --jfi-form-control-border-style: solid;
+  --jfi-form-control-color: #212529;
+  --jfi-form-control-font-size: 1rem;
+  --jfi-form-control-font-weight: 400;
+  --jfi-form-control-line-height: 2;
+  --jfi-form-control-margin: 0;
+  --jfi-form-control-padding: .5rem .875rem;
+  --jfi-error-message-color: #C12727;
+  --jfi-error-message-margin: .25rem 0 0 0;
+  --jfi-error-message-font-size: .875rem;
+  `)
 }
 
 export const Completed = Template.bind({});
