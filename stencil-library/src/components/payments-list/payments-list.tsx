b4b9ab530e91d8758e414168c43f2471bd1f3ea3
Tomás Chernov <Chernatom@gmail.com>
2023-06-13 13:15:40 -0300

    Add missing docs and css parts

--- stencil-library/src/components/payments-list/payments-list.tsx
+++ stencil-library/src/components/payments-list/payments-list.tsx
@@ -2,7 +2,15 @@ import { Component, Host, h, Prop, State, Watch } from '@stencil/core';
 import { Api, IApiResponseCollection, Payment } from '../../api';
 import { formatCurrency, formatDate, formatTime } from '../../utils/utils';
 
-
+/**
+   *
+   * @exportedPart table-head: Head of the table
+   * @exportedPart table-body: Body of the table
+   * @exportedPart loading-state: Row for loading state
+   * @exportedPart error-state: Row for Error state
+   * @exportedPart empty-state: Row for Emtpy state
+   *
+   */
 @Component({
   tag: 'justifi-payments-list',
   styleUrl: 'payments-list.scss',
@@ -14,6 +22,7 @@ export class PaymentsList {
   @Prop() authToken: string;
   @State() payments: Payment[] = [];
   @State() loading: boolean = true;
+  @State() errorMessage: string;
 
   @Watch('accountId')
   @Watch('authToken')
@@ -25,22 +34,24 @@ export class PaymentsList {
     this.fetchData();
   }
 
-  fetchData(): void {
+  async fetchData(): Promise<void> {
+    if (!this.accountId || !this.authToken) {
+      this.errorMessage = "Can not fetch any data without an AccountID and an AuthToken"
+      this.loading = false;
+      return;
+    }
     this.loading = true;
-    const accountId = this.accountId;
-    const endpoint = `account/${accountId}/payments`;
-    Api(this.authToken)
-      .get(endpoint)
-      .then((response: IApiResponseCollection<Payment[]>) => {
-        const data = response?.data?.map(dataItem => new Payment(dataItem));
-        this.payments = data;
-      })
-      .catch((e) => {
-        console.error(e);
-      })
-      .finally(() => {
-        this.loading = false;
-      });
+    const endpoint = `account/${this.accountId}/payments`;
+
+    const response: IApiResponseCollection<Payment[]> = await Api(this.authToken).get(endpoint);
+    if (!response.error) {
+      const data = response?.data?.map(dataItem => new Payment(dataItem));
+      this.payments = data;
+    } else {
+      this.errorMessage = typeof response.error === 'string' ? response.error : response.error.message;
+    }
+
+    this.loading = false;
   }
 
   showEmptyState() {
@@ -48,26 +59,34 @@ export class PaymentsList {
   }
 
   emptyState = (
-    <tr>
+    <tr part="empty-state">
       <td colSpan={8} style={{ textAlign: 'center' }}>No payments to show</td>
     </tr>
   );
 
+  errorState = () => (
+    <tr part="error-state">
+      <td colSpan={8} style={{ textAlign: 'center' }}>
+        <span>An unexpected error occurred: {this.errorMessage}</span>
+      </td>
+    </tr>
+  );
+
   loadingState = (
-    <tr>
+    <tr part="loading-state">
       <td colSpan={8} style={{ textAlign: 'center' }}>
         <div class="spinner-border" role="status">
           <span class="visually-hidden">Loading...</span>
         </div>
       </td>
     </tr>
-  )
+  );
 
   render() {
     return (
-      <Host>
+      <Host exportparts="table-head,table-body,loading-state,error-state,empty-state">
         <table class="table">
-          <thead>
+          <thead part='table-head'>
             <tr>
               <th scope="col" title="The date and time each payment was made">
                 Made on
@@ -75,35 +94,32 @@ export class PaymentsList {
               <th scope="col" title="The dollar amount of each payment">
                 Amount
               </th>
-              <th scope="col">Account</th>
               <th scope="col">Description</th>
-              <th scope="col">Payment ID</th>
               <th scope="col">Cardholder</th>
               <th scope="col">Payment Method</th>
               <th scope="col">Status</th>
+              <th scope="col">Payment ID</th>
             </tr>
           </thead>
-          <tbody>
-            {!this.loading
-              ? (this.showEmptyState()
-              ? this.emptyState
-              : this.payments?.map(payment =>
-                  <tr>
-                    <td>
-                      <div>{formatDate(payment.created_at)}</div>
-                      <div>{formatTime(payment.created_at)}</div>
-                    </td>
-                    <td>{formatCurrency(payment.amount)}</td>
-                    <td>{payment.account_id}</td>
-                    <td>{payment.description}</td>
-                    <td>{payment.id}</td>
-                    <td>{payment.payment_method?.card?.name}</td>
-                    <td>{payment.payment_method?.card?.acct_last_four}</td>
-                    <td>{payment.status}</td>
-                  </tr>
-                )
+          <tbody part='table-body'>
+            {
+              this.loading ? this.loadingState :
+              this.errorMessage ? this.errorState() :
+              this.showEmptyState() ? this.emptyState :
+              this.payments?.map(payment =>
+                <tr>
+                  <td>
+                    <div>{formatDate(payment.created_at)}</div>
+                    <div>{formatTime(payment.created_at)}</div>
+                  </td>
+                  <td>{formatCurrency(payment.amount)}</td>
+                  <td>{payment.description}</td>
+                  <td>{payment.payment_method?.card?.name}</td>
+                  <td>{payment.payment_method?.card?.acct_last_four}</td>
+                  <td>{payment.status}</td>
+                  <td>{payment.id}</td>
+                </tr>
               )
-              : this.loadingState
             }
           </tbody>
         </table>
