d305356299cc95c93aa12900b725c083a630c492
Tomás Chernov <Chernatom@gmail.com>
2023-06-13 02:53:55 -0300

    finish basic functionality

--- stencil-library/src/components/payments-list/payments-list.tsx
+++ stencil-library/src/components/payments-list/payments-list.tsx
@@ -2,36 +2,49 @@ import { Component, Host, h, Prop, State, Watch } from '@stencil/core';
 import { Api, IApiResponseCollection, Payment } from '../../api';
 import { formatCurrency, formatDate, formatTime } from '../../utils/utils';
 
+
 @Component({
   tag: 'justifi-payments-list',
   styleUrl: 'payments-list.scss',
   shadow: true,
 })
+
 export class PaymentsList {
   @Prop() accountId: string;
   @Prop() authToken: string;
   @State() payments: Payment[] = [];
+  @State() loading: boolean = true;
 
   @Watch('accountId')
   @Watch('authToken')
-  requestPropsChanged() {
-    console.log('this.authToken', this.authToken);
+  updateOnPropChange() {
+    this.fetchData();
+  }
+
+  connectedCallback() {
     this.fetchData();
   }
 
   fetchData(): void {
+    this.loading = true;
     const accountId = this.accountId;
     const endpoint = `account/${accountId}/payments`;
-    Api(this.auth.token)
+    Api(this.authToken)
       .get(endpoint)
       .then((response: IApiResponseCollection<Payment[]>) => {
-        const data = response.data.map(dataItem => new Payment(dataItem));
+        const data = response?.data?.map(dataItem => new Payment(dataItem));
         this.payments = data;
+      })
+      .catch((e) => {
+        console.error(e);
+      })
+      .finally(() => {
+        this.loading = false;
       });
   }
 
   showEmptyState() {
-    return this.payments.length < 1;
+    return this.payments ? this.payments.length < 1 : true;
   }
 
   emptyState = (
@@ -39,6 +52,16 @@ export class PaymentsList {
       <td colSpan={8} style={{ textAlign: 'center' }}>No payments to show</td>
     </tr>
   );
+
+  loadingState = (
+    <tr>
+      <td colSpan={8} style={{ textAlign: 'center' }}>
+        <div class="spinner-border" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </td>
+    </tr>
+  )
 
   render() {
     return (
@@ -61,21 +84,27 @@ export class PaymentsList {
             </tr>
           </thead>
           <tbody>
-            {this.payments.map(payment => {
-              <tr>
-                <td>
-                  <div>{formatDate(payment.created_at)}</div>
-                  <div>{formatTime(payment.created_at)}</div>
-                </td>
-                <td>{formatCurrency(payment.amount)}</td>
-                <td>{payment.account_id}</td>
-                <td>{payment.description}</td>
-                <td>{payment.id}</td>
-                <td>{payment.payment_method?.card?.name}</td>
-                <td>{payment.payment_method?.card?.acct_last_four}</td>
-                <td>{payment.status}</td>
-              </tr>;
-            })}
+            {!this.loading
+              ? (this.showEmptyState()
+              ? this.emptyState
+              : this.payments?.map(payment =>
+                  <tr>
+                    <td>
+                      <div>{formatDate(payment.created_at)}</div>
+                      <div>{formatTime(payment.created_at)}</div>
+                    </td>
+                    <td>{formatCurrency(payment.amount)}</td>
+                    <td>{payment.account_id}</td>
+                    <td>{payment.description}</td>
+                    <td>{payment.id}</td>
+                    <td>{payment.payment_method?.card?.name}</td>
+                    <td>{payment.payment_method?.card?.acct_last_four}</td>
+                    <td>{payment.status}</td>
+                  </tr>
+                )
+              )
+              : this.loadingState
+            }
           </tbody>
         </table>
       </Host>
