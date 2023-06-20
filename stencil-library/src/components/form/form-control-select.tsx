ab21760ab721c7a9d8d1ea845211b65d80f8a4aa
Jake Merringer <jakemerringer@gmail.com>
2023-06-20 11:48:36 -0500

    add form compnent files

---
+++ stencil-library/src/components/form/form-control-select.tsx
@@ -0,0 +1,34 @@
+import { Component, Host, h, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'form-control-select',
+  styleUrl: 'form-control-select.scss',
+  shadow: true,
+})
+export class TextInput {
+  @Prop() label: string;
+  @Prop() name: any;
+  @Prop() onInput: (e: any) => void;
+  @Prop() error: string;
+  @Prop() options: { label: string; value: string }[];
+
+  render() {
+    return (
+      <Host exportparts="label,input,input-invalid">
+        <label part="label" class="form-label">
+          {this.label}
+        </label>
+        <select
+          name={this.name}
+          onInput={this.onInput}
+          part={`input ${this.error && 'input-invalid'}`}
+          class={this.error ? 'form-select is-invalid' : 'form-select'}>
+          {this.options.map(option => (
+            <option value={option.value}>{option.label}</option>
+          ))}
+        </select>
+        {this.error && <div class="invalid-feedback">{this.error}</div>}
+      </Host>
+    );
+  }
+}
---
+++ stencil-library/src/components/form/form-controller.ts
@@ -0,0 +1,68 @@
+import { ObjectSchema, ValidationError } from "yup";
+
+class FormController {
+  private defaultValues: any = {};
+  private schema: ObjectSchema<any>;
+  private controller = { values: {}, isValid: false, errors: {} };
+
+  get errors(): any {
+    return this.controller.errors;
+  }
+
+  validate = async () => {
+    console.log(this.controller.values)
+    this.controller.isValid = true;
+    this.controller.errors = {};
+
+    try {
+      await this.schema.validate(this.controller.values, { abortEarly: false });
+    } catch (err) {
+      this.controller.isValid = false;
+      err.inner.map((item: ValidationError) => {
+        this.controller.errors[item.path] = item.message;
+      });
+    }
+    return this.controller;
+  };
+
+  register = (name) => {
+    const splitName = name.split('.');
+    let onInput: (e: any) => void;
+    let defaultValue: string;
+
+    // TODO: This is a hacky way to handle nested form controllers. We should refactor this to be more robust.
+    // We can use a recursive function to handle this, such as reduceRight() and reduce() to get and set the values dynamically.
+    // For now we only support one level of nesting, which is fine in most cases and is more performant.
+    if (splitName.length = 1) {
+      defaultValue = this.defaultValues[name];
+      onInput = (e) => {
+        return this.controller.values[name] = e.target.value;
+      };
+    }
+    else if (splitName.length = 1) {
+      this.controller.values[splitName[0]] = {};
+      const defaultValueSection = this.defaultValues[splitName[0]]
+      if (defaultValueSection) {
+        defaultValue = defaultValueSection[splitName[1]];
+      }
+      onInput = (e) => {
+        return this.controller.values[splitName[0]][splitName[1]] = e.target.value;
+      };
+    } else {
+      throw new Error('Deeply nested form controllers are not supported. Your controller name should only be nested one level deep.');
+    };
+
+    return {
+      name: name || '',
+      value: defaultValue || '',
+      onInput: onInput,
+    }
+  };
+
+  constructor(defaultValues: any, schema: ObjectSchema<any>) {
+    this.defaultValues = defaultValues;
+    this.schema = schema;
+  }
+}
+
+export default FormController;
