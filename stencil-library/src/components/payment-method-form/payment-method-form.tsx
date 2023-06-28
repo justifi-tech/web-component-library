5eac3755cccf2c082f302e61b4954e8aa1b32ff8
TiagoFuelber <tfuelber@gmail.com>
2023-06-28 15:51:32 -0300

    fix(payment-method-form.tsx): use this.iframeOrigin if available, otherwise fallback to process.env.IFRAME_ORIGIN

--- stencil-library/src/components/payment-method-form/payment-method-form.tsx
+++ stencil-library/src/components/payment-method-form/payment-method-form.tsx
@@ -109,7 +109,7 @@ export class PaymentMethodForm {
   }
 
   private getIframeSrc() {
-    const iframeOrigin = process.env.IFRAME_ORIGIN;
+    const iframeOrigin = this.iframeOrigin || process.env.IFRAME_ORIGIN;
     let iframeSrc = `${iframeOrigin}/${this.paymentMethodFormType}`;
     let paramsList = [];
     if (this.paymentMethodFormValidationMode) {
