48eda0204d9853614a18a343c5272f556bb8b934
TiagoFuelber <tfuelber@gmail.com>
2023-07-04 17:43:58 -0300

    test(billing-form.spec.tsx): update test case descriptions and add new test cases for filling the form, validating the form, and getting form values
    test(business-address.spec.tsx): update component name from 'business-address' to 'justifi-business-address'
    test(business-address.spec.tsx): update component HTML to match new component name
    test(business-address.spec.tsx): add test for handling 'fieldReceivedInput' event
    test(business-address.spec.tsx): add test for handling form submission and validation
    
    fix(select-input.tsx): initialize options prop with an empty array
    fix(select-input.tsx): fix part attribute value to include 'input-invalid' class when error is present
    
    test(select-input.spec.tsx): add tests for rendering select-input component with different props and error message
    
    test(text-input.spec.tsx): add tests for rendering with values, rendering with error, and emitting fieldReceivedInput event on input

--- stencil-library/src/components/billing-form/test/billing-form.spec.tsx
+++ stencil-library/src/components/billing-form/test/billing-form.spec.tsx
@@ -1,32 +1,71 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { BillingForm } from '../billing-form';
 
-describe('Billing Form', () => {
-  it('renders', async () => {
+describe('justifi-billing-form', () => {
+  it('fills the form', async () => {
     const page = await newSpecPage({
       components: [BillingForm],
-      html: `<justifi-billing-form />`,
+      html: `<justifi-billing-form></justifi-billing-form>`,
     });
-    expect(page.root).toEqualHtml(`
-      <justifi-billing-form>
-        <mock:shadow-root>
-          <fieldset>
-          <text-input defaultvalue="" label="Street Address" name="address_line1"></text-input>
-          <text-input defaultvalue="" label="Apartment, Suite, etc. (optional)" name="address_line2"></text-input>
-          <text-input defaultvalue="" label="City" name="address_city"></text-input>
-          <select-input defaultvalue="" label="State" name="address_state"></select-input>
-          <text-input defaultvalue="" label="ZIP" name="address_postal_code"></text-input>
-          </fieldset>
-        </mock:shadow-root>
-      </justifi-billing-form>
-    `);
-  });
-  it('has a validate method and is calllable', () => {
-    const billingForm = new BillingForm();
-    expect(billingForm.validate).toBeDefined();
 
-    const validateSpy = jest.spyOn(billingForm, 'validate');
-    billingForm.validate();
-    expect(validateSpy).toHaveBeenCalled();
+    const instance: any = page.rootInstance;
+
+    const fields = {
+      name: 'John Doe',
+      address_line1: 'Street Address',
+      address_line2: 'Apartment Address',
+      address_city: 'New York',
+      address_state: 'NY',
+      address_postal_code: '10000',
+    };
+
+    await instance.fill(fields);
+    expect(instance.billingFields).toEqual(fields);
+  });
+
+  it('validates the form', async () => {
+    const page = await newSpecPage({
+      components: [BillingForm],
+      html: `<justifi-billing-form></justifi-billing-form>`,
+    });
+
+    const instance: any = page.rootInstance;
+
+    const fields = {
+      name: 'John Doe',
+      address_line1: 'Street Address',
+      address_line2: 'Apartment Address',
+      address_city: 'New York',
+      address_state: 'NY',
+      address_postal_code: '10000',
+    };
+
+    await instance.fill(fields);
+    const { isValid } = await instance.validate();
+
+    expect(isValid).toBe(true); // Assuming provided fields pass the validation
+  });
+
+  it('gets values of the form', async () => {
+    const page = await newSpecPage({
+      components: [BillingForm],
+      html: `<justifi-billing-form></justifi-billing-form>`,
+    });
+
+    const instance: any = page.rootInstance;
+
+    const fields = {
+      name: 'John Doe',
+      address_line1: 'Street Address',
+      address_line2: 'Apartment Address',
+      address_city: 'New York',
+      address_state: 'NY',
+      address_postal_code: '10000',
+    };
+
+    await instance.fill(fields);
+    const values = await instance.getValues();
+
+    expect(values).toEqual(fields);
   });
 });
