48fd7bff66641d3e0039bb89b468314b29f68724
Tiago Fuelber <tfuelber@gmail.com>
2023-07-05 14:51:52 -0300

    Fix unit tests (#127)
    * test(billing-form.spec.tsx): update test case descriptions and add new test cases for filling the form, validating the form, and getting form values
    
    test(business-address.spec.tsx): update component name from 'business-address' to 'justifi-business-address'
    test(business-address.spec.tsx): update component HTML to match new component name
    test(business-address.spec.tsx): add test for handling 'fieldReceivedInput' event
    test(business-address.spec.tsx): add test for handling form submission and validation
    
    fix(select-input.tsx): initialize options prop with an empty array
    fix(select-input.tsx): fix part attribute value to include 'input-invalid' class when error is present
    
    test(select-input.spec.tsx): add tests for rendering select-input component with different props and error message
    
    test(text-input.spec.tsx): add tests for rendering with values, rendering with error, and emitting fieldReceivedInput event on input
    
    * test(business-address.e2e.ts): update component name from 'business-address' to 'justifi-business-address'

--- stencil-library/src/components/business-address/test/business-address.spec.tsx
+++ stencil-library/src/components/business-address/test/business-address.spec.tsx
@@ -1,18 +1,72 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { BusinessAddress } from '../business-address';
+import { BusinessAddressFormFields } from '../business-address-schema';
 
 describe('business-address', () => {
-  it('renders', async () => {
+  it('should render BusinessAddress component', async () => {
     const page = await newSpecPage({
       components: [BusinessAddress],
-      html: `<business-address></business-address>`,
+      html: `<justifi-business-address></justifi-business-address>`,
     });
     expect(page.root).toEqualHtml(`
-      <business-address>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </business-address>
-    `);
+    <justifi-business-address exportparts="label,input,input-invalid">
+      <mock:shadow-root>
+        <div class="gx-2 gy-2 row">
+          <div class="col-12">
+            <text-input defaultvalue="" label="Street Address" name="line1"></text-input>
+          </div>
+          <div class="col-12">
+            <text-input defaultvalue="" label="Apartment, Suite, etc. (optional)" name="line2"></text-input>
+          </div>
+          <div class="col-12">
+            <text-input defaultvalue="" label="City" name="city"></text-input>
+          </div>
+          <div class="col-12">
+            <select-input defaultvalue="" label="State" name="state"></select-input>
+          </div>
+          <div class="col-12">
+            <text-input defaultvalue="" label="Postal Code" name="postal_code"></text-input>
+          </div>
+        </div>
+      </mock:shadow-root>
+    </justifi-business-address>
+  `);
+  });
+
+  it('should handle fieldReceivedInput event', async () => {
+    const page = await newSpecPage({
+      components: [BusinessAddress],
+      html: `<justifi-business-address></justifi-business-address>`,
+    });
+
+    let component = page.rootInstance;
+
+    let newEvent = new CustomEvent('fieldReceivedInput', {
+      detail: { name: 'city', value: 'New York' },
+    });
+
+    component.setFormValue(newEvent);
+    expect(component.businessAddress.city).toBe('New York');
+  });
+
+  it('should handle form submission and validation', async () => {
+    const page = await newSpecPage({
+      components: [BusinessAddress],
+      html: `<justifi-business-address></justifi-business-address>`,
+    });
+
+    let component = page.rootInstance;
+    component.businessAddress = {
+      line1: 'Street 1',
+      line2: 'Apartment 1',
+      city: 'City',
+      state: 'State',
+      postal_code: '12345',
+      country: 'Country',
+    } as BusinessAddressFormFields;
+
+    let submissionResult = await component.submit();
+
+    expect(submissionResult.isValid).toBe(true);
   });
 });
