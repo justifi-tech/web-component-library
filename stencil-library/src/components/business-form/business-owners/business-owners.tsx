f1340a2a27c87140e131de037bb51766fe44543b
Jake Merringer <jakemerringer@gmail.com>
2023-07-24 15:41:35 -0500

    begin owners form

--- stencil-library/src/components/business-form/business-owners/business-owners.tsx
+++ stencil-library/src/components/business-form/business-owners/business-owners.tsx
@@ -1,6 +1,20 @@
 import { Component, Host, h, Prop, State, Watch } from '@stencil/core';
 import { FormController } from '../../form/form';
 
+class BusinessOwner {
+  name: string = '';
+  title: string = '';
+  email: string = '';
+  phone: string = '';
+  dob_day: string = '';
+  dob_month: string = '';
+  dob_year: string = '';
+  identification_number: string = '';
+  is_owner: boolean = true;
+  metadata: any = {};
+  address: any = {};
+}
+
 /**
  * @exportedPart label: Label for inputs
  * @exportedPart input: The input fields
@@ -15,7 +29,7 @@ export class BusinessOwners {
   @Prop() formController: FormController
   @State() errors: any = {};
   @State() defaultValues: any = {};
-  @State() owners: any = [];
+  @State() owners: BusinessOwner[] = [];
 
   @Watch('owners')
   handleRepresentativeChange(newValues: any) {
@@ -27,12 +41,29 @@ export class BusinessOwners {
     this.formController.defaultValues.subscribe((defaultValues) => this.defaultValues = { ...defaultValues });
   }
 
-  // inputHandler(name: string, value: string) {
-  // };
+  inputHandler(name: string, value: string, index: number) {
+    this.owners[index][name] = value;
+  };
 
   render() {
     return (
       <Host exportparts="label,input,input-invalid">
+        <fieldset>
+          <legend>Representative</legend>
+          {this.owners.map((owner: any, index: number) => {
+            <div class="row gx-2 gy-2">
+              <div class="col-12">
+                <form-control-text
+                  name="name"
+                  label="Full Name"
+                  defaultValue={this.defaultValues[index].name}
+                  error={this.errors[index].name}
+                  inputHandler={(name, value) => this.inputHandler(name, value, index)} />
+              </div>
+            </div>
+          })}
+          <button onClick={() => this.owners.push(new BusinessOwner())}>+ Add an owner</button>
+        </fieldset>
       </Host>
     );
   }
