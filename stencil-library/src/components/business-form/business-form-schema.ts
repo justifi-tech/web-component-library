c42d2f1e055b42380c1d93e32a8d1faf53e336b5
TiagoFuelber <tfuelber@gmail.com>
2023-07-28 16:06:26 -0300

    refactor(business-form-schema.ts): improve formatting and add type annotations to BusinessTypeOptions and BusinessStructureOptions arrays
    chore(business-form-schema.ts): add OwnerSchema to validate owner information in BusinessFormSchema
    fix(business-form-schema.ts): fix indentation and formatting in BusinessFormSchema
    
    chore(business-owners.tsx): add missing semicolon to formController prop declaration
    
    refactor(business-owners.tsx): clean up code formatting and add missing form control inputs for owner details
    feat(business-owners.tsx): add support for additional owner details such as email, phone, date of birth, identification number, and address
    fix(business-owners.tsx): fix remove owner button not working

--- stencil-library/src/components/business-form/business-form-schema.ts
+++ stencil-library/src/components/business-form/business-form-schema.ts
@@ -3,20 +3,21 @@ import * as Yup from 'yup';
 export const RegExZip = /^\d{5}/;
 
 type BusinessType = 'for_profit' | 'non_profit' | 'government_entity' | 'individual' | '';
-type BusinessStructure = 'sole_proprietorship' |
-  'single_llc' |
-  'multi_llc' |
-  'private_partnership' |
-  'private_corporation' |
-  'unincorporated_association' |
-  'public_partnership' |
-  'public_corporation' |
-  'incorporated' |
-  'unincorporated' |
-  'government_unit' |
-  'government_instrumentality' |
-  'tax_exempt_government_instrumentality' |
-  '';
+type BusinessStructure =
+  | 'sole_proprietorship'
+  | 'single_llc'
+  | 'multi_llc'
+  | 'private_partnership'
+  | 'private_corporation'
+  | 'unincorporated_association'
+  | 'public_partnership'
+  | 'public_corporation'
+  | 'incorporated'
+  | 'unincorporated'
+  | 'government_unit'
+  | 'government_instrumentality'
+  | 'tax_exempt_government_instrumentality'
+  | '';
 
 export class Business {
   public legal_name = '';
@@ -44,7 +45,7 @@ export class Business {
   }
 }
 
-export const BusinessTypeOptions: { label: string, value: BusinessType }[] = [
+export const BusinessTypeOptions: { label: string; value: BusinessType }[] = [
   {
     label: 'Choose business type',
     value: '',
@@ -67,7 +68,7 @@ export const BusinessTypeOptions: { label: string, value: BusinessType }[] = [
   },
 ];
 
-export const BusinessStructureOptions: { label: string, value: BusinessStructure }[] = [
+export const BusinessStructureOptions: { label: string; value: BusinessStructure }[] = [
   {
     label: 'Choose business structure',
     value: '',
@@ -126,6 +127,25 @@ export const BusinessStructureOptions: { label: string, value: BusinessStructure
   },
 ];
 
+const OwnerSchema = Yup.object({
+  name: Yup.string().required('Enter owner name'),
+  title: Yup.string().required('Enter owner title'),
+  email: Yup.string().email('Enter valid owner email').required('Enter owner email'),
+  phone: Yup.string().required('Enter owner phone number'),
+  dob_day: Yup.string().required('Enter owner birth day'),
+  dob_month: Yup.string().required('Enter owner birth month'),
+  dob_year: Yup.string().required('Enter owner birth year'),
+  identification_number: Yup.string().required('Enter owner identification number'),
+  is_owner: Yup.boolean(),
+  metadata: Yup.mixed(),
+  address: Yup.object({
+    line1: Yup.string().required('Enter owner street address'),
+    city: Yup.string().required('Enter owner city'),
+    state: Yup.string().required('Select owner state'),
+    postal_code: Yup.string().matches(RegExZip, 'Enter valid postal code').required('Enter owner postal code'),
+  }),
+});
+
 const BusinessFormSchema = Yup.object({
   legal_name: Yup.string().required('Enter legal name'),
   website_url: Yup.string().url('Enter valid website url').required('Enter website url'),
@@ -148,11 +168,9 @@ const BusinessFormSchema = Yup.object({
       city: Yup.string().required('Enter city'),
       state: Yup.string().required('Select state'),
       postal_code: Yup.string().required('Enter postal code'),
-    })
+    }),
   }),
-  owners: Yup.array().of(
-    Yup.object({ name: Yup.string().required('Enter owner name') })
-  ).min(1, 'Enter at least 1 owners'),
+  owners: Yup.array().of(OwnerSchema).min(1, 'Enter at least 1 owners'),
 });
 
 export default BusinessFormSchema;
--- stencil-library/src/components/business-form/business-owners/business-owners.tsx
+++ stencil-library/src/components/business-form/business-owners/business-owners.tsx
@@ -26,7 +26,7 @@ class BusinessOwner {
   shadow: false,
 })
 export class BusinessOwners {
-  @Prop() formController: FormController
+  @Prop() formController: FormController;
   @State() defaultValues: any[] = [];
   @State() errors: any[] = [];
   @State() owners: BusinessOwner[] = [];
@@ -34,61 +34,130 @@ export class BusinessOwners {
   @Watch('owners')
   handleOwnersChange(newValues: any) {
     this.formController.setValues({ owners: newValues });
-  };
+  }
 
   componentDidLoad() {
-    this.formController.defaultValues.subscribe((defaultValues) => this.defaultValues = { ...defaultValues.owners });
-    this.formController.errors.subscribe((errors) => this.errors = { ...errors.owners });
-  };
+    this.formController.defaultValues.subscribe(defaultValues => (this.defaultValues = { ...defaultValues.owners }));
+    this.formController.errors.subscribe(errors => (this.errors = { ...errors.owners }));
+  }
 
   inputHandler(name: string, value: string, index: number): void {
     this.owners[index][name] = value;
-    this.owners = [...this.owners]
-  };
+    this.owners = [...this.owners];
+  }
 
   addOwner(event: MouseEvent): void {
     event.preventDefault();
     this.owners.push(new BusinessOwner());
     this.owners = [...this.owners];
-  };
+  }
 
   removeOwner(event: MouseEvent, index: number): void {
     event.preventDefault();
     this.owners.splice(index, 1);
     this.owners = [...this.owners];
-  };
+  }
 
   render() {
     return (
       <Host exportparts="label,input,input-invalid">
         <fieldset>
           <legend>Owners</legend>
-          {this.owners.map((owner: BusinessOwner, index: number) => {
+          {this.owners.map((_owner: BusinessOwner, index: number) => {
             return (
               <div class="row gx-2 gy-2">
                 <div class="col-12">
-                  {owner}
                   <form-control-text
                     name="name"
                     label="Full Name"
                     defaultValue={this.defaultValues[index]?.name}
                     error={this.errors[index]?.name}
-                    inputHandler={(name, value) => this.inputHandler(name, value, index)} />
+                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                  />
+
                   <form-control-text
                     name="title"
                     label="Title"
                     defaultValue={this.defaultValues[index]?.title}
                     error={this.errors[index]?.title}
-                    inputHandler={(name, value) => this.inputHandler(name, value, index)} />
-                  <button type="button" class="btn" onClick={(event) => this.removeOwner(event, index)}>- Remove owner</button>
+                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                  />
+
+                  <form-control-text
+                    name="email"
+                    label="Email"
+                    defaultValue={this.defaultValues[index]?.email}
+                    error={this.errors[index]?.email}
+                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                  />
+
+                  <form-control-text
+                    name="phone"
+                    label="Phone"
+                    defaultValue={this.defaultValues[index]?.phone}
+                    error={this.errors[index]?.phone}
+                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                  />
+                </div>
+                <div class="col-4">
+                  <form-control-text
+                    name="dob_day"
+                    label="Date of Birth - Day"
+                    defaultValue={this.defaultValues[index]?.dob_day}
+                    error={this.errors[index]?.dob_day}
+                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                  />
+                </div>
+
+                <div class="col-4">
+                  <form-control-text
+                    name="dob_month"
+                    label="Date of Birth - Month"
+                    defaultValue={this.defaultValues[index]?.dob_month}
+                    error={this.errors[index]?.dob_month}
+                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                  />
+                </div>
+
+                <div class="col-4">
+                  <form-control-text
+                    name="dob_year"
+                    label="Date of Birth - Year"
+                    defaultValue={this.defaultValues[index]?.dob_year}
+                    error={this.errors[index]?.dob_year}
+                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                  />
+                </div>
+                <div>
+                  <form-control-text
+                    name="identification_number"
+                    label="Identification Number"
+                    defaultValue={this.defaultValues[index]?.identification_number}
+                    error={this.errors[index]?.identification_number}
+                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                  />
+
+                  {/* Assuming address is a single line text input for simplicity */}
+                  <form-control-text
+                    name="address"
+                    label="Address"
+                    defaultValue={this.defaultValues[index]?.address}
+                    error={this.errors[index]?.address}
+                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                  />
+
+                  <button type="button" class="btn" onClick={event => this.removeOwner(event, index)}>
+                    - Remove owner
+                  </button>
                 </div>
               </div>
             );
           })}
-          <button type="button" class="btn" onClick={(event) => this.addOwner(event)}>+ Add an owner</button>
+          <button type="button" class="btn" onClick={event => this.addOwner(event)}>
+            + Add an owner
+          </button>
         </fieldset>
       </Host>
     );
   }
-
 }
