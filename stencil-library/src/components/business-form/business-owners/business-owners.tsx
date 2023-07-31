3b38e5debd0b5d96a81b08d2d0666178e2c452f5
TiagoFuelber <tfuelber@gmail.com>
2023-07-31 11:34:11 -0300

    feat(react-library): add JustifiBusinessOwners component to the generated index.ts file
    refactor(business-owners.tsx): improve layout and styling of the BusinessOwners component
    feat(business-owners.tsx): add support for displaying and editing multiple owners
    feat(business-owners.tsx): add support for displaying and editing owner's full name, title, email, phone, date of birth, identification number, and address
    feat(business-owners.tsx): add support for adding and removing owners

--- stencil-library/src/components/business-form/business-owners/business-owners.tsx
+++ stencil-library/src/components/business-form/business-owners/business-owners.tsx
@@ -61,101 +61,175 @@ export class BusinessOwners {
   render() {
     return (
       <Host exportparts="label,input,input-invalid">
-        <fieldset>
-          <legend>Owners</legend>
+        <fieldset class="mb-4">
+          {!!this.owners.length && <legend>Owners</legend>}
           {this.owners.map((_owner: BusinessOwner, index: number) => {
             return (
-              <div class="row gx-2 gy-2">
+              <div class="row gx-2 gy-2 mb-4">
                 <div class="col-12">
-                  <form-control-text
-                    name="name"
-                    label="Full Name"
-                    defaultValue={this.defaultValues[index]?.name}
-                    error={this.errors[index]?.name}
-                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
-                  />
+                  <div class="row mb-3">
+                    <div class="col">
+                      <form-control-text
+                        name="name"
+                        label="Full Name"
+                        defaultValue={this.defaultValues[index]?.name}
+                        error={this.errors[index]?.name}
+                        inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                        class="form-control"
+                      />
+                    </div>
+                    <div class="col">
+                      <form-control-text
+                        name="title"
+                        label="Title"
+                        defaultValue={this.defaultValues[index]?.title}
+                        error={this.errors[index]?.title}
+                        inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                        class="form-control"
+                      />
+                    </div>
+                  </div>
 
-                  <form-control-text
-                    name="title"
-                    label="Title"
-                    defaultValue={this.defaultValues[index]?.title}
-                    error={this.errors[index]?.title}
-                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
-                  />
+                  <div class="row mb-3">
+                    <div class="col">
+                      <form-control-text
+                        name="email"
+                        label="Email"
+                        defaultValue={this.defaultValues[index]?.email}
+                        error={this.errors[index]?.email}
+                        inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                        class="form-control"
+                      />
+                    </div>
+                    <div class="col">
+                      <form-control-text
+                        name="phone"
+                        label="Phone"
+                        defaultValue={this.defaultValues[index]?.phone}
+                        error={this.errors[index]?.phone}
+                        inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                        class="form-control"
+                      />
+                    </div>
+                  </div>
 
-                  <form-control-text
-                    name="email"
-                    label="Email"
-                    defaultValue={this.defaultValues[index]?.email}
-                    error={this.errors[index]?.email}
-                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
-                  />
+                  <div class="row mb-3">
+                    <div class="col-4">
+                      <form-control-text
+                        name="dob_day"
+                        label="Date of Birth - Day"
+                        defaultValue={this.defaultValues[index]?.dob_day}
+                        error={this.errors[index]?.dob_day}
+                        inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                        class="form-control"
+                      />
+                    </div>
+                    <div class="col-4">
+                      <form-control-text
+                        name="dob_month"
+                        label="Date of Birth - Month"
+                        defaultValue={this.defaultValues[index]?.dob_month}
+                        error={this.errors[index]?.dob_month}
+                        inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                        class="form-control"
+                      />
+                    </div>
+                    <div class="col-4">
+                      <form-control-text
+                        name="dob_year"
+                        label="Date of Birth - Year"
+                        defaultValue={this.defaultValues[index]?.dob_year}
+                        error={this.errors[index]?.dob_year}
+                        inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                        class="form-control"
+                      />
+                    </div>
+                  </div>
 
-                  <form-control-text
-                    name="phone"
-                    label="Phone"
-                    defaultValue={this.defaultValues[index]?.phone}
-                    error={this.errors[index]?.phone}
-                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
-                  />
-                </div>
-                <div class="col-4">
-                  <form-control-text
-                    name="dob_day"
-                    label="Date of Birth - Day"
-                    defaultValue={this.defaultValues[index]?.dob_day}
-                    error={this.errors[index]?.dob_day}
-                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
-                  />
-                </div>
+                  <div class="mb-3">
+                    <form-control-text
+                      name="identification_number"
+                      label="Identification Number"
+                      defaultValue={this.defaultValues[index]?.identification_number}
+                      error={this.errors[index]?.identification_number}
+                      inputHandler={(name, value) => this.inputHandler(name, value, index)}
+                      class="form-control"
+                    />
+                  </div>
 
-                <div class="col-4">
-                  <form-control-text
-                    name="dob_month"
-                    label="Date of Birth - Month"
-                    defaultValue={this.defaultValues[index]?.dob_month}
-                    error={this.errors[index]?.dob_month}
-                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
-                  />
-                </div>
+                  <legend class="mb-3">Owner Address:</legend>
 
-                <div class="col-4">
-                  <form-control-text
-                    name="dob_year"
-                    label="Date of Birth - Year"
-                    defaultValue={this.defaultValues[index]?.dob_year}
-                    error={this.errors[index]?.dob_year}
-                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
-                  />
-                </div>
-                <div>
-                  <form-control-text
-                    name="identification_number"
-                    label="Identification Number"
-                    defaultValue={this.defaultValues[index]?.identification_number}
-                    error={this.errors[index]?.identification_number}
-                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
-                  />
+                  <div class="row mb-3">
+                    <div class="col-6">
+                      <form-control-text
+                        name="line1"
+                        label="Street Address"
+                        defaultValue={this.defaultValues[index]?.address?.line1}
+                        error={this.errors[index]?.address?.line1}
+                        inputHandler={(_name, value) => {
+                          this.owners[index].address.line1 = value;
+                          this.owners = [...this.owners];
+                        }}
+                        class="form-control"
+                      />
+                    </div>
+                    <div class="col-6">
+                      <form-control-text
+                        name="city"
+                        label="City"
+                        defaultValue={this.defaultValues[index]?.address?.city}
+                        error={this.errors[index]?.address?.city}
+                        inputHandler={(_name, value) => {
+                          this.owners[index].address.city = value;
+                          this.owners = [...this.owners];
+                        }}
+                        class="form-control"
+                      />
+                    </div>
+                  </div>
 
-                  {/* Assuming address is a single line text input for simplicity */}
-                  <form-control-text
-                    name="address"
-                    label="Address"
-                    defaultValue={this.defaultValues[index]?.address}
-                    error={this.errors[index]?.address}
-                    inputHandler={(name, value) => this.inputHandler(name, value, index)}
-                  />
-
-                  <button type="button" class="btn" onClick={event => this.removeOwner(event, index)}>
+                  <div class="row mb-3">
+                    <div class="col-6">
+                      <form-control-text
+                        name="state"
+                        label="State"
+                        defaultValue={this.defaultValues[index]?.address?.state}
+                        error={this.errors[index]?.address?.state}
+                        inputHandler={(_name, value) => {
+                          this.owners[index].address.state = value;
+                          this.owners = [...this.owners];
+                        }}
+                        class="form-control"
+                      />
+                    </div>
+                    <div class="col-6">
+                      <form-control-text
+                        name="postal_code"
+                        label="Postal Code"
+                        defaultValue={this.defaultValues[index]?.address?.postal_code}
+                        error={this.errors[index]?.address?.postal_code}
+                        inputHandler={(_name, value) => {
+                          this.owners[index].address.postal_code = value;
+                          this.owners = [...this.owners];
+                        }}
+                        class="form-control"
+                      />
+                    </div>
+                  </div>
+                  <button type="button" class="btn btn-danger mt-3" onClick={event => this.removeOwner(event, index)}>
                     - Remove owner
                   </button>
                 </div>
               </div>
             );
           })}
-          <button type="button" class="btn" onClick={event => this.addOwner(event)}>
-            + Add an owner
-          </button>
+          <div class="row gy-3">
+            <div class="col-12">
+              <button type="button" class="btn btn-primary" onClick={event => this.addOwner(event)}>
+                + Add an owner
+              </button>
+            </div>
+          </div>
         </fieldset>
       </Host>
     );
