2929d1b10dae09e24e0c03d6d68b3d1bc1b7557e
Jake Merringer <jakemerringer@gmail.com>
2023-10-06 10:31:20 -0500

    very basic form stepping

--- stencil-library/src/components/business-form/business-form-stepped.tsx
+++ stencil-library/src/components/business-form/business-form-stepped.tsx
@@ -4,6 +4,16 @@ import businessFormSchema from './business-form-schema';
 import { Api } from '../../api';
 import { parseForPatching } from './helpers';
 
+
+const componentStepMapping = {
+  0: (formController) => <justifi-business-generic-info formController={formController} />,
+  1: (formController) => <justifi-legal-address-form formController={formController} />,
+  2: (formController) => <justifi-additional-questions formController={formController} />,
+  3: (formController) => <justifi-business-representative formController={formController} />,
+  4: (formController) => <justifi-business-owners formController={formController} />
+};
+
+
 /**
  * @exportedPart label: Label for inputs
  * @exportedPart input: The input fields
@@ -17,6 +27,7 @@ export class BusinessFormStepped {
   @Prop() authToken: string;
   @Prop() businessId?: string;
   @State() isLoading: boolean = false;
+  @State() currentStep: number = 0;
 
   private formController: FormController;
   private api: any;
@@ -96,44 +107,34 @@ export class BusinessFormStepped {
     return (
       <Host exportparts="label,input,input-invalid">
         <h1>Business Information</h1>
-        <form onSubmit={event => this.validateAndSubmit(event)}>
-          <div class="row gy-6 gap-3">
-            <div class="col-12">
-              <justifi-business-generic-info
-                formController={this.formController}
-              />
-            </div>
-            <div class="col-12">
-              <justifi-legal-address-form
-                formController={this.formController}
-                legend="Legal Address"
-              />
-            </div>
-            <div class="col-12">
-              <justifi-additional-questions
-                formController={this.formController}
-              />
-            </div>
-            <div class="col-12">
-              <justifi-business-representative
-                formController={this.formController}
-              />
-            </div>
-            <div class="col-12">
-              <justifi-business-owners
-                isEditing={!!this.businessId}
-                formController={this.formController}
-              />
-            </div>
-            <div class="col-12 d-flex flex-row-reverse">
-              <button
-                type="submit"
-                class="btn btn-primary jfi-submit-button"
-                disabled={!this.authToken || this.isLoading}
-              >
-                {this.isLoading ? 'Loading...' : 'Submit'}
-              </button>
-            </div>
+        <form onSubmit={this.validateAndSubmit}>
+          {componentStepMapping[this.currentStep](this.formController)}
+          <div class="buttons">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              onClick={() => {
+                if (this.currentStep > 0) {
+                  this.currentStep--;
+                }
+              }}
+            >
+              Previous
+            </button>
+            <button
+              type="button"
+              class="btn btn-secondary"
+              onClick={() => {
+                if (this.currentStep < 4) {
+                  this.currentStep++;
+                }
+              }}
+            >
+              Next
+            </button>
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
           </div>
         </form>
       </Host>
