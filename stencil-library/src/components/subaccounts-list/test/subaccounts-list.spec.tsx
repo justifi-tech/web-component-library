20aa69b7d27eb16956dbb5717a51215a4a703805
Colgan Meanor <colganmeanor7@gmail.com>
2023-11-01 16:40:34 -0500

    Update test spec file for subaccounts list

--- stencil-library/src/components/subaccounts-list/test/subaccounts-list.spec.tsx
+++ stencil-library/src/components/subaccounts-list/test/subaccounts-list.spec.tsx
@@ -5,14 +5,38 @@ describe('subaccounts-list', () => {
   it('renders', async () => {
     const page = await newSpecPage({
       components: [SubaccountsList],
-      html: `<subaccounts-list></subaccounts-list>`,
+      html: `<justifi-subaccounts-list></justifi-subaccounts-list>`,
     });
+
     expect(page.root).toEqualHtml(`
-      <subaccounts-list>
+      <justifi-subaccounts-list>
         <mock:shadow-root>
-          <slot></slot>
+          <justifi-table error-message="Can not fetch any data without an AccountID and an AuthToken">
         </mock:shadow-root>
-      </subaccounts-list>
+      </justifi-subaccounts-list>
     `);
   });
+
+  it('stops loading', async () => {
+    const page = await newSpecPage({
+      components: [SubaccountsList],
+      html: `<justifi-subaccounts-list></justifi-subaccounts-list>`,
+    });
+
+    const loading = page.root.__shadowRoot.querySelector('.loading-state');
+
+    expect(loading).toEqual(null);
+  });
+
+  it('renders an error when not given proper auth data', async () => {
+    const page = await newSpecPage({
+      components: [SubaccountsList],
+      html: `<justifi-subaccounts-list></justifi-subaccounts-list>`,
+    });
+
+    const error = page.root.__shadowRoot.querySelector('.error-state');
+
+    expect(error).toBeDefined();
+  });
+
 });
