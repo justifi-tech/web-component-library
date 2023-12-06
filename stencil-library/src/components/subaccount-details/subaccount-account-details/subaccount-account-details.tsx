cbdb75eac4d93bbf4ea4b33e4f48d2049c8ca233
Colgan Meanor <colganmeanor7@gmail.com>
2023-12-06 16:12:36 -0600

    Generate account details component based on mockup

---
+++ stencil-library/src/components/subaccount-details/subaccount-account-details/subaccount-account-details.tsx
@@ -0,0 +1,32 @@
+import { Component, Host, Prop, h } from '@stencil/core';
+import { IOnboardingData, SubAccount } from '../../../api/SubAccount';
+import { DetailItem, DetailSection } from '../../details/utils';
+
+@Component({
+  tag: 'subaccount-account-details',
+  styleUrl: 'subaccount-account-details.scss',
+  shadow: true,
+})
+export class SubaccountAccountDetails {
+  @Prop() subaccount: SubAccount
+  @Prop() onboardingData: IOnboardingData
+
+  render() {
+    return (
+      <Host>
+        <DetailSection sectionTitle='Account Details'>
+          <div class="row gy-3">
+            <div class="col-12 col-md-6">
+              <DetailItem title='Name' value={this.subaccount?.name} />
+              <DetailItem
+                title='Terms of Service Accepted'
+                value={this.onboardingData?.payload?.terms_and_conditions.accepted.toString()}
+                />
+              <DetailItem title='Business Entity' value={'something here'} />
+            </div>
+          </div>
+        </DetailSection>
+      </Host>
+    );
+  }
+}
