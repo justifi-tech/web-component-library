72ed84204fae00dad5145509a869ebf831d9d64f
Jake Merringer <jakemerringer@gmail.com>
2023-12-27 12:56:51 -0600

    Generic table message

--- stencil-library/src/components/table/table.tsx
+++ stencil-library/src/components/table/table.tsx
@@ -4,7 +4,7 @@ import { ExtendedPagingDefaults, ExtendedPagingInfo } from '../../api/Pagination
 export interface TableProps {
   loading: boolean;
   errorMessage: string;
-  columnData: (string|string[])[];
+  columnData: (string | string[])[];
   rowData: any[];
   paging: ExtendedPagingInfo;
   params: any
@@ -32,7 +32,7 @@ export class Table {
 
   emptyState = () => (
     <tr>
-      <td class="empty-state" part="empty-state" colSpan={this.columnData?.length} style={{ textAlign: 'center' }}>No payments to show</td>
+      <td class="empty-state" part="empty-state" colSpan={this.columnData?.length} style={{ textAlign: 'center' }}>No results</td>
     </tr>
   );
 
@@ -63,59 +63,59 @@ export class Table {
         previous-button-text,next-button-text
       ">
         {this.columnData ?
-        <table class="table table-hover">
-          <thead class="table-head sticky-top" part="table-head">
-            <tr class="table-light" part='table-head-row'>
-              {
-                this.columnData?.map((column) =>
-                  <th part="table-head-cell" scope="col" title={Array.isArray(column) ? column[1] : ''}>
-                    {!Array.isArray(column) ? column : column[0]}
-                  </th>
-                )
-              }
-            </tr>
-          </thead>
-          <tbody class="table-body" part='table-body'>
-            {
-              this.loading ? this.loadingState() :
-              this.errorMessage ? this.errorState() :
-              this.showEmptyState() ? this.emptyState() :
-              this.rowData?.map((data, index) => {
-                return (
-                <tr
-                  data-row-entity-id={this.entityId[index]}
-                  onClick={e => this.rowClickHandler ? this.rowClickHandler(e) : null}
-                  part={`table-row${index%2 ? ' table-row-even' : ' table-row-odd'}`}
-                >
-                  {
-                    data.map((dataEntry: any) =>
-                      { return (
-                          !dataEntry?.type
-                          ? <td part="table-cell">{dataEntry}</td>
-                          : <th scope="row" part="table-cell">
-                              <td part="table-cell" innerHTML={dataEntry.value}></td>
-                            </th>
-                        )
-                      }
-                    )
-                  }
-                </tr>
-                )
-              })
-            }
-          </tbody>
-          {this.paging &&
-            <tfoot class="sticky-bottom">
-              <tr class="table-light align-middle">
-                <td part="pagination-bar" colSpan={this.columnData?.length}>
-                  <pagination-menu paging={this.paging} params={this.params} />
-                </td>
+          <table class="table table-hover">
+            <thead class="table-head sticky-top" part="table-head">
+              <tr class="table-light" part='table-head-row'>
+                {
+                  this.columnData?.map((column) =>
+                    <th part="table-head-cell" scope="col" title={Array.isArray(column) ? column[1] : ''}>
+                      {!Array.isArray(column) ? column : column[0]}
+                    </th>
+                  )
+                }
               </tr>
-            </tfoot>
-          }
-        </table>
-        :
-        <div data-test-id="empty-error-state">Column data is required</div>}
+            </thead>
+            <tbody class="table-body" part='table-body'>
+              {
+                this.loading ? this.loadingState() :
+                  this.errorMessage ? this.errorState() :
+                    this.showEmptyState() ? this.emptyState() :
+                      this.rowData?.map((data, index) => {
+                        return (
+                          <tr
+                            data-row-entity-id={this.entityId[index]}
+                            onClick={e => this.rowClickHandler ? this.rowClickHandler(e) : null}
+                            part={`table-row${index % 2 ? ' table-row-even' : ' table-row-odd'}`}
+                          >
+                            {
+                              data.map((dataEntry: any) => {
+                                return (
+                                  !dataEntry?.type
+                                    ? <td part="table-cell">{dataEntry}</td>
+                                    : <th scope="row" part="table-cell">
+                                      <td part="table-cell" innerHTML={dataEntry.value}></td>
+                                    </th>
+                                )
+                              }
+                              )
+                            }
+                          </tr>
+                        )
+                      })
+              }
+            </tbody>
+            {this.paging &&
+              <tfoot class="sticky-bottom">
+                <tr class="table-light align-middle">
+                  <td part="pagination-bar" colSpan={this.columnData?.length}>
+                    <pagination-menu paging={this.paging} params={this.params} />
+                  </td>
+                </tr>
+              </tfoot>
+            }
+          </table>
+          :
+          <div data-test-id="empty-error-state">Column data is required</div>}
       </Host>
     )
   }
