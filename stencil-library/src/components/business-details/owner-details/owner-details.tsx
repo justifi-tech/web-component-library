a6539b8daf9fdbc3cf75b6234db02f86103543fd
TiagoFuelber <tfuelber@gmail.com>
2024-01-05 18:14:53 -0300

    refact: display table on DetailsSection
    * Turns DetailsSection to display table
    * Remove the Owner Address DetailsSection

--- stencil-library/src/components/business-details/owner-details/owner-details.tsx
+++ stencil-library/src/components/business-details/owner-details/owner-details.tsx
@@ -25,50 +25,44 @@ export class OwnerDetails {
         {!!this?.owners.length ? (
           this.owners.map(owner => (
             <div class="row">
-              <div class="col-12 col-md-6">
-                <DetailSection sectionTitle={`${owner?.name}'s Details`}>
-                  <DetailItem title="Name" value={owner?.name} />
-                  <DetailItem title="Title" value={owner?.title} />
-                  <DetailItem title="Email" value={owner?.email} />
-                  <DetailItem title="Phone" value={owner?.phone} />
-                  <DetailItem
-                    title="Date of Birth"
-                    value={formatMediumDate(
-                      new Date(
-                        Number(owner?.dob_day),
-                        Number(owner?.dob_month),
-                        Number(owner?.dob_year),
-                      ),
-                    )}
-                  />
+              <DetailSection sectionTitle={`Owner ${owner?.name}'s Details`}>
+                <DetailItem title="Name" value={owner?.name} />
+                <DetailItem title="Title" value={owner?.title} />
+                <DetailItem title="Email" value={owner?.email} />
+                <DetailItem title="Phone" value={owner?.phone} />
+                <DetailItem
+                  title="Date of Birth"
+                  value={formatMediumDate(
+                    new Date(
+                      Number(owner?.dob_day),
+                      Number(owner?.dob_month),
+                      Number(owner?.dob_year),
+                    ),
+                  )}
+                />
 
-                  <DetailItem
-                    title="Identification Number"
-                    value={`********${owner?.ssn_last4}`}
-                  />
-                </DetailSection>
-              </div>
-              <div class="col-12 col-md-6">
-                <DetailSection sectionTitle={`${owner?.name}'s Address`}>
-                  <DetailItem
-                    title="Line 1"
-                    value={owner?.address?.line1}
-                  />
-                  <DetailItem
-                    title="Line 2"
-                    value={owner?.address?.line2}
-                  />
-                  <DetailItem
-                    title="City"
-                    value={owner?.address?.city}
-                  />
-                  <DetailItem title="State" value={owner?.address?.state} />
-                  <DetailItem
-                    title="Zip"
-                    value={owner?.address?.postal_code}
-                  />
-                </DetailSection>
-              </div>
+                <DetailItem
+                  title="Identification Number"
+                  value={`********${owner?.ssn_last4}`}
+                />
+                <DetailItem
+                  title="Line 1"
+                  value={owner?.address?.line1}
+                />
+                <DetailItem
+                  title="Line 2"
+                  value={owner?.address?.line2}
+                />
+                <DetailItem
+                  title="City"
+                  value={owner?.address?.city}
+                />
+                <DetailItem title="State" value={owner?.address?.state} />
+                <DetailItem
+                  title="Zip"
+                  value={owner?.address?.postal_code}
+                />
+              </DetailSection>
             </div>))
         ) : (
           <DetailItem title="No owners" value="" />
