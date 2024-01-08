f0dc73aa57b6d3497fd445a0c66ae4aba3a8cd46
Jake Merringer <jakemerringer@gmail.com>
2024-01-08 15:20:26 -0600

    remove storybook-static from checked in files

--- apps/docs/stories/payments-list.stories.tsx
+++ apps/docs/stories/payments-list.stories.tsx
@@ -1,38 +1,10 @@
-import type { Meta, StoryObj } from "@storybook/react";
 import { JustifiBankAccountForm } from '@justifi/webcomponents';
 
-const meta: Meta<typeof JustifiBankAccountForm> = {
-  component: JustifiBankAccountForm,
-  argTypes: {
-    type: {
-      control: { type: "radio" },
-      options: ["button", "submit", "reset"],
-    },
-  },
-};
+const JustifiBankAccountFormStory = () => (
+  <JustifiBankAccountForm
+    clientId="123456789"
+    clientSecret="123456789"
+  />
+);
 
-export default meta;
-
-type Story = StoryObj<typeof Button>;
-
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/react/api/csf
- * to learn how to use render functions.
- */
-export const Primary: Story = {
-  render: (props) => (
-    <justifi-payments-list></justifi-payments-list>
-  ),
-  name: "Button",
-  args: {
-    children: "Hello",
-    type: "button",
-    style: {
-      color: "blue",
-      border: "1px solid gray",
-      padding: 10,
-      borderRadius: 10,
-    },
-  },
-};
+export default JustifiBankAccountFormStory;
