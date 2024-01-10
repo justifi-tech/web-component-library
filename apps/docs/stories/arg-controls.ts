a1493262ba404baddc5a8c3e025b1609030cda5d
Jake Merringer <jakemerringer@gmail.com>
2024-01-09 23:13:19 -0600

    add arg types and shared arg type controls

---
+++ apps/docs/stories/arg-controls.ts
@@ -0,0 +1,17 @@
+export const accountId = {
+  type: 'string',
+  description: 'Account ID',
+  defaultValue: '1234567890',
+  control: {
+    type: 'text',
+  }
+}
+
+export const authToken = {
+  type: 'string',
+  description: 'Auth Token',
+  defaultValue: '1234567890',
+  control: {
+    type: 'text',
+  }
+}
--- apps/docs/stories/components/payments-list.stories.tsx
+++ apps/docs/stories/components/payments-list.stories.tsx
@@ -1,27 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/web-components';
+import { accountId, authToken } from '../arg-controls';
 import '@justifi/webcomponents/dist/module/justifi-payments-list';
 
 const meta: Meta = {
   title: 'Components/PaymentsList',
   component: 'justifi-payments-list',
   argTypes: {
-    accountId: {
-      type: 'string',
-      description: 'Account ID',
-      defaultValue: '1234567890',
-      control: {
-        type: 'text',
-      },
-    },
-    authToken: {
-      type: 'string',
-      description: 'Auth Token',
-      defaultValue: '1234567890',
-      control: {
-        type: 'text',
-      },
-    },
-  },
+    accountId,
+    authToken
+  }
 };
 
 export default meta;
