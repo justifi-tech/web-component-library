0e3c0770789834372a83cf390516f67b1c7e2ac9
Jake Merringer <jakemerringer@gmail.com>
2024-01-10 13:21:56 -0600

    PaymentsList styled example

--- apps/docs/stories/components/payments-list.stories.tsx
+++ apps/docs/stories/components/payments-list.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/web-components';
 import { withActions } from '@storybook/addon-actions/decorator';
 import StoryBaseArgs from '../shared/base-args';
+import { renderComponentWithStyles } from '../shared/utils';
 import '@justifi/webcomponents/dist/module/justifi-payments-list';
 
 const storyBaseArgs = new StoryBaseArgs(['account-id', 'auth-token']);
@@ -19,12 +20,49 @@ const meta: Meta = {
       handles: ['payment-row-clicked'],
     },
   },
-  decorators: [withActions],
+  decorators: [withActions], // https://github.com/storybookjs/storybook/issues/22384
 };
 
 export default meta;
 
 type Story = StoryObj;
 
-export const Basic: Story = {
+export const Basic: Story = {};
+export const Styled: Story = {
+  args: {
+    style: {
+      'justifi-payments-list::part(table-head-cell)': {
+        'background-color': '#F4F4F6'
+      },
+      'justifi-payments-list::part(pagination-bar)': {
+        'background-color': '#F4F4F6'
+      },
+      'justifi-payments-list::part(arrow)': {
+        '--bs-btn-disabled-bg': '#212529',
+        '--bs-btn-disabled-border-color': '#212529',
+        '--bs-btn-bg': '#212529',
+        '--bs-btn-border-color': '#212529',
+        '--bs-btn-hover-bg': '#fccc32',
+        '--bs-btn-hover-border-color': '#fccc32',
+      },
+      'justifi-payments-list::part(error-state)': {
+        'color': 'red',
+        'background-color': '#EEEEF5'
+      },
+      'justifi-payments-list::part(loading-state-cell)': {
+        'background-color': '#EEEEF5'
+      },
+      'justifi-payments-list::part(table-row)': {
+        'background-color': '#EEEEF5'
+      },
+      'justifi-payments-list::part(table-row-even)': {
+        'background-color': '#F4F4F6'
+      }
+    }
+  },
+  render: ({ items, ...args }) => renderComponentWithStyles(
+    () => <justifi-payments-list></justifi-payments-list>,
+    args.style
+  )
 };
+
--- apps/docs/stories/shared/utils.tsx
+++ apps/docs/stories/shared/utils.tsx
@@ -11,6 +11,28 @@ const filterDocsByTag = (tag: string) => {
   return webcomponentsDocsJson.components.filter(comp => tag === comp.tag)[0];
 };
 
+const renderComponentWithStyles = (componentRenderFunc: () => HTMLElement, stylesArg: any) => {
+  return (
+    <>
+      <style>
+        {Object.keys(stylesArg).map((style) => {
+          const selector = style;
+          const styles = stylesArg[style];
+          return (
+            `
+            ${selector} {
+              ${Object.keys(styles).map((style) => {
+              return `${style}: ${styles[style]};`;
+            }).join('')}
+          }`
+          );
+        })}
+      </style>
+      {componentRenderFunc()}
+    </>
+  );
+}
+
 const ExportedParts = ({ tags, component, compact }: { tags: string[]; component: string, compact: boolean }) => {
   return (
     <ul>
@@ -62,6 +84,7 @@ const SummaryElement = (props: { title: string, children: any }) => {
 
 export {
   extractVersionFromPackage,
+  renderComponentWithStyles,
   filterDocsByTag,
   ExportedParts,
   SummaryElement
