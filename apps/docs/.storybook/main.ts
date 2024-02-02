1b4096efbd4574277c452169e398326551f38a92
Tiago Fuelber <tfuelber@gmail.com>
2024-02-02 11:46:31 -0300

    Update main.ts

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -1,18 +1,28 @@
 import { mergeConfig } from 'vite';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path'; // Import path module
 
 const config = {
   framework: '@storybook/web-components-vite',
-  stories: ["../stories/**/*.stories.@(mdx|ts|tsx)"],
+  stories: ['../stories/**/*.stories.@(mdx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-docs'
+    '@storybook/addon-docs',
   ],
   docs: {
     autodocs: true,
   },
   async viteFinal(config, { configType }) {
+    // Add an alias for @justifi/webcomponents to ensure proper resolution
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        '@justifi/webcomponents': path.resolve(__dirname, '../node_modules/@justifi/webcomponents'),
+      },
+    };
+
     return mergeConfig(config, {
       plugins: [react()],
     });
