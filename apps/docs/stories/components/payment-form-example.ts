f5df98f7a6bee8b8449203dc1217032f446739c0
Nick Halm <nick.halm@justifi.tech>
2024-02-21 09:39:27 -0600

    Updatd the example for the payments form

--- apps/docs/stories/components/payment-form-example.ts
+++ apps/docs/stories/components/payment-form-example.ts
@@ -1,12 +1,26 @@
-import { codeExampleHead } from '../utils';
+import { codeExampleHead } from "../utils";
 
 export default `<!DOCTYPE html>
 <html dir="ltr" lang="en">
 
-${codeExampleHead('justifi-payment-form')}
+${codeExampleHead("justifi-payment-form")}
+
+/*
+* Props definition:
+*
+* client-id is your JustiFi API Credential Client Id
+* account-id is the merchant subaccount that you intend to tokenize the payment on.
+*/
 
 <body>
-  <justifi-payment-form bank-account="true" card="true"></justifi-payment-form>
+  <justifi-payment-form
+    client-id="test_df97f04afebc3c018de30df3562d7cdd"
+    account-id="acc_5Et9iXrSSAZR2KSouQGAWi"
+    bank-account="true"
+    card="true"
+    email="test@email.com"
+    submit-button-text="Something you want here"
+  ></justifi-payment-form>
 </body>
 
 <script>
