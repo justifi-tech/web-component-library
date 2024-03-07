ce6127fff0970116963725302d1d6521f56b220b
Jake Merringer <jakemerringer@gmail.com>
2024-03-07 17:11:32 -0600

    Flip checkout service args

--- packages/webcomponents/src/api/services/checkout.service.ts
+++ packages/webcomponents/src/api/services/checkout.service.ts
@@ -3,17 +3,17 @@ import { config } from '../../../config';
 
 export interface ICheckoutService {
   fetchCheckout(
-    paymentId: string,
-    authToken: string
+    authToken: string,
+    checkoutId: string,
   ): Promise<IApiResponse<ICheckout>>;
 }
 
 export class CheckoutService implements ICheckoutService {
   async fetchCheckout(
-    paymentId: string,
-    authToken: string
+    authToken: string,
+    checkoutId: string,
   ): Promise<IApiResponse<ICheckout>> {
-    const endpoint = `payments/${paymentId}`;
+    const endpoint = `checkout/${checkoutId}`;
     return Api(authToken, config.proxyApiOrigin).get(endpoint);
   }
 }
