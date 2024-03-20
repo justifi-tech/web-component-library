5b1bfae13c0c8482466ca0a75ab4a1833c1f91bb
Colgan Meanor <colganmeanor7@gmail.com>
2024-03-20 16:21:41 -0500

    Refactor business-owners-form-step to make room for new identity form component

--- packages/webcomponents/src/components/business-forms/business-form-stepped/business-owners/business-owners-form-step.tsx
+++ packages/webcomponents/src/components/business-forms/business-form-stepped/business-owners/business-owners-form-step.tsx
@@ -1,20 +1,8 @@
-import { Component, Host, h, Prop, State } from '@stencil/core';
-import { FormController } from '../../../form/form';
-import { PHONE_MASKS } from '../../../../utils/form-input-masks';
-
-class BusinessOwner {
-  name: string = '';
-  title: string = '';
-  email: string = '';
-  phone: string = '';
-  dob_day: string = '';
-  dob_month: string = '';
-  dob_year: string = '';
-  identification_number: string = '';
-  is_owner: boolean = true;
-  metadata: any = {};
-  address: any = {};
-}
+import { Component, Host, h, Prop, State, Event, EventEmitter } from '@stencil/core';
+import { IBusiness } from '../../../../api/Business';
+import { BusinessFormServerErrorEvent, BusinessFormServerErrors } from '../../utils/business-form-types';
+import { Api, IApiResponse } from '../../../../api';
+import { config } from '../../../../../config';
 
 /**
  * @exportedPart label: Label for inputs
@@ -27,294 +15,70 @@ class BusinessOwner {
   shadow: false,
 })
 export class BusinessOwnersFormStep {
-  @Prop() formController: FormController;
-  @State() errors: any[] = [];
-  @State() owners: BusinessOwner[] = [];
+  @Prop() authToken: string;
+  @Prop() businessId: string;
+  @State() ownerIds: string[] = [];
+  @Event() formLoading: EventEmitter<boolean>;
+  @Event() serverError: EventEmitter<BusinessFormServerErrorEvent>;
 
-  get disabledState() {
-    return !!this.formController.getInitialValues().owners && !!this.formController.getInitialValues().owners.length;
+  private api: any;
+
+  get businessEndpoint() {
+    return `entities/business/${this.businessId}`
   }
 
-  constructor() {
-    this.inputHandler = this.inputHandler.bind(this);
-    this.addOwner = this.addOwner.bind(this);
-    this.removeOwner = this.removeOwner.bind(this);
+  private fetchData = async () => {
+    this.formLoading.emit(true);
+    try {
+      const response: IApiResponse<IBusiness> = await this.api.get(this.businessEndpoint);
+      this.ownerIds = response.data.owners.map((owner) => owner.id);
+    } catch (error) {
+      this.serverError.emit({ data: error, message: BusinessFormServerErrors.fetchData });
+    } finally {
+      this.formLoading.emit(false);
+    }
   }
 
-  componentDidLoad() {
-    this.formController.errors.subscribe(errors => {
-      this.errors = errors?.owners || [];
-    });
-    this.formController.values.subscribe(
-      values => (this.owners = values?.owners || []),
-    );
+  componentWillLoad() {
+    const missingAuthTokenMessage = 'Warning: Missing auth-token. The form will not be functional without it.';
+    const missingBusinessIdMessage = 'Warning: Missing business-id. The form requires an existing business-id to function.';
+    if (!this.authToken) console.error(missingAuthTokenMessage);
+    if (!this.businessId) console.error(missingBusinessIdMessage);
+
+    this.api = Api(this.authToken, config.proxyApiOrigin);
+    this.fetchData();
   }
 
-  inputHandler(name: string, value: string, index: number): void {
-    this.formController.setValues({
-      ...this.formController.values.getValue(),
-      owners: this.owners.map((owner, i) => {
-        if (i === index) {
-          return {
-            ...owner,
-            [name]: value,
-          };
-        }
-        return owner;
-      }),
-    });
-  }
+  // addOwner(event: MouseEvent): void {
+  //   event.preventDefault();
 
-  addressInputHandler(name: string, value: string, index: number): void {
-    this.formController.setValues({
-      ...this.formController.values.getValue(),
-      owners: this.owners.map((owner, i) => {
-        if (i === index) {
-          return {
-            ...owner,
-            address: {
-              ...owner.address,
-              [name]: value,
-            },
-          };
-        }
-        return owner;
-      }),
-    });
-  }
+  //   this.formController.setValues({
+  //     ...this.formController.values.getValue(),
+  //     owners: [...this.owners, { ...new BusinessOwner() }],
+  //   });
+  // }
 
-  addOwner(event: MouseEvent): void {
-    event.preventDefault();
-
-    this.formController.setValues({
-      ...this.formController.values.getValue(),
-      owners: [...this.owners, { ...new BusinessOwner() }],
-    });
-  }
-
-  removeOwner(event: MouseEvent, index: number): void {
-    event.preventDefault();
-    this.formController.setValues({
-      ...this.formController.values.getValue(),
-      owners: this.owners.filter((_owner, i) => i !== index),
-    });
-  }
+  // removeOwner(event: MouseEvent, index: number): void {
+  //   event.preventDefault();
+  //   this.formController.setValues({
+  //     ...this.formController.values.getValue(),
+  //     owners: this.owners.filter((_owner, i) => i !== index),
+  //   });
+  // }
 
   render() {
-    const ownersDefaultValue =
-      this.formController.values.getValue().owners || [];
       
     return (
       <Host exportparts="label,input,input-invalid">
-        <fieldset>
-          <legend>Owners</legend>
-          <hr />
-          {this.owners.map((_owner: BusinessOwner, index: number) => {
-            return (
-              <div class="row gx-2 gy-2 mb-4">
-                <div class="col-12">
-                  <div class="row mb-3">
-                    <div class="col-12 col-md-6">
-                      <form-control-text
-                        name="name"
-                        label="Full Name"
-                        defaultValue={ownersDefaultValue[index]?.name}
-                        error={this.errors[index]?.name}
-                        inputHandler={(name, value) =>
-                          this.inputHandler(name, value, index)
-                        }
-                        disabled={this.disabledState}
-                      />
-                    </div>
-                    <div class="col-12 col-md-6">
-                      <form-control-text
-                        name="title"
-                        label="Title"
-                        defaultValue={ownersDefaultValue[index]?.title}
-                        error={this.errors[index]?.title}
-                        inputHandler={(name, value) =>
-                          this.inputHandler(name, value, index)
-                        }
-                        disabled={this.disabledState}
-                      />
-                    </div>
-                  </div>
-
-                  <div class="row mb-3">
-                    <div class="col-12 col-md-6">
-                      <form-control-text
-                        name="email"
-                        label="Email"
-                        defaultValue={ownersDefaultValue[index]?.email}
-                        error={this.errors[index]?.email}
-                        inputHandler={(name, value) =>
-                          this.inputHandler(name, value, index)
-                        }
-                        disabled={this.disabledState}
-                      />
-                    </div>
-                    <div class="col-12 col-md-6">
-                      <form-control-number-masked
-                        name="phone"
-                        label="Phone"
-                        defaultValue={ownersDefaultValue[index]?.phone}
-                        error={this.errors[index]?.phone}
-                        inputHandler={(name, value) =>
-                          this.inputHandler(name, value, index)
-                        }
-                        mask={PHONE_MASKS.US}
-                        disabled={this.disabledState}
-                      />
-                    </div>
-                  </div>
-
-                  <div class="row mb-3">
-                    <div class="col-12 col-md-4">
-                      <form-control-datepart
-                        name="dob_day"
-                        label="Date of Birth - Day"
-                        defaultValue={ownersDefaultValue[index]?.dob_day}
-                        error={this.errors[index]?.dob_day}
-                        inputHandler={(name, value) =>
-                          this.inputHandler(name, value, index)
-                        }
-                        type="day"
-                        disabled={this.disabledState}
-                      />
-                    </div>
-                    <div class="col-12 col-md-4">
-                      <form-control-datepart
-                        name="dob_month"
-                        label="Date of Birth - Month"
-                        defaultValue={ownersDefaultValue[index]?.dob_month}
-                        error={this.errors[index]?.dob_month}
-                        inputHandler={(name, value) =>
-                          this.inputHandler(name, value, index)
-                        }
-                        type="month"
-                        disabled={this.disabledState}
-                      />
-                    </div>
-                    <div class="col-12 col-md-4">
-                      <form-control-datepart
-                        name="dob_year"
-                        label="Date of Birth - Year"
-                        defaultValue={ownersDefaultValue[index]?.dob_year}
-                        error={this.errors[index]?.dob_year}
-                        inputHandler={(name, value) =>
-                          this.inputHandler(name, value, index)
-                        }
-                        type="year"
-                        disabled={this.disabledState}
-                      />
-                    </div>
-                  </div>
-
-                  <div class="mb-3">
-                    <form-control-number
-                      name="identification_number"
-                      label="Identification Number"
-                      defaultValue={
-                        ownersDefaultValue[index]?.identification_number
-                      }
-                      error={this.errors[index]?.identification_number}
-                      inputHandler={(name, value) =>
-                        this.inputHandler(name, value, index)
-                      }
-                      disabled={this.disabledState}
-                    />
-                  </div>
-
-                  <div class="grid gap-3">
-                    <legend class="mb-3">Owner Address:</legend>
-
-                    <div class="row mb-3">
-                      <div class="col-12 col-md-6">
-                        <form-control-text
-                          name="line1"
-                          label="Street Address"
-                          defaultValue={
-                            ownersDefaultValue[index]?.address?.line1
-                          }
-                          error={this.errors[index]?.address?.line1}
-                          inputHandler={(name, value) =>
-                            this.addressInputHandler(name, value, index)
-                          }
-                          disabled={this.disabledState}
-                        />
-                      </div>
-                      <div class="col-12 col-md-6">
-                        <form-control-text
-                          name="city"
-                          label="City"
-                          defaultValue={
-                            ownersDefaultValue[index]?.address?.city
-                          }
-                          error={this.errors[index]?.address?.city}
-                          inputHandler={(name, value) =>
-                            this.addressInputHandler(name, value, index)
-                          }
-                          disabled={this.disabledState}
-                        />
-                      </div>
-                    </div>
-
-                    <div class="row mb-3">
-                      <div class="col-12 col-md-6">
-                        <form-control-text
-                          name="state"
-                          label="State"
-                          defaultValue={
-                            ownersDefaultValue[index]?.address?.state
-                          }
-                          error={this.errors[index]?.address?.state}
-                          inputHandler={(name, value) =>
-                            this.addressInputHandler(name, value, index)
-                          }
-                          disabled={this.disabledState}
-                        />
-                      </div>
-                      <div class="col-12 col-md-6">
-                        <form-control-number
-                          name="postal_code"
-                          label="Postal Code"
-                          defaultValue={
-                            ownersDefaultValue[index]?.address?.postal_code
-                          }
-                          error={this.errors[index]?.address?.postal_code}
-                          inputHandler={(name, value) =>
-                            this.addressInputHandler(name, value, index)
-                          }
-                          disabled={this.disabledState}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <button
-                    type="button"
-                    class="btn btn-outline-danger"
-                    onClick={event => this.removeOwner(event, index)}
-                    disabled={this.disabledState}
-                  >
-                    Remove owner
-                  </button>
-                </div>
-              </div>
-            )
-          })}
-          <div class="row gy-3">
-            <div class="col-12">
-              <button
-                type="button"
-                class="btn btn-outline-primary"
-                onClick={event => this.addOwner(event)}
-                disabled={this.disabledState}
-              >
-                Add an owner
-              </button>
-            </div>
-          </div>
-        </fieldset>
+       {this.ownerIds.map((ownerId) => {
+          return (
+            <justifi-owner-form 
+              authToken={this.authToken} 
+              businessId={this.businessId} 
+              ownerId={ownerId}
+            />
+          );
+       })}
       </Host>
     );
   }
