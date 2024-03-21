2699c910fb5e8bc47fb9001cc14287f93bc3c09a
Colgan Meanor <colganmeanor7@gmail.com>
2024-03-21 16:55:50 -0500

    Render owners form in business form and submit data individually or in bulk

--- packages/webcomponents/src/components/business-forms/business-form-stepped/business-owners/business-owners-form-step.tsx
+++ packages/webcomponents/src/components/business-forms/business-form-stepped/business-owners/business-owners-form-step.tsx
@@ -1,8 +1,9 @@
-import { Component, Host, h, Prop, State, Event, EventEmitter } from '@stencil/core';
+import { Component, Host, h, Prop, State, Event, EventEmitter, Method } from '@stencil/core';
 import { IBusiness } from '../../../../api/Business';
 import { BusinessFormServerErrorEvent, BusinessFormServerErrors } from '../../utils/business-form-types';
 import { Api, IApiResponse } from '../../../../api';
 import { config } from '../../../../../config';
+import { Owner } from '../../../../api/Identity';
 
 /**
  * @exportedPart label: Label for inputs
@@ -17,7 +18,7 @@ import { config } from '../../../../../config';
 export class BusinessOwnersFormStep {
   @Prop() authToken: string;
   @Prop() businessId: string;
-  @State() ownerIds: string[] = [];
+  @State() owners: Owner[] = [];
   @Event() formLoading: EventEmitter<boolean>;
   @Event() serverError: EventEmitter<BusinessFormServerErrorEvent>;
 
@@ -31,13 +32,20 @@ export class BusinessOwnersFormStep {
     this.formLoading.emit(true);
     try {
       const response: IApiResponse<IBusiness> = await this.api.get(this.businessEndpoint);
-      this.ownerIds = response.data.owners.map((owner) => owner.id);
+      this.owners = response.data.owners;
     } catch (error) {
       this.serverError.emit({ data: error, message: BusinessFormServerErrors.fetchData });
     } finally {
       this.formLoading.emit(false);
     }
   }
+
+
+  private ownerRef1: any;
+  private ownerRef2: any;
+  private ownerRef3: any;
+  // private ownerRef4: any;
+  private refs = [];
 
   componentWillLoad() {
     const missingAuthTokenMessage = 'Warning: Missing auth-token. The form will not be functional without it.';
@@ -47,6 +55,7 @@ export class BusinessOwnersFormStep {
 
     this.api = Api(this.authToken, config.proxyApiOrigin);
     this.fetchData();
+    this.refs = [this.ownerRef1, this.ownerRef2, this.ownerRef3];
   }
 
   // addOwner(event: MouseEvent): void {
@@ -66,19 +75,40 @@ export class BusinessOwnersFormStep {
   //   });
   // }
 
+  private submitAll = async () => {
+    const refSubmissions = this.refs.map(ref => ref.submit());
+    Promise.all(refSubmissions)
+  };
+
+
+  @Method()
+  async validateAndSubmit({ onSuccess }) {
+    const refValidations = this.refs.map(ref => ref.validate());
+    Promise.all(refValidations)
+      .then(values => {
+        if (values.every(value => value === true)) {
+          this.submitAll();
+          onSuccess();
+        }
+      });
+  };
+
   render() {
       
     return (
       <Host exportparts="label,input,input-invalid">
-       {this.ownerIds.map((ownerId) => {
-          return (
-            <justifi-owner-form 
-              authToken={this.authToken} 
-              businessId={this.businessId} 
-              ownerId={ownerId}
-            />
-          );
-       })}
+        <div class='col-12'>
+          {this.owners.map((owner, index) => {
+            return (
+              <justifi-owner-form 
+                authToken={this.authToken} 
+                businessId={this.businessId} 
+                ownerId={owner.id}
+                ref={(ref) => this.refs[index] = ref}
+              />
+            );
+          })}
+        </div>
       </Host>
     );
   }
--- packages/webcomponents/src/components/business-forms/owner-form/owner-form.tsx
+++ packages/webcomponents/src/components/business-forms/owner-form/owner-form.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Prop, State, Event, EventEmitter } from '@stencil/core';
+import { Component, Host, h, Prop, State, Event, EventEmitter, Method } from '@stencil/core';
 import { FormController } from '../../form/form';
 import { PHONE_MASKS } from '../../../utils/form-input-masks';
 import { Api, IApiResponse } from '../../../api';
@@ -21,7 +21,7 @@ export class BusinessOwnerForm {
   @State() isLoading: boolean = false;
   @State() formController: FormController;
   @State() errors: any = {};
-  @State() owner: any = {};
+  @State() owner: Owner = {};
   @Event({ bubbles: true }) submitted: EventEmitter<OwnerFormSubmitEvent>;
   @Event() serverError: EventEmitter<OwnerFormServerErrorEvent>;
 
@@ -116,10 +116,20 @@ export class BusinessOwnerForm {
       }
     });
   }
-  
-  private validateAndSubmit(event: any) {
+
+  @Method()
+  async validate() {
+    return this.formController.validate();
+  }
+
+  @Method()
+  async submit() {
+    this.sendData();
+  }
+
+  async validateAndSubmit(event: any) {
     event.preventDefault();
-    this.formController.validateAndSubmit(this.sendData);
+    return this.formController.validateAndSubmit(this.sendData);
   }
 
   render() {
@@ -132,109 +142,109 @@ export class BusinessOwnerForm {
         <form onSubmit={this.validateAndSubmit}>
           <fieldset>
             <legend>{this.formTitle}</legend>
-            <hr />
-            <div class="row gy-3">
-              <div class="col-12 col-md-8">
-                <form-control-text
-                  name="name"
-                  label="Full Name"
-                  defaultValue={ownerDefaultValue?.name}
-                  error={this.errors.name}
-                  inputHandler={this.inputHandler}
-                />
-              </div>
-              <div class="col-12 col-md-4">
-                <form-control-select
-                  name="title"
-                  label="Prefix"
-                  defaultValue={ownerDefaultValue?.title}
-                  options={[
-                    { label: 'Select Prefix', value: '' },
-                    { label: 'Mrs.', value: 'Mrs.' },
-                  ]}
-                  error={this.errors.title}
-                  inputHandler={this.inputHandler}
-                />
-              </div>
-              <div class="col-12 col-md-6">
-                <form-control-text
-                  name="email"
-                  label="Email Address"
-                  defaultValue={ownerDefaultValue?.email}
-                  error={this.errors.email}
-                  inputHandler={this.inputHandler}
-                />
-              </div>
-              <div class="col-12 col-md-6">
-                <form-control-number-masked
-                  name="phone"
-                  label="Phone Number"
-                  defaultValue={ownerDefaultValue?.phone}
-                  error={this.errors.phone}
-                  inputHandler={this.inputHandler}
-                  mask={PHONE_MASKS.US}
-                />
-              </div>
-              <div class="col-12">
-                <label part="label" class="form-label">
-                  Birth Date
-                </label>
-              </div>
-              <div class="col-12 col-md-4">
-                <form-control-datepart
-                  name="dob_day"
-                  label="Day"
-                  defaultValue={ownerDefaultValue?.dob_day}
-                  error={this.errors.dob_day}
-                  inputHandler={this.inputHandler}
-                  type="day"
-                />
-              </div>
-              <div class="col-12 col-md-4">
-                <form-control-datepart
-                  name="dob_month"
-                  label="Month"
-                  defaultValue={ownerDefaultValue?.dob_month}
-                  error={this.errors.dob_month}
-                  inputHandler={this.inputHandler}
-                  type="month"
-                />
-              </div>
-              <div class="col-12 col-md-4">
-                <form-control-datepart
-                  name="dob_year"
-                  label="Year"
-                  defaultValue={ownerDefaultValue?.dob_year}
-                  error={this.errors.dob_year}
-                  inputHandler={this.inputHandler}
-                  type="year"
-                />
-              </div>
-              <div class="col-12">
-                <form-control-number
-                  name="identification_number"
-                  label="EIN/SSN"
-                  defaultValue={ownerDefaultValue?.identification_number}
-                  error={this.errors.identification_number}
-                  inputHandler={this.inputHandler}
-                />
-              </div>
-              <div class="col-12">
-                <justifi-business-address-form
-                  errors={this.errors.address}
-                  defaultValues={ownerDefaultValue?.address}
-                  handleFormUpdate={values => this.onAddressFormUpdate(values)}
-                />
-              </div>
-              <div class="col-12 d-flex flex-row-reverse">
-                <button
-                  type="submit"
-                  class={`btn btn-primary jfi-submit-button${this.isLoading ? ' jfi-submit-button-loading' : ''}`}
-                  disabled={this.isLoading}>
-                  {this.isLoading ? LoadingSpinner() : this.submitButtonText}
-                </button>
-              </div>
-            </div>
+            <div class='row gx-2 gy-2 mb-4'>
+                  <div class="col-12 col-md-6">
+                    <form-control-text
+                      name="name"
+                      label="Full Name"
+                      defaultValue={ownerDefaultValue?.name}
+                      error={this.errors.name}
+                      inputHandler={this.inputHandler}
+                    />
+                  </div>
+                  <div class="col-12 col-md-4">
+                    <form-control-select
+                      name="title"
+                      label="Prefix"
+                      defaultValue={ownerDefaultValue?.title}
+                      options={[
+                        { label: 'Select Prefix', value: '' },
+                        { label: 'Mrs.', value: 'Mrs.' },
+                      ]}
+                      error={this.errors.title}
+                      inputHandler={this.inputHandler}
+                    />
+                  </div>
+                  <div class="col-12 col-md-6">
+                    <form-control-text
+                      name="email"
+                      label="Email Address"
+                      defaultValue={ownerDefaultValue?.email}
+                      error={this.errors.email}
+                      inputHandler={this.inputHandler}
+                    />
+                  </div>
+                  <div class="col-12 col-md-6">
+                    <form-control-number-masked
+                      name="phone"
+                      label="Phone Number"
+                      defaultValue={ownerDefaultValue?.phone}
+                      error={this.errors.phone}
+                      inputHandler={this.inputHandler}
+                      mask={PHONE_MASKS.US}
+                    />
+                  </div>
+                  <div class="col-12">
+                    <label part="label" class="form-label">
+                      Birth Date
+                    </label>
+                  </div>
+                  <div class="col-12 col-md-4">
+                    <form-control-datepart
+                      name="dob_day"
+                      label="Day"
+                      defaultValue={ownerDefaultValue?.dob_day}
+                      error={this.errors.dob_day}
+                      inputHandler={this.inputHandler}
+                      type="day"
+                    />
+                  </div>
+                  <div class="col-12 col-md-4">
+                    <form-control-datepart
+                      name="dob_month"
+                      label="Month"
+                      defaultValue={ownerDefaultValue?.dob_month}
+                      error={this.errors.dob_month}
+                      inputHandler={this.inputHandler}
+                      type="month"
+                    />
+                  </div>
+                  <div class="col-12 col-md-4">
+                    <form-control-datepart
+                      name="dob_year"
+                      label="Year"
+                      defaultValue={ownerDefaultValue?.dob_year}
+                      error={this.errors.dob_year}
+                      inputHandler={this.inputHandler}
+                      type="year"
+                    />
+                  </div>
+                  <div class="col-12">
+                    <form-control-number
+                      name="identification_number"
+                      label="EIN/SSN"
+                      defaultValue={ownerDefaultValue?.identification_number}
+                      error={this.errors.identification_number}
+                      inputHandler={this.inputHandler}
+                    />
+                  </div>
+                  <div class="col-12">
+                    <justifi-business-address-form
+                      errors={this.errors.address}
+                      defaultValues={ownerDefaultValue?.address}
+                      handleFormUpdate={values => this.onAddressFormUpdate(values)}
+                    />
+                  </div>
+                  <div class="d-flex gap-2">
+                    <button
+                      type="submit"
+                      class={`btn btn-primary jfi-submit-button${this.isLoading ? ' jfi-submit-button-loading' : ''}`}
+                      disabled={this.isLoading}>
+                      {this.isLoading ? LoadingSpinner() : this.submitButtonText}
+                    </button>
+                  </div>
+                </div>
+                <hr />
           </fieldset>
         </form>
       </Host>
