3472800ac4c0e42b03067888387e530c75da67aa
TiagoFuelber <tfuelber@gmail.com>
2024-03-26 11:03:42 -0300

    add tokenExpired event to payments list

--- packages/webcomponents/src/components/payments-list/test/payments-list.spec.tsx
+++ packages/webcomponents/src/components/payments-list/test/payments-list.spec.tsx
@@ -1,6 +1,9 @@
+import { h } from "@stencil/core";
 import { newSpecPage } from "@stencil/core/testing";
 import { PaymentsList } from "../payments-list";
 import { PaymentsListCore } from "../payments-list-core";
+import { Table } from "../../table/table";
+import { PaymentService } from "../../../api/services/payment.service";
 
 describe('payments-list', () => {
   it('renders an error message when accountId and authToken are not provided', async () => {
@@ -28,5 +31,29 @@ describe('payments-list', () => {
     });
     await page.waitForChanges();
     expect(page.root).toMatchSnapshot();
+  });
+
+  it('emit tokenExpired event when the authToken has expired', async () => {
+    const fetchPaymentsMock = jest.spyOn(PaymentService.prototype, 'fetchPayments' as keyof PaymentService);
+
+    fetchPaymentsMock.mockResolvedValue({
+      "error": {
+        "code": "not_authenticated",
+        "message": "Not Authenticated"
+      }
+    });
+
+    const spy = jest.fn();
+
+    const page = await newSpecPage({
+      components: [PaymentsList, PaymentsListCore, Table],
+      template: () => <justifi-payments-list account-id="abc" auth-token="abc" onTokenExpired={spy} />,
+    });
+
+    await page.waitForChanges();
+
+    expect(page.root).toMatchSnapshot();
+
+    expect(spy).toHaveBeenCalled();
   });
 });
