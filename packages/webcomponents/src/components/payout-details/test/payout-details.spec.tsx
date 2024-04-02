604b4396129e07aed5d9c8fe00f2241abf0f6f4c
Tiago Fuelber <tfuelber@gmail.com>
2024-04-02 11:06:54 -0300

    add tokenExpired event to payout details (#431)

--- packages/webcomponents/src/components/payout-details/test/payout-details.spec.tsx
+++ packages/webcomponents/src/components/payout-details/test/payout-details.spec.tsx
@@ -1,14 +1,38 @@
+import { h } from '@stencil/core';
 import { newSpecPage } from "@stencil/core/testing";
 import { PayoutDetails } from "../payout-details";
 import { PayoutDetailsCore } from "../payout-details-core";
+import { PayoutService } from '../../../api/services/payout.service';
 
 describe('payout-details', () => {
   it('renders error state when no payoutId or authToken', async () => {
     const page = await newSpecPage({
       components: [PayoutDetails, PayoutDetailsCore],
-      html: '<justifi-payout-details></justifi-payout-details>',
+      template: () => <justifi-payout-details />,
     });
     await page.waitForChanges();
     expect(page.root).toMatchSnapshot();
+  });
+
+  it('emits tokenExpired when passed an expired token', async () => {
+    const fetchPayoutMock = jest.spyOn(PayoutService.prototype, 'fetchPayout');
+
+    fetchPayoutMock.mockResolvedValue({
+      "error": {
+        "code": "not_authenticated",
+        "message": "Not Authenticated"
+      }
+    });
+
+    const eventSpy = jest.fn();
+
+    const page = await newSpecPage({
+      components: [PayoutDetails, PayoutDetailsCore],
+      template: () => <justifi-payout-details authToken='123' payoutId='123' onTokenExpired={eventSpy} />
+    })
+
+    await page.waitForChanges();
+
+    expect(eventSpy).toHaveBeenCalled();
   });
 });
