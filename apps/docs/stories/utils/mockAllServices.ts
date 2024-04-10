6ab039c5423df8fd37cc5f3027591fb0964941f3
Tiago Fuelber <tfuelber@gmail.com>
2024-04-10 10:29:23 -0300

    Mock all components (#444)
    * feat: mock the rest of the components
    
    * remove comment typo

--- apps/docs/stories/utils/mockAllServices.ts
+++ apps/docs/stories/utils/mockAllServices.ts
@@ -1,16 +1,45 @@
 import { createServer } from 'miragejs';
 import mockBusinessDetails from '../../../../mockData/mockBusinessDetails.json';
+import mockGrossPaymentChart from '../../../../mockData/mockGrossVolumeReportSuccess.json';
+import mockPayment from '../../../../mockData/mockPaymentDetailSuccess.json';
+import mockPayments from '../../../../mockData/mockPaymentsSuccess.json';
+import mockPayout from '../../../../mockData/mockPayoutDetailsSuccess.json';
+import mockPayouts from '../../../../mockData/mockPayoutsSuccess.json';
 
 export const mockAllServices = () => {
   createServer({
     routes() {
       this.urlPrefix = 'https://wc-proxy.justifi.ai/v1';
 
-      // The rest of the routes will be created in the following PRs
-      this.namespace = '/entities/business';
+      // BusinessDetails
+      this.get('/entities/business/:id', () => mockBusinessDetails);
 
-      // To test an error response, just replace 'mockBusinessDetails' by an error response
-      this.get('/:id', () => mockBusinessDetails);
+      this.patch('/entities/business/:id', (schema, request) => {
+        const id = request.params.id;
+        const newDetails = JSON.parse(request.requestBody);
+        return { ...mockBusinessDetails, ...newDetails, id };
+      });
+
+      // GrossPaymentChart
+      this.get(
+        '/account/:accountId/reports/gross_volume',
+        () => mockGrossPaymentChart
+      );
+
+      // PaymentDetails
+      this.get('payments/:id', () => mockPayment);
+
+      // PaymentsList
+      this.get('account/:id/payments', () => mockPayments);
+
+      // PayoutDetails
+      this.get('payouts/:id', () => mockPayout);
+
+      // PayoutsList
+      this.get('account/:id/payouts', () => mockPayouts);
+
+      // To test an error response, you can use something like:
+      // this.get('/somepath', new Response(500, {}, { error: 'An error message' }));
     },
   });
 };
