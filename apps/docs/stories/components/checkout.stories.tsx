28ce242621615fb3770c783064f7ac0df9dc9d3c
Jake Merringer <jakemerringer@gmail.com>
2024-04-11 14:52:04 -0500

    mock sezzel payment plan

--- apps/docs/stories/components/checkout.stories.tsx
+++ apps/docs/stories/components/checkout.stories.tsx
@@ -10,8 +10,8 @@ const meta: Meta = {
   title: 'dev/Payment Facilitation/Payments/Checkout',
   component: 'justifi-checkout',
   args: {
-    'auth-token': '',
-    'checkout-id': ''
+    'auth-token': 'eyJraWQiOiJqdXN0aWZpLWUyNDgyMmU3ODE1MmEzZjRkMjU1IiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2F1dGguanVzdGlmaS5haS8iLCJhenAiOiJ3Y3RfN2E3eVVOWkhUdUVzeGpObmhMUXhBRyIsInN1YiI6IndjdF83YTd5VU5aSFR1RXN4ak5uaExReEFHQHNlc3Npb25zIiwiYXVkIjoiaHR0cHM6Ly9hcGkuanVzdGlmaS5haS92MSIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyIsInBlcm1pc3Npb25zIjpbIndyaXRlOmNoZWNrb3V0OmNob183ZDNGbTZwVVVjS08xVVZQZElUVVk0Il0sImV4cCI6MTcxMjg2NTg0NywiaWF0IjoxNzEyODYyMjQ3LCJwbGF0Zm9ybV9hY2NvdW50X2lkIjoiYWNjXzNyZU5iNGFOWXkyaVdEWlFWY3pteDQifQ.fu8RVFVjt7_QIdUo8AFfo36DL9cWN-dgUvL5XcnvRRaMk9xdCAomVTl92EMcb0mZZuMd6ctP_ApubbsS6kq_J_vzJMkJCgyc9Xv7pPNSk_d9vsiMrZvDpMOxN8R9rwlpj7zKb_U_PllK3dQeSp_t5CEGibqOMlvkjfBVHBIOc3qgs6yCJipv7abkYzCTK32Aa1ee4pE44xYTILYoL467wibQlNEp7O1VZJrnLELSe_u5b0J3-oMV8i54syM5R9uNBfR6zABZf_uRnpFIX8m-ewX0217aBVycftqHAVgycL08TEiM-fEo-Pdqxd-2d57H6cDqfQiX6DFk-4auzQc2hw',
+    'checkout-id': 'cho_7d3Fm6pUUcKO1UVPdITUY4'
   },
   argTypes: {
     ...storyBaseArgs.argTypes,
--- packages/webcomponents/src/components/checkout/sezzel-payment-method.tsx
+++ packages/webcomponents/src/components/checkout/sezzel-payment-method.tsx
@@ -1,6 +1,7 @@
 import { Component, h, Prop, Method, Event, EventEmitter, State } from '@stencil/core';
 import { config } from '../../../config';
 import { PaymentMethodOption } from './payment-method-option-utils';
+// import { formatCurrency, formatDate } from '../../utils/utils';
 
 @Component({
   tag: 'justifi-sezzel-payment-method',
@@ -12,7 +13,7 @@ export class SezzelPaymentMethod {
   @Prop() accountId: string;
   @Prop() paymentMethodOption: PaymentMethodOption;
   @Prop() isSelected: boolean;
-  @State() sezzleScriptLoaded: boolean = false;
+  @State() sezzelLoaded: boolean = false;
 
   @Event({ bubbles: true }) paymentMethodOptionSelected: EventEmitter;
 
@@ -22,19 +23,72 @@ export class SezzelPaymentMethod {
   }
 
   componentWillLoad() {
-    this.loadSezzleScript();
   }
 
   onPaymentMethodOptionClick = () => {
     this.paymentMethodOptionSelected.emit(this.paymentMethodOption);
   };
 
-  showNewPaymentMethodForm() {
+  makeMockInstallmentPlan() {
+    return {
+      "schedule": "bi-weekly",
+      "totalInCents": 1000,
+      "installments": [
+        {
+          "installment": 1,
+          "amountInCents": 250,
+          "dueDate": "2020-10-14"
+        },
+        {
+          "installment": 2,
+          "amountInCents": 250,
+          "dueDate": "2020-10-28"
+        },
+        {
+          "installment": 3,
+          "amountInCents": 250,
+          "dueDate": "2020-11-11"
+        },
+        {
+          "installment": 4,
+          "amountInCents": 250,
+          "dueDate": "2020-11-25"
+        }
+      ]
+    };
+  }
+
+  showSezzelPaymentPlan() {
+    this.loadSezzleScript();
+    const paymentPlan = this.makeMockInstallmentPlan();
+
     return (
       <div class="mt-2 pb-4 border-bottom">
-        <div class="mb-3">
-          Payment Plan
-        </div>
+        {this.sezzelLoaded ? (
+          <div class="mb-3">
+            Make {paymentPlan.installments.length} {paymentPlan.schedule} payments
+            <ul class="list-group">
+              {paymentPlan.installments.map((installment) => {
+                return (
+
+                  <li class="list-group-item">Installment #{installment.installment}</li>
+                  // <div>
+                  //   <div>Installment #{installment.installment} {formatCurrency(installment.amountInCents)}</div>
+                  //   <div>Due {formatDate(installment.dueDate)}</div>
+                  // </div>
+                );
+              })}
+            </ul>
+
+            <div id="sezzle-smart-button-container"></div>
+          </div>
+        ) : (
+          <div class="d-flex justify-content-center">
+            <div class="spinner-border text-primary" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
@@ -42,7 +96,7 @@ export class SezzelPaymentMethod {
   loadSezzleScript() {
     const script = document.createElement('script');
     script.src = 'https://checkout-sdk.sezzle.com/checkout.min.js';
-    script.onload = () => { this.sezzleScriptLoaded = true; };
+    script.onload = () => { this.sezzelLoaded = true; };
     // script.onerror = () => { };
     script.async = true;
     document.body.appendChild(script);
@@ -66,11 +120,11 @@ export class SezzelPaymentMethod {
           <label
             htmlFor={this.paymentMethodOption?.id}
             class="form-check-label">
-            Sezzel
+            Buy now, pay later with Sezzle
           </label>
         </div>
 
-        {this.isSelected ? this.showNewPaymentMethodForm() : null}
+        {(this.isSelected) ? this.showSezzelPaymentPlan() : null}
       </div>
     );
   }
