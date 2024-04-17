cd47b280692a5eef3398d5703c9745bba2fb366c
TiagoFuelber <tfuelber@gmail.com>
2024-04-17 19:13:56 -0300

    add error event to business details

--- packages/webcomponents/src/components/business-details/test/busines-details-core.spec.tsx
+++ packages/webcomponents/src/components/business-details/test/busines-details-core.spec.tsx
@@ -1,14 +1,16 @@
+import { h } from "@stencil/core";
 import { newSpecPage } from "@stencil/core/testing";
 import { BusinessDetailsCore } from "../business-details-core";
 import { makeGetBusiness } from "../get-business";
 import mockBusinessDetails from './../../../api/mockData/mockBusinessDetails.json';
 import { Business, IBusiness } from "../../../api/Business";
+import { API_NOT_AUTHENTICATED_ERROR } from "../../../api/shared";
 
 describe('BusinessDetailsCore', () => {
   it('should display loading state correclty', async () => {
     const page = await newSpecPage({
       components: [BusinessDetailsCore],
-      html: '<business-details-core></business-details-core>',
+      template: () => <business-details-core />,
     });
 
     await page.waitForChanges();
@@ -19,7 +21,7 @@ describe('BusinessDetailsCore', () => {
   it('should display error state correctly', async () => {
     const page = await newSpecPage({
       components: [BusinessDetailsCore],
-      html: '<business-details-core></business-details-core>',
+      template: () => <business-details-core />,
     });
 
     const getBusiness = makeGetBusiness({
@@ -44,7 +46,7 @@ describe('BusinessDetailsCore', () => {
   it('should set business details correctly to state', async () => {
     const page = await newSpecPage({
       components: [BusinessDetailsCore],
-      html: '<business-details-core></business-details-core>',
+      template: () => <business-details-core />,
     });
 
     const getBusiness = makeGetBusiness({
@@ -66,6 +68,64 @@ describe('BusinessDetailsCore', () => {
     const expectedBusiness = new Business(mockBusinessDetails.data as unknown as IBusiness);
     expect(page.rootInstance.business).toEqual(expectedBusiness);
     expect(page.root).toMatchSnapshot();
+  });
+
+  it('should emit error event when fetch fails', async () => {
+    const getBusiness = makeGetBusiness({
+      authToken: 'abc',
+      id: 'abc',
+      service: {
+        fetchBusiness: jest.fn().mockRejectedValue(new Error('Fetch error')),
+      },
+    })
+
+    const errorSpy = jest.fn();
+
+    const page = await newSpecPage({
+      components: [BusinessDetailsCore],
+      template: () => <business-details-core getBusiness={getBusiness} onErrorEvent={errorSpy} />,
+    });
+
+    await page.waitForChanges();
+
+    expect(errorSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        detail: {
+          errorCode: 'fetch-error',
+          message: 'Fetch error',
+          severity: 'error',
+        }
+      })
+    );
+  });
+
+  it('should emit error event when API returns error', async () => {
+    const getBusiness = makeGetBusiness({
+      authToken: 'abc',
+      id: 'abc',
+      service: {
+        fetchBusiness: jest.fn().mockResolvedValue(API_NOT_AUTHENTICATED_ERROR),
+      },
+    })
+
+    const errorSpy = jest.fn();
+
+    const page = await newSpecPage({
+      components: [BusinessDetailsCore],
+      template: () => <business-details-core getBusiness={getBusiness} onErrorEvent={errorSpy} />,
+    });
+
+    await page.waitForChanges();
+
+    expect(errorSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        detail: {
+          errorCode: 'fetch-error',
+          message: 'Not Authenticated',
+          severity: 'error',
+        }
+      })
+    );
   });
 });
 
--- packages/webcomponents/src/components/business-details/test/business-details.spec.tsx
+++ packages/webcomponents/src/components/business-details/test/business-details.spec.tsx
@@ -1,3 +1,4 @@
+import { h } from '@stencil/core';
 import { newSpecPage } from '@stencil/core/testing';
 import { BusinessDetails } from '../business-details';
 import { BusinessDetailsCore } from '../business-details-core';
@@ -6,7 +7,7 @@ describe('justifi-business-details', () => {
   it('initializes getBusiness on load with valid props', async () => {
     const page = await newSpecPage({
       components: [BusinessDetails],
-      html: `<justifi-business-details business-id="123" auth-token="token" />`,
+      template: () => <justifi-business-details business-id="123" auth-token="token" />,
     });
 
     page.waitForChanges();
@@ -18,7 +19,7 @@ describe('justifi-business-details', () => {
   it('sets an error message with invalid props', async () => {
     const page = await newSpecPage({
       components: [BusinessDetails],
-      html: `<justifi-business-details></justifi-business-details>`, // No props provided
+      template: () => <justifi-business-details />, // No props provided
     });
 
     page.waitForChanges();
@@ -29,7 +30,7 @@ describe('justifi-business-details', () => {
   it('renders the error state when there is an error message', async () => {
     const page = await newSpecPage({
       components: [BusinessDetails, BusinessDetailsCore],
-      html: `<justifi-business-details />`,
+      template: () => <justifi-business-details />,
     });
 
     await page.waitForChanges();
@@ -41,12 +42,33 @@ describe('justifi-business-details', () => {
   it('renders business-details-core when there is no error message', async () => {
     const page = await newSpecPage({
       components: [BusinessDetails, BusinessDetailsCore],
-      html: `<justifi-business-details business-id="123" auth-token="token" />`,
+      template: () => <justifi-business-details business-id="123" auth-token="token" />,
     });
 
     page.waitForChanges();
 
     const core = page.root.shadowRoot.querySelector('business-details-core');
     expect(core).not.toBeNull();
+  });
+
+  it('emits an error event when there is no auth token', async () => {
+    const errorSpy = jest.fn();
+
+    const page = await newSpecPage({
+      components: [BusinessDetails],
+      template: () => <justifi-business-details business-id="123" onErrorEvent={errorSpy} />,
+    });
+
+    page.waitForChanges();
+
+    expect(errorSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        detail: {
+          errorCode: 'missing-props',
+          message: 'Invalid business id or auth token',
+          severity: 'error',
+        }
+      })
+    );
   });
 });
