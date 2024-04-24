82cd8a58999fccfa85a377da524100b3f8dd4512
Colgan Meanor <colganmeanor7@gmail.com>
2024-04-24 15:59:01 -0500

    Update core-info-schema with correct syntax

--- packages/webcomponents/src/components/business-forms/schemas/business-core-info-schema.ts
+++ packages/webcomponents/src/components/business-forms/schemas/business-core-info-schema.ts
@@ -15,17 +15,16 @@ export const businessCoreInfoSchema = (easyValidate?: boolean) => {
   });
 
   const easySchema = object({
-    legal_name: string().required('Enter legal name').nullable(),
+    legal_name: string().required(),
     website_url: string()
       .url('Enter valid website url')
-      .required('Enter website url')
       .nullable(),
-    email: string().email('Enter valid email').required('Enter email').nullable(),
-    phone: string().required('Enter phone number').nullable(),
-    doing_business_as: string().required('Enter doing business as').nullable(),
-    business_type: string().required('Select business type').nullable(),
-    business_structure: string().required('Select business structure').nullable(),
-    industry: string().required('Enter a business industry').nullable(),
+    email: string().email('Enter valid email').nullable(),
+    phone: string().matches(/^\d{10}$/, 'Enter valid phone number').nullable(),
+    doing_business_as: string().nullable(),
+    business_type: string().nullable(),
+    business_structure: string().nullable(),
+    industry: string().nullable(),
   });
 
   return easyValidate ? easySchema : schema;
