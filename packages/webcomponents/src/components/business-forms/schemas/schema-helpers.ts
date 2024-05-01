9bee4ba9e8cd82a4780e6472aa5b864abd477075
Colgan Meanor <colganmeanor7@gmail.com>
2024-05-01 15:45:15 -0500

    Move transform empty string logic to schema-helpers

--- packages/webcomponents/src/components/business-forms/schemas/schema-helpers.ts
+++ packages/webcomponents/src/components/business-forms/schemas/schema-helpers.ts
@@ -1,4 +1,12 @@
 
+export const transformEmptyString = (value: string) => {
+  return value === '' ? null : value;
+}
+
+export const businessNameRegex = /^(?!^\s+$)[a-zA-Z0-9\s,&.-]*$/;
+
 export const phoneRegex = /^\d{10}$/;
 
 export const urlRegex = /^(?:http(s)?:\/\/)?(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
+
+export const stringLettersOnlyRegex = /^(?!^\s+$)[a-zA-Z\s]*$/;
