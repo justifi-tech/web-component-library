99a4906224947e09cf6300040589376e1405ed09
Jake Merringer <jakemerringer@gmail.com>
2024-05-09 13:50:04 -0500

    empty 'theme' until it is a focus later

--- apps/docs/stories/theme.tsx
+++ apps/docs/stories/theme.tsx
@@ -1,43 +1,5 @@
 const theme = `
-  body {
-    background-color: rgb(247, 247, 247);
-  }
-
-  justifi-checkout {
-    display: block;
-    margin: 0 auto;
-    max-width: 420px;
-    padding: 40px 60px;
-    background-color: white;
-    border-radius: 10px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  }
-
-  justifi-checkout::part(summary-heading) {
-    margin-top: 10px;
-  }
-  justifi-checkout::part(payment-description) {
-    margin-bottom: 20px;
-    padding-bottom: 20px;
-    border-bottom: 2px dotted rgba(0, 0, 0, 0.3);
-  }
-  justifi-checkout::part(payment-total) {
-    display: flex;
-    justify-content: space-between;
-    align-items: baseline;
-  }
-  justifi-checkout::part(payment-total-label),
-  justifi-checkout::part(payment-total-amount) {
-    font-size: 16px;
-    font-weight: bold;
-    line-height: 20px;
-    display: block;
-    background-color: white;
-  }
-  justifi-checkout::part(pay-button) {
-    width: 100%;
-    margin: 0 auto;
-    font-weight: bold;
+  body {  
   }
 `;
 
