99ac425dd36421e3f7c46045ea920f7b3b1f0b0b
Jake Merringer <jakemerringer@gmail.com>
2024-05-15 18:06:03 -0500

    add working theme switcher to checkout story

--- apps/docs/stories/theme.tsx
+++ apps/docs/stories/theme.tsx
@@ -1,6 +1,24 @@
-const theme = `
-  body {  
+export enum ThemeNames {
+  None = 'None (bootstrap defaults)',
+  Light = 'Light',
+  Dark = 'Dark',
+}
+
+const light = `
+  body {
+    background-color: #f8f8f8;
   }
 `;
 
-export default theme;
+const dark = `
+  body {
+    background-color: #333;
+  }
+`;
+
+const themes = {
+  [ThemeNames.Light]: light,
+  [ThemeNames.Dark]: dark
+}
+
+export default themes;
