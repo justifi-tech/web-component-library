e3747ef5c04355c3cb3f452e4d724053e4e4d86e
Colgan Meanor <colganmeanor7@gmail.com>
2024-07-01 15:16:13 -0500

    Fix broken test

--- packages/webcomponents/src/components/business-forms/owner-form/identity-address/test/identity-address.spec.tsx
+++ packages/webcomponents/src/components/business-forms/owner-form/identity-address/test/identity-address.spec.tsx
@@ -13,33 +13,31 @@ describe('identity-address', () => {
     });
     expect(page.root).toEqualHtml(`
     <justifi-identity-address-form exportparts="label,input,input-invalid">
-      <mock:shadow-root>
-        <div class="row gy-3">
-          <div class="col-12 col-md-8">
-            <form-control-text label="Street Address" name="line1"></form-control-text>
-          </div>
-
-          <div class="col-12 col-md-4">
-            <form-control-text label="Apartment, Suite, etc. (optional)" name="line2"></form-control-text>
-          </div>
-
-          <div class="col-12">
-            <form-control-text label="City" name="city"></form-control-text>
-          </div>
-
-          <div class="col-12 col-md-6">
-             <form-control-select label="State" name="state"></form-control-select>
-          </div>
-
-          <div class="col-12 col-md-6">
-            <form-control-text label="Postal Code" maxlength="5" name="postal_code"></form-control-text>
-          </div>
-
-          <div class="col-12">
-            <form-control-select disabled="" label="Country" name="country"></form-control-select>
-          </div>
+      <div class="row gy-3">
+        <div class="col-12 col-md-8">
+          <form-control-text label="Street Address" name="line1"></form-control-text>
         </div>
-      </mock:shadow-root>
+
+        <div class="col-12 col-md-4">
+          <form-control-text label="Apartment, Suite, etc. (optional)" name="line2"></form-control-text>
+        </div>
+
+        <div class="col-12">
+          <form-control-text label="City" name="city"></form-control-text>
+        </div>
+
+        <div class="col-12 col-md-6">
+            <form-control-select label="State" name="state"></form-control-select>
+        </div>
+
+        <div class="col-12 col-md-6">
+          <form-control-text label="Postal Code" maxlength="5" name="postal_code"></form-control-text>
+        </div>
+
+        <div class="col-12">
+          <form-control-select disabled="" label="Country" name="country"></form-control-select>
+        </div>
+      </div>
     </justifi-identity-address-form>
   `);
   });
@@ -63,28 +61,26 @@ describe('identity-address', () => {
     });
     expect(page.root).toEqualHtml(`
     <justifi-identity-address-form exportparts="label,input,input-invalid">
-      <mock:shadow-root>
-        <div class="gy-3 row">
-          <div class="col-12 col-md-8">
-            <form-control-text defaultValue="Street 1" label="Street Address" name="line1"></form-control-text>
-          </div>
-          <div class="col-12 col-md-4">
-            <form-control-text defaultValue="Apartment 1" label="Apartment, Suite, etc. (optional)" name="line2"></form-control-text>
-          </div>
-          <div class="col-12">
-            <form-control-text defaultvalue="City" label="City" name="city"></form-control-text>
-          </div>
-          <div class="col-12 col-md-6">
-            <form-control-select defaultValue="State" label="State" name="state"></form-control-select>
-          </div>
-          <div class="col-12 col-md-6">
-            <form-control-text defaultvalue="12345" label="Postal Code" maxlength="5" name="postal_code"></form-control-text>
-          </div>
-          <div class="col-12">
-            <form-control-select defaultValue="Country" disabled="" label="Country" name="country"></form-control-select>
-          </div>
+      <div class="gy-3 row">
+        <div class="col-12 col-md-8">
+          <form-control-text defaultValue="Street 1" label="Street Address" name="line1"></form-control-text>
         </div>
-      </mock:shadow-root>
+        <div class="col-12 col-md-4">
+          <form-control-text defaultValue="Apartment 1" label="Apartment, Suite, etc. (optional)" name="line2"></form-control-text>
+        </div>
+        <div class="col-12">
+          <form-control-text defaultvalue="City" label="City" name="city"></form-control-text>
+        </div>
+        <div class="col-12 col-md-6">
+          <form-control-select defaultValue="State" label="State" name="state"></form-control-select>
+        </div>
+        <div class="col-12 col-md-6">
+          <form-control-text defaultvalue="12345" label="Postal Code" maxlength="5" name="postal_code"></form-control-text>
+        </div>
+        <div class="col-12">
+          <form-control-select defaultValue="Country" disabled="" label="Country" name="country"></form-control-select>
+        </div>
+      </div>
     </justifi-identity-address-form>
   `);
   });
