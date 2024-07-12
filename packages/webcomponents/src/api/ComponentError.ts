ee0f23273e15c499b748817db7585f81bbdd34a5
Colgan Meanor <colganmeanor7@gmail.com>
2024-07-12 12:49:41 -0500

    Add new component error codes for patching and posting data

--- packages/webcomponents/src/api/ComponentError.ts
+++ packages/webcomponents/src/api/ComponentError.ts
@@ -1,6 +1,8 @@
 export enum ComponentErrorCodes {
   MISSING_PROPS = 'missing-props',
   FETCH_ERROR = 'fetch-error',
+  PATCH_ERROR = 'patch-error',
+  POST_ERROR = 'post-error',
   UNKNOWN_ERROR = 'unknown-error',
   TOKENIZE_ERROR = 'tokenize-error',
   NOT_AUTHENTICATED = 'not-authenticated'
