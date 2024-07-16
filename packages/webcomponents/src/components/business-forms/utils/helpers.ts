e6cdb0206a8633c900d5a2f2116095501da2497a
Colgan Meanor <colganmeanor7@gmail.com>
2024-07-16 12:23:21 -0500

    Add helper function to check provisioning status

--- packages/webcomponents/src/components/business-forms/utils/helpers.ts
+++ packages/webcomponents/src/components/business-forms/utils/helpers.ts
@@ -1,3 +1,4 @@
+import { IBusiness } from "../../../components";
 import StateOptions from "../../../utils/state-options";
 
 export function getStateAbbreviation(stateName: string): string | undefined {
@@ -25,4 +26,12 @@ export function deconstructDate(formInput: any): { dob_year: string, dob_month: 
   const dateString = formInput.value;
   const [dob_year, dob_month, dob_day] = dateString.split('-');
   return { dob_year, dob_month, dob_day };
+}
+
+export function checkProvisioningStatus(business: IBusiness): boolean{
+  if (business.associated_accounts.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
