3166b1e0d37b58747a9f6843063c74af7e6056bb
Jake Merringer <jakemerringer@gmail.com>
2024-07-22 10:15:24 -0500

    Update checkout example to show insurance component usage

--- apps/docs/stories/components/checkout-example.ts
+++ apps/docs/stories/components/checkout-example.ts
@@ -6,7 +6,13 @@ export default `<!DOCTYPE html>
 ${codeExampleHead('justifi-checkout')}
 
 <body>
-  <justifi-checkout auth-token="token" checkout-id="abc123" ></justifi-checkout>
+  <justifi-checkout auth-token="token" checkout-id="abc123">
+    <!-- Optional: add the insurance slot and component -->
+    <div slot="insurance">
+      <!-- see the insurance component docs for the full list of props -->
+      <justifi-season-interruption-insurance checkout-id="abc123"></justifi-season-interruption-insurance>
+    </div>
+  </justifi-checkout>
 </body>
 
 <script>
