f020283813b0bd2a682ed43a6d35ce3c4ea06ac3
Colgan Meanor <colganmeanor7@gmail.com>
2024-07-22 16:03:49 -0500

    Replace double quotes with single quotes

--- packages/webcomponents/src/components/business-forms/payment-provisioning/legal-address-form/legal-address-form-step-core.tsx
+++ packages/webcomponents/src/components/business-forms/payment-provisioning/legal-address-form/legal-address-form-step-core.tsx
@@ -100,54 +100,54 @@ export class LegalAddressFormStepCore {
       this.formController.getInitialValues();
 
     return (
-      <Host exportparts="label,input,input-invalid">
+      <Host exportparts='label,input,input-invalid'>
         <form>
           <fieldset>
             <legend>Business Legal Address</legend>
             <hr />
-            <div class="row gy-3">
-              <div class="col-12">
+            <div class='row gy-3'>
+              <div class='col-12'>
                 <form-control-text
-                  name="line1"
-                  label="Legal Address"
+                  name='line1'
+                  label='Legal Address'
                   inputHandler={this.inputHandler}
                   defaultValue={legalAddressDefaultValue?.line1}
                   errorText={this.errors?.line1}
                 />
               </div>
-              <div class="col-12">
+              <div class='col-12'>
                 <form-control-text
-                  name="line2"
-                  label="Address Line 2"
+                  name='line2'
+                  label='Address Line 2'
                   inputHandler={this.inputHandler}
                   defaultValue={legalAddressDefaultValue?.line2}
                   errorText={this.errors?.line2}
 
                 />
               </div>
-              <div class="col-12">
+              <div class='col-12'>
                 <form-control-text
-                  name="city"
-                  label="City"
+                  name='city'
+                  label='City'
                   inputHandler={this.inputHandler}
                   defaultValue={legalAddressDefaultValue?.city}
                   errorText={this.errors?.city}
                 />
               </div>
-              <div class="col-12">
+              <div class='col-12'>
                 <form-control-select
-                  name="state"
-                  label="State"
+                  name='state'
+                  label='State'
                   options={StateOptions}
                   inputHandler={this.inputHandler}
                   defaultValue={legalAddressDefaultValue?.state}
                   errorText={this.errors?.state}
                 />
               </div>
-              <div class="col-12">
+              <div class='col-12'>
                 <form-control-text
-                  name="postal_code"
-                  label="Postal Code"
+                  name='postal_code'
+                  label='Postal Code'
                   inputHandler={this.inputHandler}
                   defaultValue={legalAddressDefaultValue?.postal_code}
                   errorText={this.errors?.postal_code}
@@ -155,10 +155,10 @@ export class LegalAddressFormStepCore {
                   keyDownHandler={numberOnlyHandler}
                 />
               </div>
-              <div class="col-12">
+              <div class='col-12'>
                 <form-control-select
-                  name="country"
-                  label="Country"
+                  name='country'
+                  label='Country'
                   options={[{ label: 'United States', value: 'USA' }]}
                   inputHandler={this.inputHandler}
                   defaultValue={legalAddressDefaultValue?.country}
