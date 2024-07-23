733f3235cf249f75160d53f4a67e54107bbfd53f
Jake Merringer <jakemerringer@gmail.com>
2024-07-23 15:55:38 -0500

    Use theme provider component to append stylesheets to head

--- packages/webcomponents/src/components/theme-provider/theme-provider.tsx
+++ packages/webcomponents/src/components/theme-provider/theme-provider.tsx
@@ -1,17 +1,27 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Prop } from '@stencil/core';
+import { version } from '../../../package.json'
 
 @Component({
   tag: 'justifi-theme-provider',
   shadow: true,
 })
 export class ThemeProvider {
+  @Prop() href: string; // URL to the theme css file
 
-  render() {
-    return (
-      <Host>
-        <slot></slot>
-      </Host>
-    );
+  componentWillLoad() {
+    // This should pull the local stylesheet in development
+    this.appendStylesheet(`https://cdn.jsdelivr.net/npm/@justifi/webcomponents@${version}/dist/webcomponents/webcomponents.css`);
+
+    if (this.href) {
+      this.appendStylesheet(this.href);
+    }
   }
 
+  appendStylesheet(href: string) {
+    const documentHead = document.head;
+    const baseStylesheet = document.createElement('link');
+    baseStylesheet.rel = 'stylesheet';
+    baseStylesheet.href = href;
+    documentHead.appendChild(baseStylesheet);
+  }
 }
