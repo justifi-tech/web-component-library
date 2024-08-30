58e7eff634972868d4a9117633b4d9c48e7ee242
Jake Merringer <jakemerringer@gmail.com>
2024-08-30 10:48:10 -0500

    Rename manager.ts stylesheet for clarity

--- apps/docs/.storybook/manager.ts
+++ apps/docs/.storybook/manager.ts
@@ -1,6 +1,6 @@
 import { addons } from '@storybook/manager-api';
 import storybookTheme from './storybookTheme';
-import './styles.css';
+import './manager.css';
 
 addons.setConfig({
   theme: storybookTheme,
