fd44172b7e708bd13c39723dc4f8f929c3099b20
TiagoFuelber <tfuelber@gmail.com>
2024-09-23 17:25:53 -0300

    attempt to fix npm workflow

--- packages/webcomponents/stencil.config.ts
+++ packages/webcomponents/stencil.config.ts
@@ -28,10 +28,8 @@ export const config: Config = {
   sourceMap: false,
   plugins: [
     replace({
-      // IFRAME_ORIGIN: JSON.stringify(process.env.IFRAME_ORIGIN),
-      IFRAME_ORIGIN: JSON.stringify('https://components.justifi.ai/v1'),
-      // PROXY_API_ORIGIN: JSON.stringify(process.env.PROXY_API_ORIGIN),
-      PROXY_API_ORIGIN: JSON.stringify('https://wc-proxy.justifi.ai'),
+      IFRAME_ORIGIN: JSON.stringify(process.env.IFRAME_ORIGIN),
+      PROXY_API_ORIGIN: JSON.stringify(process.env.PROXY_API_ORIGIN),
       preventAssignment: true,
     }),
   ],
