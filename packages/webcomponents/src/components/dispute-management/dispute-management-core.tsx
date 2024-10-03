aaf410388d50eaedec5b64cdde53587ddfeb4efa
Jake Merringer <jakemerringer@gmail.com>
2024-10-03 13:15:37 -0500

    add dispute boilerplate

---
+++ packages/webcomponents/src/components/dispute-management/dispute-management-core.tsx
@@ -0,0 +1,17 @@
+import { Component, h } from "@stencil/core";
+import { StyledHost } from "../../ui-components";
+
+@Component({
+  tag: 'justifi-dispute-management-core',
+  styleUrls: [],
+  shadow: true,
+})
+export class DisputeManagementCore {
+
+  render() {
+    return (
+      <StyledHost>
+      </StyledHost>
+    );
+  }
+};
---
+++ packages/webcomponents/src/components/dispute-management/dispute-management.tsx
@@ -0,0 +1,15 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'justifi-dispute-management',
+  shadow: true,
+})
+export class DisputeManagement {
+
+  render() {
+    return (
+      <justifi-dispute-management-core>
+      </justifi-dispute-management-core>
+    );
+  }
+}
