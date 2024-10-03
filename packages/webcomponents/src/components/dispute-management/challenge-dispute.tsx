574fc8a896cc6155f4d69fc06740f4d0d33de429
Jake Merringer <jakemerringer@gmail.com>
2024-10-03 15:53:11 -0500

    split dispute component into sub-components with template conditional

---
+++ packages/webcomponents/src/components/dispute-management/challenge-dispute.tsx
@@ -0,0 +1,15 @@
+import { Component, h } from "@stencil/core";
+
+@Component({
+  tag: 'justifi-challenge-dispute',
+})
+export class ChallengeDispute {
+
+  render() {
+    return (
+      <div>
+        Challenge dispute
+      </div>
+    );
+  }
+};
--- packages/webcomponents/src/components/dispute-management/dispute-management-core.tsx
+++ packages/webcomponents/src/components/dispute-management/dispute-management-core.tsx
@@ -1,50 +1,20 @@
-import { Component, h } from "@stencil/core";
+import { Component, h, State } from "@stencil/core";
 import { StyledHost } from "../../ui-components";
 
 @Component({
   tag: 'justifi-dispute-management-core',
-  styleUrls: [],
 })
 export class DisputeManagementCore {
+  @State() isDisputeNotificationVisible = true;
 
   render() {
     return (
       <StyledHost>
-        <h1 class="h4">This payment was disputed</h1>
-        <p>The cardholder for this payment claims that the product was not received.</p>
-        <p>you can either challenge the dispute by presenting proof of the product's delivery to or use by the cardholder, or you can choose to accept the dispute right away, refund the cardholder, and resolve the matter.</p>
-
-        <h2 class="h5">Dispute details</h2>
-        <div class="d-table gap-2">
-          <div class="d-table-row gap-2">
-            <span part="detail-section-item-title" class="fw-bold d-table-cell pe-4">
-              Amount
-            </span>
-            <span part="detail-section-item-data" class="flex-1 d-table-cell text-wrap">
-              $50.00
-            </span>
-          </div>
-          <div class="d-table-row gap-2">
-            <span part="detail-section-item-title" class="fw-bold d-table-cell pe-4">
-              Reason
-            </span>
-            <span part="detail-section-item-data" class="flex-1 d-table-cell text-wrap">
-              Product not received
-            </span>
-          </div>
-          <div class="d-table-row gap-2">
-            <span part="detail-section-item-title" class="fw-bold d-table-cell pe-4">
-              Dispute ID
-            </span>
-            <span part="detail-section-item-data" class="flex-1 d-table-cell text-wrap">
-              dis_1234567890
-            </span>
-          </div>
-        </div>
-        <div class="d-flex gap-2 mt-4 justify-content-end">
-          <button class="btn btn-outline-primary">Accept dispute</button>
-          <button class="btn btn-primary">Challenge dispute</button>
-        </div>
+        {this.isDisputeNotificationVisible ? (
+          <justifi-dispute-notification></justifi-dispute-notification>
+        ) : (
+          <justifi-challenge-dispute></justifi-challenge-dispute>
+        )}
       </StyledHost>
     );
   }
