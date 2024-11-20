d984008d0bd40ff7b6168b61e4f1f39edab76d8c
Colgan Meanor <colganmeanor7@gmail.com>
2024-11-20 16:47:42 -0600

    Update Checkout type file

--- packages/webcomponents/src/api/Checkout.ts
+++ packages/webcomponents/src/api/Checkout.ts
@@ -9,9 +9,56 @@ export interface IBnpl {
   provider_api_version: string;
 }
 
+interface ICompletion {
+  payment_mode?: ICheckoutPaymentMode;
+  payment_token?: string | null;
+  payment_status?: string | null;
+  payment_response?: any;
+  payment_error_code?: string | null;
+  payment_error_description?: string | null;
+  checkout_id?: string;
+  additional_transactions?: any[];
+  payment_id?: string;
+  payment_method_id?: string;
+  status?: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export class Completion implements ICompletion {
+  payment_mode?: ICheckoutPaymentMode;
+  payment_token?: string | null;
+  payment_status?: string | null;
+  payment_response?: any;
+  payment_error_code?: string | null;
+  payment_error_description?: string | null;
+  checkout_id?: string;
+  additional_transactions?: any[];
+  payment_id?: string;
+  payment_method_id?: string;
+  status?: string;
+  created_at: string;
+  updated_at: string;
+
+  constructor(data: ICompletion) {
+    this.payment_mode = ICheckoutPaymentMode[data.payment_mode];
+    this.payment_token = data.payment_token;
+    this.payment_status = data.payment_status;
+    this.payment_response = data.payment_response;
+    this.payment_error_code = data.payment_error_code;
+    this.payment_error_description = data.payment_error_description;
+    this.checkout_id = data.checkout_id;
+    this.additional_transactions = data.additional_transactions;
+    this.payment_id = data.payment_id;
+    this.payment_method_id = data.payment_method_id;
+    this.status = data.status;
+    this.created_at = data.created_at;
+    this.updated_at = data.updated_at;
+  }
+}
+
 export interface ICheckout {
   id: string;
-  created_at: string;
   description: string;
   payment_intent_id: string;
   account_id: string;
@@ -42,11 +89,18 @@ export interface ICheckout {
   total_amount: number;
   insurance_amount: number;
   status: ICheckoutStatus;
+  payment_currency?: string;
+  mode?: string | null;
+  statement_descriptor?: string | null;
+  application_fees?: any[] | null;
+  successful_payment_id?: string | null;
+  created_at: string;
+  updated_at: string;
+  completions?: ICompletion[];
 }
 
 export class Checkout implements ICheckout {
   id: string;
-  created_at: string;
   description: string;
   payment_intent_id: string;
   account_id: string;
@@ -54,6 +108,7 @@ export class Checkout implements ICheckout {
   payment_amount: number;
   payment_client_id: string;
   payment_description: string;
+  payment_mode: ICheckoutPaymentMode;
   payment_methods: {
     id: string;
     status: string;
@@ -77,13 +132,81 @@ export class Checkout implements ICheckout {
   total_amount: number;
   insurance_amount: number;
   status: ICheckoutStatus;
+  payment_currency?: string;
+  mode?: string | null;
+  statement_descriptor?: string | null;
+  application_fees?: any[] | null;
+  successful_payment_id?: string | null;
+  created_at: string;
+  updated_at: string;
+  completions?: ICompletion[];
+  sub_account_name?: string;
 
   constructor(data: ICheckout) {
-    Object.assign(this, data);
+    this.id = data.id;
+    this.description = data.description;
+    this.payment_intent_id = data.payment_intent_id;
+    this.account_id = data.account_id;
+    this.platform_account_id = data.platform_account_id;
+    this.payment_amount = data.payment_amount;
+    this.payment_client_id = data.payment_client_id;
+    this.payment_description = data.payment_description;
+    this.payment_mode = this.paymentMode;
+    this.payment_methods = data.payment_methods;
+    this.payment_method_group_id = data.payment_method_group_id;
+    this.payment_settings = data.payment_settings;
+    this.bnpl = data.bnpl;
+    this.total_amount = data.total_amount;
+    this.insurance_amount = data.insurance_amount;
+    this.status = data.status as ICheckoutStatus;
+    this.payment_currency = data.payment_currency;
+    this.mode = data.mode;
+    this.statement_descriptor = data.statement_descriptor;
+    this.application_fees = data.application_fees;
+    this.successful_payment_id = data.successful_payment_id;
+    this.created_at = data.created_at;
+    this.updated_at = data.updated_at;
+    this.completions = data.completions?.map((completion) => new Completion(completion));
   }
+
+  get paymentMode(): ICheckoutPaymentMode {
+    let paymentMode: ICheckoutPaymentMode;
+    
+    if (!this.completions || this.completions.length === 0) {
+      return ICheckoutPaymentMode.unknown;
+    }
+    
+    const sortedCompletions = this.completions.sort((a, b) => {
+      return new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime();
+    });
+    
+    const lastCompletion = sortedCompletions[0];
+
+    if (lastCompletion.payment_mode) {
+      paymentMode = lastCompletion.payment_mode;
+    } else if (lastCompletion.payment_response?.data?.payment_method?.card) {
+      paymentMode = ICheckoutPaymentMode.card;
+    } else if (lastCompletion.payment_response?.data?.payment_method?.bank_account) {
+      paymentMode = ICheckoutPaymentMode.bank_account;
+    } 
+
+    return paymentMode;
+  };
 }
 
-export type ICheckoutPaymentMode = 'ecom' | 'bnpl';
+export enum ICheckoutPaymentMode {
+  bnpl = 'Buy Now Pay Later',
+  ecom = 'E-commerce',
+  card = 'Card',
+  bank_account = 'Bank Account',
+  card_present = 'Card Present',
+  unknown = ''
+}
+
+export enum CompletionStatuses {
+  failed = 'failed',
+  succeeded = 'succeeded'
+}
 
 export type ICheckoutStatus = 'created' | 'completed' | 'attempted' | 'expired';
 
@@ -104,4 +227,6 @@ export type ILoadedEventResponse = {
 export interface ICheckoutsParams {
   status?: ICheckoutStatus;
   payment_mode?: ICheckoutPaymentMode;
+  after_cursor?: string;
+  before_cursor?: string;
 }
