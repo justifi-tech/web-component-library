1e66b01c4af96ecc8cedddacf0db6711e4936a45
Jake Merringer <jakemerringer@gmail.com>
2024-11-25 16:53:58 -0600

    Add makeUpdateDisputeResponse  function in actions file

--- packages/webcomponents/src/components/dispute-management/dispute-response/dispute-response-actions.ts
+++ packages/webcomponents/src/components/dispute-management/dispute-response/dispute-response-actions.ts
@@ -34,3 +34,31 @@ export const makeGetDisputeResponse =
         });
       }
     };
+
+export const makeUpdateDisputeResponse = ({ authToken, disputeId, service }) =>
+  async ({ payload, onSuccess, onError, final = () => { } }) => {
+    try {
+      const response = await service.updateDisputeResponse(disputeId, authToken, payload);
+
+      if (!response.error) {
+        onSuccess(response);
+      } else {
+        const responseError = getErrorMessage(response.error);
+        const code = getErrorCode(response.error?.code);
+        return onError({
+          error: responseError,
+          code,
+          severity: ComponentErrorSeverity.ERROR,
+        });
+      }
+    } catch (error) {
+      const code = getErrorCode(error?.code);
+      return onError({
+        error: error.message || error,
+        code,
+        severity: ComponentErrorSeverity.ERROR,
+      });
+    } finally {
+      return final()
+    }
+  };
