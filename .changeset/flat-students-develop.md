1e071176bc08e5f7d383c73e939d4df083527257
Colgan Meanor <colganmeanor7@gmail.com>
2024-12-02 11:37:41 -0600

    Update bank account form schema

---
+++ .changeset/flat-students-develop.md
@@ -0,0 +1,6 @@
+---
+"@justifi/webcomponents": patch
+---
+
+- Updated validation for account owner input field in bank account form step of `justifi-payment-provisioning` component. 
+  - In addition to alphabetical characters, this input will now accept numbers and certain special characters: `,`, `.`, `&`, and `-`
--- packages/webcomponents/src/components/business-forms/schemas/business-bank-account-schema.ts
+++ packages/webcomponents/src/components/business-forms/schemas/business-bank-account-schema.ts
@@ -3,7 +3,7 @@ import {
   accountNumberValidation, 
   accountTypeValidation, 
   bankNameValidation, 
-  identityNameValidation, 
+  businessNameValidation, 
   nicknameValidation, 
   routingNumberValidation } 
 from './schema-validations';
@@ -12,7 +12,7 @@ export const businessBankAccountSchema = (allowOptionalFields?: boolean) => {
   const schema = object({
     bank_name: bankNameValidation.required('Enter bank name'),
     nickname: nicknameValidation.required('Enter nickname'),
-    account_owner_name: identityNameValidation.required('Enter account owner name'),
+    account_owner_name: businessNameValidation.required('Enter account owner name'),
     account_type: accountTypeValidation.required('Select account type'),
     account_number: accountNumberValidation.required('Enter account number'),
     routing_number: routingNumberValidation.required('Enter routing number')
@@ -21,7 +21,7 @@ export const businessBankAccountSchema = (allowOptionalFields?: boolean) => {
   const easySchema = object({
     bank_name: bankNameValidation.nullable(),
     nickname: nicknameValidation.nullable(),
-    account_owner_name: identityNameValidation.nullable(),
+    account_owner_name: businessNameValidation.nullable(),
     account_type: accountTypeValidation.nullable(),
     account_number: accountNumberValidation.nullable(),
     routing_number: routingNumberValidation.nullable()
