4320d9166d39861839ebaef310c6a6c31c5e0631
Tiago Fuelber <tfuelber@gmail.com>
2024-12-05 15:31:11 -0300

    Update Terminals and Checkouts List stories to use the new Theme logic and add missing table-cell part (#771)

--- apps/docs/stories/components/CheckoutsList/index.stories.tsx
+++ apps/docs/stories/components/CheckoutsList/index.stories.tsx
@@ -3,95 +3,7 @@ import { withActions } from "@storybook/addon-actions/decorator";
 import { StoryBaseArgs, customStoryDecorator } from "../../utils";
 
 import "@justifi/webcomponents/dist/module/justifi-checkouts-list";
-
-const themes = {
-  basic: {},
-  custom: {
-    "justifi-checkouts-list::part(label)": {
-      color: "#212529",
-      "font-family": "Calibri, sans-serif",
-      "font-weight": "700",
-      "font-size": ".8rem",
-      margin: "0 0 .5rem 0",
-    },
-    "justifi-checkouts-list::part(input)": {
-      "background-color": "#F4F4F6",
-      "border-color": "rgba(0, 0, 0, 0.42)",
-      "border-bottom-width": "1px",
-      "border-left-width": "0",
-      "border-right-width": "0",
-      "border-top-width": "0",
-      "border-radius": "4px 4px 0 0",
-      "border-style": "solid",
-      "box-shadow": "0 2px 4px rgba(0, 0, 0, 0.2)",
-      color: "#212529",
-      "font-size": ".8rem",
-      "font-weight": "400",
-      "line-height": "2",
-      margin: "0",
-      padding: ".5rem .875rem",
-    },
-    "justifi-checkouts-list::part(input):focus": {
-      color: "#212529",
-      "border-color": "#fccc32",
-      "box-shadow": "none",
-    },
-    "justifi-checkouts-list::part(input-invalid)": {
-      "border-color": "#C12727",
-      "box-shadow": "0 2px 4px rgba(0, 0, 0, 0.2)",
-    },
-    "justifi-checkouts-list::part(input-invalid):focus": {
-      "border-color": "#C12727",
-      "box-shadow": "none",
-    },
-    "justifi-checkouts-list::part(table-head)": {},
-    "justifi-checkouts-list::part(table-head-row)": {},
-    "justifi-checkouts-list::part(table-head-cell)": {
-      "background-color": "#fff",
-      "font-weight": "600",
-      "font-size": "0.8rem",
-      "text-transform": "uppercase",
-      "letter-spacing": "0.1em",
-    },
-    "justifi-checkouts-list::part(table-body)": {},
-    "justifi-checkouts-list::part(table-row)": {},
-    "justifi-checkouts-list::part(table-row):hover": {
-      cursor: "pointer",
-    },
-    "justifi-checkouts-list::part(table-row-even)": {},
-    "justifi-checkouts-list::part(table-row-odd)": {},
-    "justifi-checkouts-list::part(table-cell)": {
-      "background-color": "transparent",
-      "font-weight": "normal",
-      "font-size": "0.8rem",
-    },
-    "justifi-checkouts-list::part(loading-state-cell)": {},
-    "justifi-checkouts-list::part(loading-state-spinner)": {
-      color: "#ccc",
-    },
-    "justifi-checkouts-list::part(error-state)": {},
-    "justifi-checkouts-list::part(empty-state)": {},
-    "justifi-checkouts-list::part(pagination-bar)": {
-      "background-color": "#fff",
-      "border-bottom": "none",
-    },
-    "justifi-checkouts-list::part(page-button)": {
-      border: "none",
-      "background-color": "transparent",
-      "text-transform": "uppercase",
-      "font-weight": "normal",
-      "font-size": "0.8rem",
-    },
-    "justifi-checkouts-list::part(page-button-disabled)": {
-      opacity: "0.5",
-      cursor: "not-allowed",
-    },
-    "justifi-checkouts-list::part(page-arrow)": {
-      display: "none",
-    },
-    "justifi-checkouts-list::part(page-button-text)": {},
-  },
-};
+import { ThemeNames } from "../../themes";
 
 const storyBaseArgs = new StoryBaseArgs(["account-id", "auth-token", "theme"]);
 
@@ -100,9 +12,18 @@ const meta: Meta = {
   component: "justifi-checkouts-list",
   args: {
     ...storyBaseArgs.args,
+    Theme: ThemeNames.Light
   },
   argTypes: {
     ...storyBaseArgs.argTypes,
+    Theme: {
+      description:
+        "Select a theme to preview the component in. [See example](https://storybook.justifi.ai/?path=/docs/introduction--docs#styling-components-with-variables)",
+      options: Object.values(ThemeNames),
+      control: {
+        type: "select",
+      },
+    },
     "checkout-row-clicked": {
       description: "`CheckoutRowClicked`",
       table: {
@@ -138,7 +59,6 @@ const meta: Meta = {
     chromatic: {
       delay: 2000,
     },
-    themes,
   },
   decorators: [
     customStoryDecorator,
