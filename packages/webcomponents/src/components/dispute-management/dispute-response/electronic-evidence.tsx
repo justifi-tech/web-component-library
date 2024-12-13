ff9387ae684ecb2415f46ee99c93095ab7338d69
Jake Merringer <jakemerringer@gmail.com>
2024-12-13 15:21:35 -0600

    make activity logs a file upload

--- packages/webcomponents/src/components/dispute-management/dispute-response/electronic-evidence.tsx
+++ packages/webcomponents/src/components/dispute-management/dispute-response/electronic-evidence.tsx
@@ -1,7 +1,7 @@
 import { Component, h, State, Method, Prop } from "@stencil/core";
 import { FormController } from "../../../ui-components/form/form";
 import ElectronicEvidenceSchema from "./schemas/electronic-evidence-schema";
-import { DisputeEvidenceDocument } from "../../../api/DisputeEvidenceDocument";
+import { DisputeEvidenceDocument, DisputeEvidenceDocumentType } from "../../../api/DisputeEvidenceDocument";
 
 @Component({
   tag: 'justifi-electronic-evidence',
@@ -10,10 +10,11 @@ export class ElectronicEvidence {
   @Prop() disputeResponse: any;
   @State() form: FormController;
   @State() errors: any = {};
+  @State() documentList = [];
 
   @Method()
   async validateAndSubmit(onSuccess: (formData: any, documentList: DisputeEvidenceDocument[]) => void) {
-    this.form.validateAndSubmit((formData) => onSuccess(formData, []));
+    this.form.validateAndSubmit((formData) => onSuccess(formData, this.documentList));
   };
 
   componentWillLoad() {
@@ -24,6 +25,15 @@ export class ElectronicEvidence {
     this.form.errors.subscribe(errors => {
       this.errors = { ...errors };
     });
+  }
+
+  private handleFileSelection = (e: InputEvent) => {
+    const target = e.target as HTMLInputElement;
+    const name = target.name as DisputeEvidenceDocumentType;
+    const files = target.files as unknown as File[];
+    for (const file of files) {
+      this.documentList.push(new DisputeEvidenceDocument(file, name));
+    }
   }
 
   private inputHandler = (name: string, value: string) => {
@@ -51,12 +61,11 @@ export class ElectronicEvidence {
             />
           </div>
           <div class="col-12">
-            <form-control-textarea
+            <form-control-file-v2
               label="Activity Logs"
               name="access_activity_log"
               helpText="Any server or activity logs that provide evidence of the customer's access to or download of the purchased digital product. This information should encompass IP addresses, relevant timestamps, and any detailed records of activity."
-              defaultValue={this.disputeResponse?.access_activity_log}
-              inputHandler={this.inputHandler}
+              onChange={this.handleFileSelection}
               errorText={this.errors.access_activity_log}
             />
           </div>
