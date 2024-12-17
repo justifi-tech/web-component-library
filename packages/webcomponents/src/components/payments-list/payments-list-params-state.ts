1717c7b4e2f2f0abce651513a22ecbfcff3073fa
Colgan Meanor <colganmeanor7@gmail.com>
2024-12-17 15:48:48 -0600

    Update payments params type name

--- packages/webcomponents/src/components/payments-list/payments-list-params-state.ts
+++ packages/webcomponents/src/components/payments-list/payments-list-params-state.ts
@@ -1,9 +1,9 @@
 import { createStore } from '@stencil/store';
-import { PaymentsParams } from '../../api';
+import { PaymentsQueryParams } from '../../api';
 
 const initialState = {};
 
-const paymentsListQueryParams = createStore<PaymentsParams>(() => initialState);
+const paymentsListQueryParams = createStore<PaymentsQueryParams>(() => initialState);
 const { state: queryParams, on: onQueryParamsChange } = paymentsListQueryParams;
 
 const clearParams = () => {
