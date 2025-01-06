d4a69d060fa44de0146410b98af2300dd2332b06
TiagoFuelber <tfuelber@gmail.com>
2025-01-06 18:38:26 -0300

    update table-empty style to include background color

--- packages/webcomponents/src/styles/parts.ts
+++ packages/webcomponents/src/styles/parts.ts
@@ -66,7 +66,7 @@ export const tableCellEven = `table-cell-even ${tableCell} ${text} ${backgroundC
 export const tableCellOdd = `table-cell-odd ${tableCell} ${text} ${backgroundColor}`;
 export const getAlternateTableCellPart = (index) =>
   index % 2 ? tableCellEven : tableCellOdd;
-export const tableEmpty = `table-empty ${text}`;
+export const tableEmpty = `table-empty ${text} ${backgroundColor}`;
 export const tableError = `table-error ${text} ${backgroundColor}`;
 
 // Loading spinner
