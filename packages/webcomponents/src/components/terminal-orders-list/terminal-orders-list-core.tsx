38c4d548e59b28ace4b0385ad7d37ae8dcf322c2
Colgan Meanor <colganmeanor7@gmail.com>
2025-02-14 16:49:52 -0600

    Create terminal-orders-list and core component

---
+++ packages/webcomponents/src/components/terminal-orders-list/terminal-orders-list-core.tsx
@@ -0,0 +1,125 @@
+import { Component, h, Prop, State, Watch, Event, EventEmitter } from '@stencil/core';
+import { Table } from '../../utils/table';
+import { table, tableCell } from '../../styles/parts';
+import { TableEmptyState, TableErrorState, TableLoadingState } from '../../ui-components';
+import { pagingDefaults, PagingInfo } from '../../api';
+
+@Component({
+  tag: 'terminal-orders-list-core',
+})
+
+export class TerminalOrdersListCore {
+  @Prop() getTerminalOrders: Function;
+  @Prop() columns: string;
+
+  @State() terminalOrders: any[] = [];
+  @State() terminalOrdersTable: Table<any>;
+  @State() loading: boolean = true;
+  @State() errorMessage: string;
+  @State() paging: PagingInfo = pagingDefaults;
+  @State() pagingParams: any = {};
+
+  @Watch('pagingParams')
+  @Watch('getTerminalOrders')
+  @Watch('columns')
+  updateOnPropChange() {
+    this.fetchData();
+  }
+
+  @Event({ eventName: 'error-event' }) errorEvent: EventEmitter<any>;
+
+  componentWillLoad() {
+    if (this.getTerminalOrders) {
+      this.fetchData();
+    }
+  }
+
+  fetchData(): void {
+    this.loading = true;
+    console.log('fetching data');
+  }
+
+  handleClickPrevious = (beforeCursor: string) => {
+    this.pagingParams = { before_cursor: beforeCursor };
+  };
+
+  handleClickNext = (afterCursor: string) => {
+    this.pagingParams = { after_cursor: afterCursor };
+  };
+
+  rowClickHandler = (e) => {
+    const clickedOrderId = e.target.closest('tr').dataset.rowEntityId;
+    if (!clickedOrderId) return;
+    console.log('clickedOrderId', clickedOrderId);
+  };
+
+  get entityId() {
+    return this.terminalOrders.map((terminalOrder) => terminalOrder.id);
+  }
+
+  get showEmptyState() {
+    return !this.loading && !this.errorMessage && this.terminalOrdersTable.rowData.length < 1;
+  }
+
+  get showErrorState() {
+    return !this.loading && !!this.errorMessage;
+  }
+
+  get showRowData() {
+    return !this.showEmptyState && !this.showErrorState && !this.loading;
+  }
+
+  render() {
+    return (
+      <div>
+        <div class="table-wrapper">
+          <table class="table table-hover" part={table}>
+            <thead class="table-head sticky-top">
+              <tr class="table-light text-nowrap">
+                {this.terminalOrdersTable.columnData.map((column) => column)}
+              </tr>
+            </thead>
+            <tbody class="table-body">
+              <TableLoadingState
+                columnSpan={this.terminalOrdersTable.columnKeys.length}
+                isLoading={this.loading}
+              />
+              <TableEmptyState
+                isEmpty={this.showEmptyState}
+                columnSpan={this.terminalOrdersTable.columnKeys.length}
+              />
+              <TableErrorState
+                columnSpan={this.terminalOrdersTable.columnKeys.length}
+                errorMessage={this.errorMessage}
+              />
+              {this.showRowData && this.terminalOrdersTable.rowData.map((data, index) => (
+                <tr
+                  data-test-id="table-row"
+                  data-row-entity-id={this.entityId[index]}
+                  onClick={this.rowClickHandler}
+                >
+                  {data}
+                </tr>
+              ))}
+            </tbody>
+            {this.paging && (
+              <tfoot class="sticky-bottom">
+                <tr class="table-light align-middle">
+                  <td part={tableCell} colSpan={this.terminalOrdersTable.columnData.length}>
+                    <pagination-menu
+                      paging={{
+                        ...this.paging,
+                        handleClickPrevious: this.handleClickPrevious,
+                        handleClickNext: this.handleClickNext,
+                      }}
+                    />
+                  </td>
+                </tr>
+              </tfoot>
+            )}
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ packages/webcomponents/src/components/terminal-orders-list/terminal-orders-list.tsx
@@ -0,0 +1,44 @@
+import { Component, h, Prop, State } from '@stencil/core';
+import { StyledHost } from '../../ui-components';
+import { checkPkgVersion } from '../../utils/check-pkg-version';
+
+@Component({
+  tag: 'justifi-terminal-orders-list',
+  shadow: true
+})
+
+export class TerminalOrdersList {
+  @State() getTerminalOrders: Function;
+  @State() errorMessage: string = null;
+
+  @Prop() businessId: string;
+  @Prop() authToken: string;
+  @Prop() apiOrigin?: string = PROXY_API_ORIGIN;
+  @Prop() columns?: string;
+  
+  componentWillLoad() {
+    checkPkgVersion();
+    this.initializeGetTerminalOrders();
+  }
+
+  private initializeGetTerminalOrders() {
+    if (this.businessId && this.authToken) {
+      this.getTerminalOrders = () => {
+        console.log('getTerminalOrders');
+      }
+    } else {
+      this.errorMessage = 'Business ID and Auth Token are required';
+    }
+  }
+
+  render() {
+    return (
+      <StyledHost>
+        <terminal-orders-list-core 
+          getTerminalOrders={this.getTerminalOrders} 
+          columns={this.columns}
+        />
+      </StyledHost>
+    );
+  }
+}
