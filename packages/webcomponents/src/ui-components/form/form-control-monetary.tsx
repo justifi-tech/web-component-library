492d03753cd5fbceee5ede73b7e98854b1f61550
Jake Merringer <jakemerringer@gmail.com>
2025-03-12 13:44:27 -0500

    add currency adornment to form-control-monetary

--- packages/webcomponents/src/ui-components/form/form-control-monetary.tsx
+++ packages/webcomponents/src/ui-components/form/form-control-monetary.tsx
@@ -9,7 +9,7 @@ import {
   State,
 } from '@stencil/core';
 import { FormControlErrorText } from '..';
-import { input, inputDisabled, inputFocused, inputInvalid, label } from '../../styles/parts';
+import { input, inputDisabled, inputFocused, inputInvalid, label, text } from '../../styles/parts';
 import { formatCurrencyNoSymbols } from '../../utils/utils';
 
 @Component({
@@ -79,33 +79,34 @@ export class MonetaryInput {
     return (
       <Host>
         <div class="form-group d-flex flex-column">
-          <div class="d-flex align-items-start gap-2">
-            <label part={label} class="form-label" htmlFor={this.name}>
-              {this.label}
-            </label>
-            <form-control-tooltip helpText={this.helpText} />
+          <label part={label} class="form-label" htmlFor={this.name}>
+            {this.label}
+          </label>
+          <form-control-tooltip helpText={this.helpText} />
+          <div class="input-group">
+            <span class="input-group-text" part={text}>$</span>
+            <input
+              ref={el => this.formControl = el}
+              id={this.name}
+              name={this.name}
+              onFocus={() => this.isFocused = true}
+              onBlur={() => {
+                this.isFocused = false;
+                this.formControlBlur.emit();
+              }}
+              onInput={(event: Event) => this.updateInput((event.target as HTMLInputElement).value)}
+              onKeyDown={this.keyDownHandler}
+              onPaste={this.keyDownHandler}
+              maxLength={this.maxLength}
+              part={this.part}
+              class={this.errorText ? "form-control is-invalid" : "form-control"}
+              type="text"
+              disabled={this.disabled}
+            />
           </div>
-          <input
-            ref={el => this.formControl = el}
-            id={this.name}
-            name={this.name}
-            onFocus={() => this.isFocused = true}
-            onBlur={() => {
-              this.isFocused = false;
-              this.formControlBlur.emit();
-            }}
-            onInput={(event: Event) => this.updateInput((event.target as HTMLInputElement).value)}
-            onKeyDown={this.keyDownHandler}
-            onPaste={this.keyDownHandler}
-            maxLength={this.maxLength}
-            part={this.part}
-            class={this.errorText ? "form-control is-invalid" : "form-control"}
-            type="text"
-            disabled={this.disabled}
-          />
           <FormControlErrorText errorText={this.errorText} name={this.name} />
         </div>
-      </Host>
+      </Host >
     );
   }
 }
