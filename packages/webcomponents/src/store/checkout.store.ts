45ca5a9c09d6caf48876f9138a52737cb69fd4d3
Tiago Fuelber <tfuelber@gmail.com>
2025-05-13 15:08:45 -0300

    Implement checkout summary component and enhance checkout store with payment details (#1032)

--- packages/webcomponents/src/store/checkout.store.ts
+++ packages/webcomponents/src/store/checkout.store.ts
@@ -3,6 +3,10 @@ import { createStore } from '@stencil/store';
 const { state, onChange } = createStore({
   authToken: '',
   accountId: '',
+  paymentMethods: [],
+  paymentMethodGroupId: '',
+  paymentDescription: '',
+  totalAmount: 0,
 });
 
 onChange('authToken', (newValue) => {
@@ -11,6 +15,14 @@ onChange('authToken', (newValue) => {
 
 onChange('accountId', (newValue) => {
   state.accountId = newValue;
+});
+
+onChange('paymentDescription', (newValue) => {
+  state.paymentDescription = newValue;
+});
+
+onChange('totalAmount', (newValue) => {
+  state.totalAmount = newValue;
 });
 
 export default state;
