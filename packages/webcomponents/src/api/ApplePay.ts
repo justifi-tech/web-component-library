d1f79987e420f532d8476f0bf5fab9128e3be03b
Ignacio Contreres <ignaciocontreres@192.168.1.13>
2025-06-05 15:40:45 -0300

    removed unused apple session statuses

--- packages/webcomponents/src/api/ApplePay.ts
+++ packages/webcomponents/src/api/ApplePay.ts
@@ -361,12 +361,6 @@ declare global {
       supportsVersion(version: number): boolean;
       STATUS_SUCCESS: number;
       STATUS_FAILURE: number;
-      STATUS_INVALID_BILLING_POSTAL_ADDRESS: number;
-      STATUS_INVALID_SHIPPING_POSTAL_ADDRESS: number;
-      STATUS_INVALID_SHIPPING_CONTACT: number;
-      STATUS_PIN_REQUIRED: number;
-      STATUS_PIN_INCORRECT: number;
-      STATUS_PIN_LOCKOUT: number;
     };
   }
 }
