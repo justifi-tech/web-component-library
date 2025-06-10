4546e7bef416ec1801473397511bd6d4e42c7fd0
TiagoFuelber <tfuelber@gmail.com>
2025-06-10 20:16:55 -0300

    Refactor Checkout component to wrap justifi-modular-checkout with StyledHost for improved styling and structure. Updated snapshot tests to reflect changes in the rendered output.

--- packages/webcomponents/src/components/checkout/checkout.tsx
+++ packages/webcomponents/src/components/checkout/checkout.tsx
@@ -13,6 +13,7 @@ import { insuranceValues, insuranceValuesOn, validateInsuranceValues } from '../
 import { PaymentMethodTypes } from '../../api/Payment';
 import { PaymentMethodOption } from './payment-method-option-utils';
 import { PaymentMethodPayload } from './payment-method-payload';
+import { StyledHost } from '../../ui-components';
 
 const PaymentMethodTypeLabels = {
   bankAccount: 'New bank account',
@@ -190,74 +191,76 @@ export class Checkout {
     const showPaymentTypeHeader = !this.disableCreditCard && !this.disableBankAccount;
 
     return (
-      <justifi-modular-checkout
-        ref={(el) => (this.modularCheckoutRef = el)}
-        authToken={this.authToken}
-        checkoutId={this.checkoutId}
-      >
-        <div class="row gy-3 jfi-checkout-core">
-          <div class="col-12 mb-4" part={checkoutSummary}>
-            <justifi-header text="Summary" level="h2" class="fs-5 fw-bold pb-3" />
-            <section class="mb-4">
-              {this.isLoading && (
-                <justifi-skeleton height="24px" />
-              )}
-              {!this.isLoading && <justifi-checkout-summary />}
-            </section>
-          </div>
-          <div class="col-12 mt-4">
-            <slot name="insurance"></slot>
-          </div>
-          <div class="col-12 mt-4">
-            <justifi-header text="Payment" level="h2" class="fs-5 fw-bold pb-3" />
-            {showPaymentTypeHeader && (
-              <justifi-header text="Select payment type" level="h3" class="fs-6 fw-bold lh-lg" />
-            )}
-            <div class="d-flex flex-column">
-              <section>
-                {/* For now, just return nothing to avoid breaking, but we can decide to show an error message here */}
-                {/* <div style={{ color: 'red' }}>Error: {this.serverError}</div>; */}
+      <StyledHost>
+        <justifi-modular-checkout
+          ref={(el) => (this.modularCheckoutRef = el)}
+          authToken={this.authToken}
+          checkoutId={this.checkoutId}
+        >
+          <div class="row gy-3 jfi-checkout-core">
+            <div class="col-12 mb-4" part={checkoutSummary}>
+              <justifi-header text="Summary" level="h2" class="fs-5 fw-bold pb-3" />
+              <section class="mb-4">
                 {this.isLoading && (
-                  <justifi-skeleton height="300px" />
+                  <justifi-skeleton height="24px" />
                 )}
-                {!this.isLoading && (
-                  <div>
-                    <justifi-saved-payment-methods />
-                    <justifi-sezzle-payment-method />
-                    <payment-method-option
-                      paymentMethodOptionId={PaymentMethodTypes.card}
-                      isSelected={checkoutStore.selectedPaymentMethod === PaymentMethodTypes.card}
-                      clickHandler={() => { checkoutStore.selectedPaymentMethod = PaymentMethodTypes.card }}
-                      radioButtonHidden={this.hiddenRadioInput}
-                      label={PaymentMethodTypeLabels[PaymentMethodTypes.card]}
-                    />
-                    <payment-method-option
-                      paymentMethodOptionId={PaymentMethodTypes.bankAccount}
-                      isSelected={checkoutStore.selectedPaymentMethod === PaymentMethodTypes.bankAccount}
-                      clickHandler={() => { checkoutStore.selectedPaymentMethod = PaymentMethodTypes.bankAccount }}
-                      radioButtonHidden={this.hiddenRadioInput}
-                      label={PaymentMethodTypeLabels[PaymentMethodTypes.bankAccount]}
-                    />
-                  </div>
-                )}
+                {!this.isLoading && <justifi-checkout-summary />}
               </section>
             </div>
-          </div>
-          <div class="col-12">
-            <div class="d-flex justify-content-end">
-              <justifi-button
-                text="Pay"
-                type="submit"
-                variant="primary"
-                clickHandler={(e) => this.submit(e)}
-                disabled={this.isLoading}
-                isLoading={this.isLoading}
-                customStyle={{ width: '100%', textAlign: "center" }}
-              />
+            <div class="col-12 mt-4">
+              <slot name="insurance"></slot>
+            </div>
+            <div class="col-12 mt-4">
+              <justifi-header text="Payment" level="h2" class="fs-5 fw-bold pb-3" />
+              {showPaymentTypeHeader && (
+                <justifi-header text="Select payment type" level="h3" class="fs-6 fw-bold lh-lg" />
+              )}
+              <div class="d-flex flex-column">
+                <section>
+                  {/* For now, just return nothing to avoid breaking, but we can decide to show an error message here */}
+                  {/* <div style={{ color: 'red' }}>Error: {this.serverError}</div>; */}
+                  {this.isLoading && (
+                    <justifi-skeleton height="300px" />
+                  )}
+                  {!this.isLoading && (
+                    <div>
+                      <justifi-saved-payment-methods />
+                      <justifi-sezzle-payment-method />
+                      <payment-method-option
+                        paymentMethodOptionId={PaymentMethodTypes.card}
+                        isSelected={checkoutStore.selectedPaymentMethod === PaymentMethodTypes.card}
+                        clickHandler={() => { checkoutStore.selectedPaymentMethod = PaymentMethodTypes.card }}
+                        radioButtonHidden={this.hiddenRadioInput}
+                        label={PaymentMethodTypeLabels[PaymentMethodTypes.card]}
+                      />
+                      <payment-method-option
+                        paymentMethodOptionId={PaymentMethodTypes.bankAccount}
+                        isSelected={checkoutStore.selectedPaymentMethod === PaymentMethodTypes.bankAccount}
+                        clickHandler={() => { checkoutStore.selectedPaymentMethod = PaymentMethodTypes.bankAccount }}
+                        radioButtonHidden={this.hiddenRadioInput}
+                        label={PaymentMethodTypeLabels[PaymentMethodTypes.bankAccount]}
+                      />
+                    </div>
+                  )}
+                </section>
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="d-flex justify-content-end">
+                <justifi-button
+                  text="Pay"
+                  type="submit"
+                  variant="primary"
+                  clickHandler={(e) => this.submit(e)}
+                  disabled={this.isLoading}
+                  isLoading={this.isLoading}
+                  customStyle={{ width: '100%', textAlign: "center" }}
+                />
+              </div>
             </div>
           </div>
-        </div>
-      </justifi-modular-checkout>
+        </justifi-modular-checkout>
+      </StyledHost>
     );
   }
 }
