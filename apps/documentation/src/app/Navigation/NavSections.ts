3e9acc2a037f2b3e81ca44a19ffc32a0f185ba17
Jake Merringer <jakemerringer@gmail.com>
2025-06-18 16:26:46 -0500

    Refactor Sidebar component by extracting navigation sections into a separate file
    - Moved the `navSections` array from `Sidebar.tsx` to a new `NavSections.tsx` file for better organization and maintainability.
    - Updated the import statement in `Sidebar.tsx` to reference the new location of `navSections`.

---
+++ apps/documentation/src/app/Navigation/NavSections.ts
@@ -0,0 +1,32 @@
+export const navSections = [
+  {
+    label: "Getting Started",
+    links: [
+      { label: "Introduction", href: "/" },
+      { label: "Installation", href: "/installation" },
+      { label: "Styling", href: "/styling" },
+    ],
+  },
+  {
+    label: "Components",
+    links: [
+      { label: "Business Details", href: "/components/business-details" },
+      { label: "Business Form", href: "/components/business-form" },
+      { label: "Checkout", href: "/components/checkout" },
+      { label: "Checkouts List", href: "/components/checkouts-list" },
+      { label: "Dispute Management", href: "/components/dispute-management" },
+      { label: "Gross Payment Chart", href: "/components/gross-payment-chart" },
+      { label: "Order Terminals", href: "/components/order-terminals" },
+      { label: "Payment Details", href: "/components/payment-details" },
+      { label: "Payment Provisioning", href: "/components/payment-provisioning" },
+      { label: "Payments List", href: "/components/payments-list" },
+      { label: "Payout Details", href: "/components/payout-details" },
+      { label: "Payouts List", href: "/components/payouts-list" },
+      { label: "Refund Payment", href: "/components/refund-payment" },
+      { label: "Season Interruption Insurance", href: "/components/season-interruption-insurance" },
+      { label: "Terminal Orders List", href: "/components/terminal-orders-list" },
+      { label: "Terminals List", href: "/components/terminals-list" },
+      { label: "Tokenize Payment Method", href: "/components/tokenize-payment-method" },
+    ],
+  }
+];
