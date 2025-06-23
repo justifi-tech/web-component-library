0319b449b3a822c49837e19dafd01f7deb90b23d
Colgan Meanor <colganmeanor7@gmail.com>
2025-06-23 12:18:46 -0500

    Add error messages enum in ComponentError.ts

--- packages/webcomponents/src/api/ComponentError.ts
+++ packages/webcomponents/src/api/ComponentError.ts
@@ -22,6 +22,7 @@ export enum ComponentErrorSeverity {
 }
 
 export enum ComponentErrorMessages {
+  NOT_AUTHENTICATED = 'Not authenticated. Please provide a valid auth token, checkout id, and account id.',
   CHECKOUT_ALREADY_COMPLETED = 'Checkout already completed. Please create a new checkout.',
   CHECKOUT_EXPIRED = 'Checkout expired. Please create a new checkout.',
 }
