297f9b37aa6e02e882f4757f8552848bc69a91f4
TiagoFuelber <tfuelber@gmail.com>
2025-07-25 10:24:58 -0300

    Remove unused API paths file from component examples

--- apps/component-examples/utils/api-paths.js
+++
@@ -1,9 +0,0 @@
-const API_PATHS = {
-  AUTH_TOKEN: 'oauth/token',
-  WEB_COMPONENT_TOKEN: 'v1/web_component_tokens',
-  CHECKOUT: 'v1/checkouts',
-  BUSINESS: 'v1/entities/business',
-  PAYMENTS: 'v1/payments',
-};
-
-module.exports = { API_PATHS };
