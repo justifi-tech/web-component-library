514511f4b2d74509aade3e01947374f5fcab5512
Caio Rosa <>
2025-08-01 19:29:52 -0300

    apple pay config changes

--- apps/component-examples/examples/modular-checkout.js
+++ apps/component-examples/examples/modular-checkout.js
@@ -2,15 +2,18 @@ require('dotenv').config({ path: '../../.env' });
 const express = require('express');
 const { API_PATHS } = require('../utils/api-paths');
 
+const fs = require('fs');
+const https = require('https');
+
 const app = express();
 const port = process.env.PORT || 3000;
-const checkoutEndpoint = `${process.env.API_ORIGIN}/${API_PATHS.CHECKOUT}`;
-const authTokenEndpoint = `${process.env.API_ORIGIN}/${API_PATHS.AUTH_TOKEN}`;
-const webComponentTokenEndpoint = `${process.env.API_ORIGIN}/${API_PATHS.WEB_COMPONENT_TOKEN}`;
+const checkoutEndpoint = `https://api.justifi-staging.com/v1/checkouts`;
+const authTokenEndpoint = `https://api.justifi-staging.com/oauth/token`;
+const webComponentTokenEndpoint = `https://api.justifi-staging.com/v1/web_component_tokens`;
 const paymentMethodGroupId = process.env.PAYMENT_METHOD_GROUP_ID;
-const subAccountId = process.env.SUB_ACCOUNT_ID;
-const clientId = process.env.CLIENT_ID;
-const clientSecret = process.env.CLIENT_SECRET;
+const subAccountId = "acc_323sM3WDAUnHH0fJD7re9h";
+const clientId = "test_eohVQllq0mJy34T14dsZJlHjXKr7rKyH";
+const clientSecret = "test_OZK_PtLPuTS6NM2X9aX9qI6FQpxhuWaCBMWGPpJw0Y3haKiOi-5GECGMNRQ-d22Z";
 
 app.use(
   '/scripts',
@@ -53,7 +56,6 @@ async function makeCheckout(token) {
     body: JSON.stringify({
       amount: 1799,
       description: 'One Chocolate Donut',
-      payment_method_group_id: paymentMethodGroupId,
       origin_url: `localhost:${port}`,
     }),
   });
@@ -94,43 +96,31 @@ app.get('/', async (req, res) => {
         <script type="module" src="/scripts/webcomponents/webcomponents.esm.js"></script>
         <link rel="stylesheet" href="/styles/theme.css">
         <link rel="stylesheet" href="/styles/example.css">
+        <script crossorigin 
+          src="https://applepay.cdn-apple.com/jsapi/1.latest/apple-pay-sdk.js" crossorigin="anonymous"> 
+        </script>
       </head>
       <body class="two-column-layout">
         <div class="column-preview">
           <justifi-modular-checkout auth-token="${webComponentToken}" checkout-id="${checkout.id}">
-            <justifi-card-form></justifi-card-form>
-            <div style="margin-top: 20px">
-              <button
-               id="submit-button" 
-               class="button"
-               style="padding:10px"
-              >
-                Submit Checkout
-              </button>
-            </div>
+              <justifi-apple-pay
+                button-style="black"
+                button-type="plain"
+                merchant-identifier="merchant.tech.justifi.checkout"
+                merchant-display-name="JustiFi Checkout"
+              />
           </justifi-modular-checkout>
         </div>
       </body>
-      <script>
-        const submitButton = document.getElementById('submit-button');
-        const checkoutWrapper = document.querySelector('justifi-modular-checkout');
-
-        submitButton.addEventListener('click', async () => {
-          await checkoutWrapper.submitCheckout({ address_postal_code: '12345' });
-        });
-
-        checkoutWrapper.addEventListener('submit-event', (e) => {
-          console.log('submit-event: ', e);
-        });
-
-        checkoutWrapper.addEventListener('error-event', (e) => {
-          console.log('error-event: ', e);
-        });
-      </script>
     </html>
   `);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+var server = https.createServer({
+  key: fs.readFileSync("/Users/caio/Documents/web-component-library/server.key"),
+  cert: fs.readFileSync("/Users/caio/Documents/web-component-library/server.cert"),
+}, app)
+
+server.listen(443, () => {
+  console.log("Running https")
+})
