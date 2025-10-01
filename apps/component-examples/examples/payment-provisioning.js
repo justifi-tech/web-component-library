2cf78a3c37ba40ea7f79353a907def361e1c2e3d
Jake Merringer <jakemerringer@gmail.com>
2025-10-01 10:31:56 -0500

    fix payment-provisioning example

--- apps/component-examples/examples/payment-provisioning.js
+++ apps/component-examples/examples/payment-provisioning.js
@@ -50,11 +50,10 @@ async function createBusiness(token) {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`,
-      'Sub-Account': subAccountId
     },
     body: JSON.stringify({
       legal_name: randomLegalName,
-      country_of_establishment: 'CAN',
+      country_of_establishment: 'USA',
     }),
   });
   const res = await response.json();
