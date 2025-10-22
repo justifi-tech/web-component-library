991c78c026e4e1034d8b7bb522c60704e0c5f67a
Rodrigo Eidelvein <rodrigoeidelvein@gmail.com>
2025-10-22 11:12:33 -0300

    docs: Add docs for Payments and Payouts transactions list (#1162)
    * Add docs for payments-transactions-list
    
    * Add docs for Payouts transactions list

---
+++ apps/docs/stories/components/PaymentTransactionsList/index.stories.tsx
@@ -0,0 +1,61 @@
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { withActions } from "@storybook/addon-actions/decorator";
+import { StoryBaseArgs, customStoryDecorator } from "../../utils";
+
+import "@justifi/webcomponents/dist/module/justifi-payment-transactions-list";
+import { ThemeNames } from "../../themes";
+
+type Story = StoryObj;
+
+const storyBaseArgs = new StoryBaseArgs(["payment-id", "auth-token"]);
+
+const meta: Meta = {
+  title: "Merchant Tools/Payment Transactions List",
+  component: "justifi-payment-transactions-list",
+  args: {
+    ...storyBaseArgs.args,
+    Theme: ThemeNames.Light,
+  },
+  argTypes: {
+    ...storyBaseArgs.argTypes,
+    Theme: {
+      description:
+        "Select a theme to preview the component in. [See example](https://storybook.justifi.ai/?path=/docs/introduction--docs#styling)",
+      options: Object.values(ThemeNames),
+      control: {
+        type: "select",
+      },
+    },
+    "click-event": {
+      description: "`ComponentClickEvent`",
+      table: {
+        category: "events",
+      },
+      action: true,
+    },
+    "error-event": {
+      description: "`ComponentError`",
+      table: {
+        category: "events",
+      },
+      action: true,
+    },
+  },
+  parameters: {
+    actions: {
+      handles: ["click-event", "error-event"],
+    },
+    chromatic: {
+      delay: 2000,
+    },
+  },
+  decorators: [
+    customStoryDecorator,
+    // @ts-ignore
+    withActions, // https://github.com/storybookjs/storybook/issues/22384
+  ],
+};
+
+export const Example: Story = {};
+
+export default meta;
---
+++ apps/docs/stories/components/PayoutTransactionsList/index.stories.tsx
@@ -0,0 +1,61 @@
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { withActions } from "@storybook/addon-actions/decorator";
+import { StoryBaseArgs, customStoryDecorator } from "../../utils";
+
+import "@justifi/webcomponents/dist/module/justifi-payout-transactions-list";
+import { ThemeNames } from "../../themes";
+
+type Story = StoryObj;
+
+const storyBaseArgs = new StoryBaseArgs(["payout-id", "auth-token"]);
+
+const meta: Meta = {
+  title: "Merchant Tools/Payout Transactions List",
+  component: "justifi-payout-transactions-list",
+  args: {
+    ...storyBaseArgs.args,
+    Theme: ThemeNames.Light,
+  },
+  argTypes: {
+    ...storyBaseArgs.argTypes,
+    Theme: {
+      description:
+        "Select a theme to preview the component in. [See example](https://storybook.justifi.ai/?path=/docs/introduction--docs#styling)",
+      options: Object.values(ThemeNames),
+      control: {
+        type: "select",
+      },
+    },
+    "click-event": {
+      description: "`ComponentClickEvent`",
+      table: {
+        category: "events",
+      },
+      action: true,
+    },
+    "error-event": {
+      description: "`ComponentError`",
+      table: {
+        category: "events",
+      },
+      action: true,
+    },
+  },
+  parameters: {
+    actions: {
+      handles: ["click-event", "error-event"],
+    },
+    chromatic: {
+      delay: 2000,
+    },
+  },
+  decorators: [
+    customStoryDecorator,
+    // @ts-ignore
+    withActions, // https://github.com/storybookjs/storybook/issues/22384
+  ],
+};
+
+export const Example: Story = {};
+
+export default meta;
