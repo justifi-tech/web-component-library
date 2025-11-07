17df22adf3403ed5ebec3dec2b25f56b278f5f68
Caio Rosa <>
2025-11-07 13:10:46 -0300

    test: dont use pmg

--- apps/component-examples/examples/checkout.js
+++ apps/component-examples/examples/checkout.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 3000;
 const checkoutEndpoint = `${process.env.API_ORIGIN}/${API_PATHS.CHECKOUT}`;
 const authTokenEndpoint = `${process.env.API_ORIGIN}/${API_PATHS.AUTH_TOKEN}`;
 const webComponentTokenEndpoint = `${process.env.API_ORIGIN}/${API_PATHS.WEB_COMPONENT_TOKEN}`;
-const paymentMethodGroupId = process.env.PAYMENT_METHOD_GROUP_ID;
 const subAccountId = process.env.SUB_ACCOUNT_ID;
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
@@ -57,7 +56,6 @@ async function makeCheckout(token) {
       body: JSON.stringify({
         amount: 1799,
         description: 'One Chocolate Donut',
-        payment_method_group_id: paymentMethodGroupId,
         origin_url: `localhost:${port}`,
       }),
     });
