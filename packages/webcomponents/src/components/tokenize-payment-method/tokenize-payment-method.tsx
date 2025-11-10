a25a14fa38a92c8210afc89e4ae0eee322fe13c0
Rodrigo Eidelvein <rodrigoeidelvein@gmail.com>
2025-11-10 15:24:09 -0300

    Migrate first part of functions from tokenize to checkout

--- packages/webcomponents/src/components/tokenize-payment-method/tokenize-payment-method.tsx
+++ packages/webcomponents/src/components/tokenize-payment-method/tokenize-payment-method.tsx
@@ -12,19 +12,7 @@ import {
 } from '../../api';
 import { checkoutStore } from '../../store/checkout.store';
 import { PAYMENT_METHODS, SelectedPaymentMethod } from '../modular-checkout/ModularCheckout';
-
-// Constants
-const PAYMENT_METHOD_TYPE_LABELS = {
-  [PAYMENT_METHODS.NEW_BANK_ACCOUNT]: 'New bank account',
-  [PAYMENT_METHODS.NEW_CARD]: 'New credit or debit card',
-} as const;
-
-const ERROR_MESSAGES = {
-  AUTH_TOKEN_REQUIRED: 'Auth token is required when using the tokenize-payment-method component not slotted in justifi-modular-checkout',
-  ACCOUNT_ID_REQUIRED: 'Account ID is required when using the tokenize-payment-method component not slotted in justifi-modular-checkout',
-  FORM_NOT_READY: 'Payment form not ready',
-  VALIDATION_ERROR: 'Validation error',
-} as const;
+import { ERROR_MESSAGES, PAYMENT_METHOD_TYPE_LABELS } from '../checkout/message-event-types';
 
 // Types
 interface ValidationResult {
