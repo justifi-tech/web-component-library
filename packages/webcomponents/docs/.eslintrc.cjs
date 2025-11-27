3428d47d9ccbded5b706cc125a1e2738a2616845
TiagoFuelber <tfuelber@gmail.com>
2025-11-27 18:35:54 -0300

    Update dependencies in webcomponents package and enhance ESLint configuration. Added new ESLint packages and updated espree version, while refining ESLint setup for improved compatibility with MDX files. Also, corrected documentation link in bank-account-form.mdx for better navigation.

--- packages/webcomponents/docs/.eslintrc.cjs
+++ packages/webcomponents/docs/.eslintrc.cjs
@@ -1,13 +1,9 @@
-const mdx = require('eslint-plugin-mdx');
+const { FlatCompat } = require('@eslint/eslintrc');
+const compat = new FlatCompat();
 
 module.exports = [
+  ...compat.extends('plugin:mdx/recommended'),
   {
-    files: ['**/*.md', '**/*.mdx'],
-    plugins: { mdx },
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
     rules: {
       'mdx/no-unused-expressions': 'off',
       'mdx/no-jsx-html-comments': 'off',
