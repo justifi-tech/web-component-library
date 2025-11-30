667cd7535fa18d11726c601f1e38277cc5540328
TiagoFuelber <tfuelber@gmail.com>
2025-11-30 19:22:01 -0300

    Add BusinessDetailsExample component and update documentation

--- packages/webcomponents/docs/helpers/index.ts
+++ packages/webcomponents/docs/helpers/index.ts
@@ -1,3 +1,5 @@
 export { PropsTable } from './PropsTable';
 export { UsageSnippet } from './UsageSnippet';
 export { PartsTable } from './PartsTable';
+export { ComponentExample } from './ComponentExample';
+export { BusinessDetailsExample } from './BusinessDetailsExample';
