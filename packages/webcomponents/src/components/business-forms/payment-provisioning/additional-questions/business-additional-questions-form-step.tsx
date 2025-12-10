52b4b84a9c61980e71bbf26d9c33efef191afc22
Rodrigo Eidelvein <rodrigoeidelvein@gmail.com>
2025-12-10 11:32:47 -0300

    Format business payment volume field

--- packages/webcomponents/src/components/business-forms/payment-provisioning/additional-questions/business-additional-questions-form-step.tsx
+++ packages/webcomponents/src/components/business-forms/payment-provisioning/additional-questions/business-additional-questions-form-step.tsx
@@ -170,12 +170,13 @@ export class AdditionalQuestionsFormStep {
               />
             </div>
             <div class="col-12 col-md-6">
-              <form-control-text
+              <form-control-monetary-provisioning
                 name="business_payment_volume"
                 label="What is your business' annual credit card & ACH volume anticipated to process?"
                 inputHandler={this.inputHandler}
                 errorText={this.errors?.business_payment_volume}
                 defaultValue={additionalQuestionsDefaultValue?.business_payment_volume}
+                maskOptions={CURRENCY_MASK.WHOLE}
               />
             </div>
             <div class="col-12">
