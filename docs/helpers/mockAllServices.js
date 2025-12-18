93594c66329adeb74d214d05a3a5ab5c93b47782
TiagoFuelber <tfuelber@gmail.com>
2025-12-18 13:30:49 -0300

    add payouts list component example
    # This PR adds the following changes:
    
    - Render the payouts list copmonent example.
    - Updated `mockAllServices.js` to return mock data on API calls.
    - Added `mockPayoutsSuccess.json`  mock file.

--- docs/helpers/mockAllServices.js
+++ docs/helpers/mockAllServices.js
@@ -1,8 +1,11 @@
 import { createServer } from 'miragejs';
 import mockBusinessDetails from '../mocks/mockBusinessDetails.json';
+import mockPayouts from '../mocks/mockPayoutsSuccess.json';
+import mockNPMVersion from '../mocks/mockNPMVersion.json';
 
 export const API_PATHS = {
   BUSINESS_DETAILS: '/entities/business/:id',
+  PAYOUTS_LIST: '/account/:id/payouts',
   PKG_VERSION: '/@justifi/webcomponents/latest',
 };
 
@@ -15,6 +18,9 @@ export const setUpMocks = () => {
 
       // BusinessDetails
       this.get(API_PATHS.BUSINESS_DETAILS, () => mockBusinessDetails);
+
+      // PayoutsList
+      this.get(API_PATHS.PAYOUTS_LIST, () => mockPayouts);
 
       // URL Prefix for NPM Package Check
       this.namespace = ''; // Reset the namespace to avoid prefixing with the primary URL prefix
