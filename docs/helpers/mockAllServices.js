7a1b4a213fa0e2921fc1e866d1a9496caff1fcc2
TiagoFuelber <tfuelber@gmail.com>
2025-12-18 16:20:32 -0300

    add tokenize payment method component example # This PR adds the following changes:
    - Render the tokenize payment method copmonent example.
    - Updated `mockAllServices.js` to return mock data on API calls.
    - Added `mockTokenizePaymentMethodSuccess.json`  mock file.

--- docs/helpers/mockAllServices.js
+++ docs/helpers/mockAllServices.js
@@ -2,11 +2,13 @@ import { createServer } from 'miragejs';
 import mockBusinessDetails from '../mocks/mockBusinessDetails.json';
 import mockTerminals from '../mocks/mockTerminalsListSuccess.json';
 import mockNPMVersion from '../mocks/mockNPMVersion.json';
+import mockPostPaymentMethods from '../mocks/mockPostPaymentMethodsSuccess.json';
 
 export const API_PATHS = {
   BUSINESS_DETAILS: '/entities/business/:id',
   TERMINALS_LIST: 'terminals',
   PKG_VERSION: '/@justifi/webcomponents/latest',
+  PAYMENT_METHODS: '/payment_methods',
 };
 
 export const setUpMocks = () => {
@@ -21,6 +23,18 @@ export const setUpMocks = () => {
 
       // TerminalsList
       this.get(API_PATHS.TERMINALS_LIST, () => mockTerminals);
+
+      // URL prefix for direct API requests (for iframed application requests which don't use the proxy)
+      // As of now this does not work because the request happens within the iframe and the iframe does not have access to the proxy
+      this.namespace = '/v1/js'; // Reset the namespace to avoid prefixing with the primary URL prefix
+      this.urlPrefix = 'https://api.justifi.ai';
+
+      // PaymentMethods (TokenizePaymentMethod component)
+      this.post(API_PATHS.PAYMENT_METHODS, () => mockPostPaymentMethods);
+
+      // Reset namespace and URL prefix for other endpoints
+      this.namespace = '/v1';
+      this.urlPrefix = 'https://wc-proxy.justifi.ai';
 
       // URL Prefix for NPM Package Check
       this.namespace = ''; // Reset the namespace to avoid prefixing with the primary URL prefix
