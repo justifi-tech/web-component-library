d372b931245f666e8c5c973886e0c5d60855820c
TiagoFuelber <tfuelber@gmail.com>
2025-12-18 11:59:37 -0300

    add payment transactions list component example
    # This PR adds the following changes:
    
    - Render payment transactions list example.
    - Update `mockAllServices.js` to handle the right endpoints.
    - Add `mockPaymentTransactionsSuccess.json` mock

--- docs/helpers/mockAllServices.js
+++ docs/helpers/mockAllServices.js
@@ -1,8 +1,11 @@
 import { createServer } from 'miragejs';
 import mockBusinessDetails from '../mocks/mockBusinessDetails.json';
+import mockPaymentTransactions from '../mocks/mockPaymentTransactionsSuccess.json';
+import mockNPMVersion from '../mocks/mockNPMVersion.json';
 
 export const API_PATHS = {
   BUSINESS_DETAILS: '/entities/business/:id',
+  PAYMENT_TRANSACTIONS: '/payments/:id/payment_balance_transactions',
   PKG_VERSION: '/@justifi/webcomponents/latest',
 };
 
@@ -15,6 +18,9 @@ export const setUpMocks = () => {
 
       // BusinessDetails
       this.get(API_PATHS.BUSINESS_DETAILS, () => mockBusinessDetails);
+
+      // PaymentTransactions
+      this.get(API_PATHS.PAYMENT_TRANSACTIONS, () => mockPaymentTransactions);
 
       // URL Prefix for NPM Package Check
       this.namespace = ''; // Reset the namespace to avoid prefixing with the primary URL prefix
