cb9babea77ea5c5f457d974d6279d87f4ac4791f
TiagoFuelber <tfuelber@gmail.com>
2025-12-18 11:12:24 -0300

    add checkouts list component example
    # This PR adds the following changes
    
    - Render `justifi-checkouts-list` component in `checkouts-list.mdx` page.
    - Updated `mockAllServices.js` with necessary endpoint interceptions for mock data.
    - Added `mockCheckoutsList.json` and `mockSubAccounts.json` mock files.

--- docs/helpers/mockAllServices.js
+++ docs/helpers/mockAllServices.js
@@ -1,8 +1,13 @@
 import { createServer } from 'miragejs';
 import mockBusinessDetails from '../mocks/mockBusinessDetails.json';
+import mockCheckoutsList from '../mocks/mockCheckoutsList.json';
+import mockSubAccounts from '../mocks/mockSubAccounts.json';
+import mockNPMVersion from '../mocks/mockNPMVersion.json';
 
 export const API_PATHS = {
   BUSINESS_DETAILS: '/entities/business/:id',
+  CHECKOUTS_LIST: 'checkouts',
+  SUB_ACCOUNTS_LIST: 'sub_accounts',
   PKG_VERSION: '/@justifi/webcomponents/latest',
 };
 
@@ -15,6 +20,12 @@ export const setUpMocks = () => {
 
       // BusinessDetails
       this.get(API_PATHS.BUSINESS_DETAILS, () => mockBusinessDetails);
+
+      // CheckoutsList
+      this.get(API_PATHS.CHECKOUTS_LIST, () => mockCheckoutsList);
+
+      // SubAccountsList
+      this.get(API_PATHS.SUB_ACCOUNTS_LIST, () => mockSubAccounts);
 
       // URL Prefix for NPM Package Check
       this.namespace = ''; // Reset the namespace to avoid prefixing with the primary URL prefix
