b768ecbfa2beeb9348cb249be1a71a6fb9f4687a
TiagoFuelber <tfuelber@gmail.com>
2025-12-18 15:42:59 -0300

    add terminal orders list component example # This PR adds the following changes:
    - Render the terminal orders list copmonent example.
    - Updated `mockAllServices.js` to return mock data on API calls.
    - Added `mockTerminalOrdersListSuccess.json`  mock file.

--- docs/helpers/mockAllServices.js
+++ docs/helpers/mockAllServices.js
@@ -1,8 +1,11 @@
 import { createServer } from 'miragejs';
 import mockBusinessDetails from '../mocks/mockBusinessDetails.json';
+import mockTerminalOrders from '../mocks/mockTerminalOrdersListSuccess.json';
+import mockNPMVersion from '../mocks/mockNPMVersion.json';
 
 export const API_PATHS = {
   BUSINESS_DETAILS: '/entities/business/:id',
+  TERMINAL_ORDERS_LIST: 'terminals/orders',
   PKG_VERSION: '/@justifi/webcomponents/latest',
 };
 
@@ -15,6 +18,9 @@ export const setUpMocks = () => {
 
       // BusinessDetails
       this.get(API_PATHS.BUSINESS_DETAILS, () => mockBusinessDetails);
+
+      // TerminalOrdersList
+      this.get(API_PATHS.TERMINAL_ORDERS_LIST, () => mockTerminalOrders);
 
       // URL Prefix for NPM Package Check
       this.namespace = ''; // Reset the namespace to avoid prefixing with the primary URL prefix
