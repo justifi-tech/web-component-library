d30989f1abc038594243432421bc57aeb3d93048
TiagoFuelber <tfuelber@gmail.com>
2025-12-18 15:22:55 -0300

    add gross payments chart component example # This PR adds the following changes:
    - Render the gross payments chart copmonent example.
    - Updated `mockAllServices.js` to return mock data on API calls.
    - Added `mockGrossVolumeReportSuccess.json`  mock file.

--- docs/helpers/mockAllServices.js
+++ docs/helpers/mockAllServices.js
@@ -1,8 +1,28 @@
 import { createServer } from 'miragejs';
 import mockBusinessDetails from '../mocks/mockBusinessDetails.json';
+import mockGrossPaymentChart from '../mocks/mockGrossVolumeReportSuccess.json';
+import mockNPMVersion from '../mocks/mockNPMVersion.json';
+
+const handleMockGrossVolumeChartMock = () => {
+  // Map all dates on the mock data to simulate dynamic data, especially to see dates from the past 30 days.
+  let dateBuffer = -1;
+  const mappedDates = mockGrossPaymentChart.data.dates.map((item) => {
+    return {
+      ...item,
+      // for each item in the array, assign a new date one day behind the current date
+      date: new Date(new Date().setDate(new Date().getDate() - ++dateBuffer))
+        .toISOString()
+        .split('T')[0],
+    };
+  });
+
+  mockGrossPaymentChart.data.dates = mappedDates;
+  return mockGrossPaymentChart;
+};
 
 export const API_PATHS = {
   BUSINESS_DETAILS: '/entities/business/:id',
+  GROSS_VOLUME: '/account/:accountId/reports/gross_volume',
   PKG_VERSION: '/@justifi/webcomponents/latest',
 };
 
@@ -15,6 +35,9 @@ export const setUpMocks = () => {
 
       // BusinessDetails
       this.get(API_PATHS.BUSINESS_DETAILS, () => mockBusinessDetails);
+
+      // GrossPaymentChart
+      this.get(API_PATHS.GROSS_VOLUME, handleMockGrossVolumeChartMock);
 
       // URL Prefix for NPM Package Check
       this.namespace = ''; // Reset the namespace to avoid prefixing with the primary URL prefix
