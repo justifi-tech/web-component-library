1e6ff777eb4bee149442daceb7e879dfdb48b54b
TiagoFuelber <tfuelber@gmail.com>
2025-12-18 15:16:58 -0300

    add payout transactions list component example # This PR adds the following changes:
    - Render the payout transactions list copmonent example.
    - Updated `mockAllServices.js` to return mock data on API calls.
    - Added `mockPayoutTransactionsSuccess.json`  mock file.

--- docs/helpers/mockAllServices.js
+++ docs/helpers/mockAllServices.js
@@ -1,8 +1,11 @@
 import { createServer } from 'miragejs';
 import mockBusinessDetails from '../mocks/mockBusinessDetails.json';
+import mockPayoutTransactions from '../mocks/mockPayoutTransactionsSuccess.json';
+import mockNPMVersion from '../mocks/mockNPMVersion.json';
 
 export const API_PATHS = {
   BUSINESS_DETAILS: '/entities/business/:id',
+  PAYOUT_TRANSACTIONS: '/balance_transactions',
   PKG_VERSION: '/@justifi/webcomponents/latest',
 };
 
@@ -15,6 +18,9 @@ export const setUpMocks = () => {
 
       // BusinessDetails
       this.get(API_PATHS.BUSINESS_DETAILS, () => mockBusinessDetails);
+
+      // PayoutTransactions
+      this.get(API_PATHS.PAYOUT_TRANSACTIONS, () => mockPayoutTransactions);
 
       // URL Prefix for NPM Package Check
       this.namespace = ''; // Reset the namespace to avoid prefixing with the primary URL prefix
