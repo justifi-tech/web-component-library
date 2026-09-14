b28710ea431debd2b2375e1a47a3e33b87474789
TiagoFuelber <tfuelber@gmail.com>
2026-09-14 11:20:29 -0300

    chore(docs) add pnpm dev:server command to the list

--- packages/webcomponents/src/ui-components/form/iframe-input.tsx
+++ packages/webcomponents/src/ui-components/form/iframe-input.tsx
@@ -77,6 +77,14 @@ export class IframeInput {
     if (eventType === 'blurred') {
       this.isFocused = false;
     }
+
+    if (eventType === 'loaded') {
+      console.log('iframe loaded and listened from message event', data);
+    }
+
+    if (eventType === 'error') {
+      console.log('iframe error and listened from message event', data);
+    }
   }
 
   private get part() {
@@ -103,6 +111,12 @@ export class IframeInput {
       return iframeInputStyles.focused;
     }
     return null;
+  }
+
+  // fontStyles arrive ~500ms after load (set by <hidden-input />). Rendering the
+  // iframe before then means `src` changes once they land, which reloads it.
+  private get stylesReady() {
+    return !!iframeInputStyles.fontStyles.fontFamily;
   }
 
   private get urlParams() {
@@ -141,7 +155,7 @@ export class IframeInput {
               overflow: 'hidden',
             }}
           >
-            <iframe
+            {this.stylesReady && <iframe
               id={this.inputId}
               name={this.inputId}
               src={`${this.iframeOrigin}?${this.urlParams}`}
@@ -158,7 +172,7 @@ export class IframeInput {
                   }
                 }, this.iframeElement);
               }}
-            />
+            />}
           </div>
           <FormControlErrorText errorText={this.errorText} name={this.inputId} />
         </div>
