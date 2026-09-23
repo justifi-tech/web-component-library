310d6258aab837ab192e1e65c1cccbeb11bd9d3c
TiagoFuelber <tfuelber@gmail.com>
2026-09-23 14:09:06 -0300

    refactor: update JustifiCheckout payment method logic and tests
    - Removed the condition that filtered out Sezzle when disableBnpl is true, ensuring it remains in availablePaymentMethods.
    - Updated related tests to reflect the new logic, confirming Sezzle is retained regardless of the disableBnpl flag.

--- packages/webcomponents/src/components/checkout/justifi-checkout.tsx
+++ packages/webcomponents/src/components/checkout/justifi-checkout.tsx
@@ -78,11 +78,6 @@ export class JustifiCheckout {
 
   @Listen('checkout-changed')
   checkoutChanged(event: CustomEvent<CheckoutChangedEventDetail>) {
-    // if disabled bnpl, remove sezzle from available payment methods
-    if (this.disableBnpl) {
-      this.availablePaymentMethods = event.detail.availablePaymentMethodTypes.filter((method) => method !== PAYMENT_METHODS.SEZZLE);
-      return;
-    }
     this.availablePaymentMethods = event.detail.availablePaymentMethodTypes;
   }
 
